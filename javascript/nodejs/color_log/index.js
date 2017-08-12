bac5e70d83348e1e12e73349696e447158c6f065
Sheraz <sheraz@mbp.com>
2017-08-12 13:30:52 -0400

    re-organizing projects

---
+++ javascript/nodejs/color_log/index.js
@@ -0,0 +1,13 @@
+var color = require('colors');
+
+exports.red_log = function (message) {
+    console.log(message.red);
+};
+
+exports.green_log = function (message) {
+    console.log(message.green);
+};
+
+exports.blue_log = function (message) {
+    console.log(message.blue);
+};
---
+++ javascript/nodejs/my_app/index.js
@@ -0,0 +1,5 @@
+var color_log = require("color_log");
+
+color_log.red_log("My red message.");
+color_log.blue_log("My blue message.");
+color_log.green_log("My green message.");
