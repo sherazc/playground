ed74a5b2b6a0dc7e20edf978a595ffa43f2709ba
Sheraz <sheraz@mbp.com>
2017-09-03 23:54:52 -0400

    Sending back JSON response and Path params Redirecting Request

--- lunch_and_learn/lal_106_expressjs/eg02_expressjs/app03_send_back_json.js
+++ lunch_and_learn/lal_106_expressjs/eg02_expressjs/app03_send_back_json.js
@@ -2,12 +2,21 @@ import express from "express";
 
 let app = express();
 
-app.get(["/", "/*", "/:userName"], (request, response) => {
+// Redirects to /user-profile
+app.get("/", (request, response) => {
+    // All http status codes
+    // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+    // http://expressjs.com/tr/api.html#res.redirect
+    response.redirect(301, "/user-profile");
+});
+
+app.get(["/user-profile", "/user-profile/:userName"], (request, response) => {
     let myUser = {name: "Sheraz", salary: 100};
     if (request.params.userName) {
         myUser.name = request.params.userName;
         myUser.message = "Hello " + request.params.userName;
     }
+    // http://expressjs.com/tr/api.html#res.json
     response.json(myUser);
 });
 
