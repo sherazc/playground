17187ff8153a7e3d1d7a9e18ea895f6a8faae744
Sheraz <sheraz@mbp.com>
2017-09-04 22:58:19 -0400

    submit form

--- lunch_and_learn/lal_106_expressjs/eg03_submit_form/app01.js
+++ lunch_and_learn/lal_106_expressjs/eg03_submit_form/app01.js
@@ -3,7 +3,11 @@ import express from "express";
 let app = express();
 
 app.get("/", (request, response) => {
-    response.send("Hello World");
+    response.redirect(301, "/users");
 });
 
-app.listen(8080);
+app.get("/users", (request, response) => {
+    response.sendFile(__dirname + "/user_register.html");
+});
+
+app.listen(8080, () => console.log("Server Started on port 8080"));
