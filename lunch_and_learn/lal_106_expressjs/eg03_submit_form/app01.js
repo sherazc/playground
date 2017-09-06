63ee5a0c67e9fffebcfade09cca33e70bad7457a
Sheraz <sheraz@mbp.com>
2017-09-05 21:21:17 -0400

    added submit form

--- lunch_and_learn/lal_106_expressjs/eg03_submit_form/app01.js
+++ lunch_and_learn/lal_106_expressjs/eg03_submit_form/app01.js
@@ -1,13 +1,25 @@
 import express from "express";
-
+import bodyParser from "body-parser"
 let app = express();
 
-app.get("/", (request, response) => {
-    response.redirect(301, "/users");
-});
+// https://www.npmjs.com/package/body-parser
+// http://expressjs.com/tr/api.html#req.body
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get("/users", (request, response) => {
+app.get("/", (request, response) => {
     response.sendFile(__dirname + "/user_register.html");
 });
+
+app.post("/", (request, response) => {
+    // http://expressjs.com/tr/api.html#req.query
+    
+    console.log(request.body);
+    console.log("Email =", request.body.email);
+    console.log("Password =", request.body.password);
+
+    response.sendFile(__dirname + "/user_register_confirm.html");
+});
+
 
 app.listen(8080, () => console.log("Server Started on port 8080"));
