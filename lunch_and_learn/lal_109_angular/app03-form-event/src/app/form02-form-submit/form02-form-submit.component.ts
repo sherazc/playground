4be7e1a87f2b49552ca1b2436f858e3f35036a13
Sheraz <sheraz@mbp.com>
2017-09-21 08:36:10 -0400

    Added static readonly class property

--- lunch_and_learn/lal_109_angular/app03-form-event/src/app/form02-form-submit/form02-form-submit.component.ts
+++ lunch_and_learn/lal_109_angular/app03-form-event/src/app/form02-form-submit/form02-form-submit.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
-const DEFAULT_MESSAGE = 'Bye';
-
 @Component({
   selector: 'app-form02-form-submit',
   templateUrl: './form02-form-submit.component.html',
   styleUrls: ['./form02-form-submit.component.css']
 })
 export class Form02FormSubmitComponent {
+  static readonly DEFAULT_MESSAGE = 'Bye';
   message: string;
 
   constructor() {
-    this.message = DEFAULT_MESSAGE;
+    this.message = Form02FormSubmitComponent.DEFAULT_MESSAGE;
   }
 
   changeMessage(personName: string) {
     if (personName) {
       this.message = `Hello ${personName}`;
     } else {
-      this.message = DEFAULT_MESSAGE;
+      this.message = Form02FormSubmitComponent.DEFAULT_MESSAGE;
     }
     return false;
   }
