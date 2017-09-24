cc701932467cc6283a209c5f265469241fced332
Sheraz <sheraz@mbp.com>
2017-09-24 18:38:10 -0400

    gulp watch

--- lunch_and_learn/lal_gulp/eg06_watch/gulpfile.js
+++ lunch_and_learn/lal_gulp/eg06_watch/gulpfile.js
@@ -1,14 +1,21 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
 
-gulp.task("default", () => {
-    // build JS
-    gulp.src("./src/js/**/*.js")
-        .pipe(concat("main.js"))
-        .pipe(gulp.dest("./dist"));
-
-    // Copy htmls
+gulp.task("build-html", () => {
     gulp.src("./src/html/**/*.html")
         .pipe(gulp.dest("./dist"));
 });
 
+gulp.task("build-js", () => {
+    gulp.src("./src/js/**/*.js")
+        .pipe(concat("main.js"))
+        .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("default", ["build-html", "build-js"]);
+
+gulp.task("build-and-watch", () => {
+    gulp.start("default");
+    gulp.watch("./src/js/**/*.js", ["build-js"]);
+    gulp.watch("./src/html/**/*.html", ["build-html"]);
+});
