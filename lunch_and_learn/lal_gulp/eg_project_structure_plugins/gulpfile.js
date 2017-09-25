96773fbdd48f5096045ef0f91a74925604721f12
sheraz <sheraz@sheraz.com>
2017-09-24 23:55:32 -0400

    Trying to make source map changes

--- lunch_and_learn/lal_gulp/eg_project_structure_plugins/gulpfile.js
+++ lunch_and_learn/lal_gulp/eg_project_structure_plugins/gulpfile.js
@@ -1,15 +1,28 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
 const browserify = require('gulp-browserify');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task("default", () => {
-    gulp.src("./src/js/calculator.js")
+gulp.task("build-js", () => {
+    gulp.src("./src/js/*.js")
+        .pipe(sourcemaps.init())
         .pipe(babel({
            presets: ['es2015']
         }))
         .pipe(browserify({
-            insertGlobals: false,
+            insertGlobals: true,
             debug: true
         }))
+        .pipe(sourcemaps.write("./sm"))
         .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("build-html", () => {
+    gulp.src("./src/html/**/*.html")
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("default", ["build-js", "build-html"], () => {
+    gulp.watch("./src/html/**/*.html", ["build-html"]);
+    gulp.watch("./src/js/**/*.js", ["build-js"]);
 });
