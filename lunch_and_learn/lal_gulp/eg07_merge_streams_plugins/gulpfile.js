ece1dedba335e1e4938929c971bc74d001e8043c
sheraz <sheraz@sheraz.com>
2017-10-01 00:42:54 -0400

    Merge Gulp Streams

--- lunch_and_learn/lal_gulp/eg07_merge_streams_plugins/gulpfile.js
+++ lunch_and_learn/lal_gulp/eg07_merge_streams_plugins/gulpfile.js
@@ -1,27 +1,30 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var babelify = require('babelify');
-var gutil = require('gulp-util');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const merge = require('merge-stream');
+const minifyCss = require('gulp-minify-css');
 
-gulp.task('scripts', function() {
-	browserify({ debug: true })
-		.transform(babelify, { presets: ["env"] })
-		.require("./src/js/main.js", { entry: true })
-		.bundle()
-		.on('error',gutil.log)
-		.pipe(source('bundle.js'))
-    	.pipe(gulp.dest('./dist/js'));
+gulp.task("compile-css", () => {
+
+	let cssStream = gulp.src("./src/css/**/*.css")
+		.pipe(concat("all_css.css"));
+
+	let sassStream = gulp.src("./src/sass/**/*.sass")
+		.pipe(sass())
+		.pipe(concat("all_sass.sass"));
+
+	merge(cssStream, sassStream)
+		.pipe(concat("styles.css"))
+		//.pipe(minifyCss())
+		.pipe(gulp.dest("dist/css/"));
 });
 
-gulp.task('watch', function () {
-    gulp.watch("./src/html/**/*.html", ["build-html"]);
-    gulp.watch(['./src/js/**/*.js'], ['scripts']);
+gulp.task("copy-html", () => {
+	gulp.src("./src/html/**/*.html")
+		.pipe(gulp.dest("dist/"))
 });
 
-gulp.task("build-html", () => {
-    gulp.src("./src/html/**/*.html")
-        .pipe(gulp.dest("dist/"));
+gulp.task("default", ["compile-css", "copy-html"], () => {
+    gulp.watch(["./src/css/**/*.css", "./src/sass/**/*.sass"], ["compile-css"]);
+    gulp.watch("./src/html/**/*.html", ["copy-html"]);
 });
-
-gulp.task('default', ['scripts', "build-html", 'watch']);
