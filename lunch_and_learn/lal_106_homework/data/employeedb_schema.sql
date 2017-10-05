6ce961084c2850ad607ed38bc54187dccac7b4b8
Sheraz <sheraz@mbp.com>
2017-10-05 15:32:14 -0400

    added datamodel

---
+++ lunch_and_learn/lal_106_homework/data/employeedb_schema.sql
@@ -0,0 +1,13 @@
+drop database if exists employeedb;
+
+create database employeedb;
+
+use employeedb;
+
+drop table if exists employee;
+
+create table employee (id integer auto_increment, name varchar(255), salary integer(10), primary key (id));
+
+insert into employee (id, name, salary) values (10, 'Sheraz name', 100);
+
+select id, name, salary from employee;
