2e2d4090d60190ed6a772eb7517541da2cdb557b
sheraz <sheraz@sheraz.com>
2017-10-14 19:08:57 -0400

    Added Colors

--- projects/azan_call/src/Main.js
+++ projects/azan_call/src/Main.js
@@ -10,16 +10,40 @@ import {
 
 let salatTime = new SalatTime('http://dashboard.masjidhamzah.com/salat_time.php');
 
+let todaySalatTime = { 
+    "id": "287", 
+    "prayer_date": "2013-10-14", 
+    "fajr_athan": "0000-00-00 06:33:00", 
+    "fajr_iqama": "0000-00-00 06:50:00", 
+    "thuhr_athan": "0000-00-00 13:24:00", 
+    "thuhr_iqama": "0000-00-00 14:00:00", 
+    "asr_athan": "0000-00-00 16:38:00", 
+    "asr_iqama": "0000-00-00 17:45:00", 
+    "maghrib_athan": "0000-00-00 19:06:00", 
+    "maghrib_iqama": "5 Min", 
+    "isha_athan": "0000-00-00 20:15:00", 
+    "isha_iqama": "0000-00-00 20:45:00", 
+    "shurooq": "0000-00-00 07:40:00", 
+    "maghrib_change": "0000-00-00 19:05:00", 
+    "maghrib_change_date": "2017-10-15", 
+    "asr_change": "0000-00-00 17:30:00", 
+    "asr_change_date": "2017-10-17", 
+    "isha_change": "0000-00-00 20:30:00", 
+    "isha_change_date": "2017-10-17", 
+    "fajr_change": "0000-00-00 07:00:00", 
+    "fajr_change_date": "2017-10-20" 
+}
+
 
 let screens = {
-    
+
 }
 
 let screen = (screenStyle, mainMessage, subMessage) => {
     return {
         screenStyle, mainMessage, subMessage
     };
-} 
+}
 
 
 
@@ -42,12 +66,15 @@ export default class Main extends Component {
 
     render() {
         return (
-            <View style={this.state.screenColor}>
-                <TouchableHighlight onPress={this.updateScreen.bind(this)}>
-                    <Text>Testing</Text>
-                </TouchableHighlight>
-                
-            </View>
+            <TouchableHighlight
+                style={styles.screenRed} 
+                onPress={this.updateScreen.bind(this)}>
+                <Text style={styles.textExtraLargeLight}>
+                    Asr Azan Not Called
+                </Text>
+            </TouchableHighlight>
+
+        
         );
     }
 }
@@ -58,7 +85,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    buttonRed: {
+    screenRed: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
@@ -67,7 +94,7 @@ const styles = StyleSheet.create({
         alignSelf: 'stretch',
 
     },
-    buttonGreen: {
+    screenGreen: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
@@ -75,20 +102,29 @@ const styles = StyleSheet.create({
         padding: 10,
         alignSelf: 'stretch',
     },
-    result: {
+    screenBlack: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#f4f1db',
         padding: 10,
         alignSelf: 'stretch',
+    }, 
+    textExtraLargeLight: {
+        color: "#ffffff",
+        fontSize: 30
     },
-    textButton: {
-        color: "#f6f6f6",
+    textExtraLargeDark: {
+        color: "#111111",
+        fontSize: 24
+
+    },
+    textExtraLargeGray: {
+        color: 24,
         fontSize: 24
     },
-    textResult: {
-        color: "#3e3e3e",
-        fontSize: 24
+    textDark: {
+        color: "#111111",
+        fontSize: 12
     }
 });
