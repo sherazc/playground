2ba37a3c261873533a47f719fea47cac97140a51
sheraz <sheraz@sheraz.com>
2017-10-15 03:00:59 -0400

    working on azan

--- projects/azan_call/src/Main.js
+++ projects/azan_call/src/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SalatTime from "./services/SalatTime";
+// import SalatTime from "./services/SalatTime";
 
 import {
     StyleSheet,
@@ -8,70 +8,89 @@ import {
     View
 } from 'react-native';
 
-let salatTime = new SalatTime('http://dashboard.masjidhamzah.com/salat_time.php');
-
-let todaySalatTime = { 
-    "id": "287", 
-    "prayer_date": "2013-10-14", 
-    "fajr_athan": "0000-00-00 06:33:00", 
-    "fajr_iqama": "0000-00-00 06:50:00", 
-    "thuhr_athan": "0000-00-00 13:24:00", 
-    "thuhr_iqama": "0000-00-00 14:00:00", 
-    "asr_athan": "0000-00-00 16:38:00", 
-    "asr_iqama": "0000-00-00 17:45:00", 
-    "maghrib_athan": "0000-00-00 19:06:00", 
-    "maghrib_iqama": "5 Min", 
-    "isha_athan": "0000-00-00 20:15:00", 
-    "isha_iqama": "0000-00-00 20:45:00", 
-    "shurooq": "0000-00-00 07:40:00", 
-    "maghrib_change": "0000-00-00 19:05:00", 
-    "maghrib_change_date": "2017-10-15", 
-    "asr_change": "0000-00-00 17:30:00", 
-    "asr_change_date": "2017-10-17", 
-    "isha_change": "0000-00-00 20:30:00", 
-    "isha_change_date": "2017-10-17", 
-    "fajr_change": "0000-00-00 07:00:00", 
-    "fajr_change_date": "2017-10-20" 
-}
-
-
-let screens = {
-
-}
-
-let screen = (screenStyle, mainMessage, subMessage) => {
-    return {
-        screenStyle, mainMessage, subMessage
-    };
-}
-
-
+// let salatTime = new SalatTime('http://dashboard.masjidhamzah.com/salat_time.php');
 
 export default class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            screenColor: styles.buttonRed
+            screen: {
+                mainMessage: "Main Loading...",
+                subMessage: "Sub Loading...",
+                style: {
+                    background: styles.screenDark,
+                    mainMessage: styles.textExtraLargeLight,
+                    subMessage: styles.textLight
+                }
+
+            },
+            azanCalled: false,
+            salatDone: false,
+            currentSalah: {},
+            nextSalah: {}
         }
     }
+
     componentDidMount() {
         // salatTime.reteriveTodaysSchedule();
     }
 
-    updateScreen() {
-        this.setState({
-            screenColor: styles.buttonGreen
-        });
+    mainTapped() {
+        if (this.state.azanCalled) {
+            this.setState({
+                screen: {
+                    mainMessage: "Main Loading...",
+                    style: {
+                        background: styles.screenGreen,
+                        //mainMessage: styles.textExtraLargeLight,
+                        //subMessage: styles.textLight
+                    }
+    
+                },
+                azanCalled: false,
+                
+            });
+        } else {
+            this.setState({
+                screen: {
+                    style: {
+                        background: styles.screenRed,
+                        //mainMessage: styles.textExtraLargeLight,
+                        //subMessage: styles.textLight
+                    }
+    
+                },
+                azanCalled: true,
+                
+            });
+        }
+        console.log("aaaaa");
+    }
+    settings() {
+        console.log("bbbb");
     }
 
     render() {
+        
+
         return (
-            <TouchableHighlight
-                style={styles.screenRed} 
-                onPress={this.updateScreen.bind(this)}>
-                <Text style={styles.textExtraLargeLight}>
-                    Asr Azan Not Called
-                </Text>
+
+
+            <TouchableHighlight onPress={this.mainTapped.bind(this)} style={this.state.screen.style.background}>
+                <View>
+                    <TouchableHighlight onPress={this.settings}>
+                        <Text style={styles.textLight}>
+                            Settings
+                        </Text>
+                    </TouchableHighlight>
+                    <Text style={this.state.screen.style.mainMessage}>
+                        {this.state.screen.mainMessage}
+                    </Text>
+                    <Text style={this.state.screen.style.subMessage}>
+                        {this.state.screen.subMessage}
+                    </Text>
+                    
+                </View>
             </TouchableHighlight>
 
         
@@ -102,11 +121,11 @@ const styles = StyleSheet.create({
         padding: 10,
         alignSelf: 'stretch',
     },
-    screenBlack: {
+    screenDark: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f4f1db',
+        backgroundColor: '#111111',
         padding: 10,
         alignSelf: 'stretch',
     }, 
@@ -125,6 +144,10 @@ const styles = StyleSheet.create({
     },
     textDark: {
         color: "#111111",
+        fontSize: 12
+    },
+    textLight: {
+        color: "#ffffff",
         fontSize: 12
     }
 });
