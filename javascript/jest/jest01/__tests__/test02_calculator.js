ac7ca660b4519e1ada0e9da5ef94dee3812ce621
Sheraz <sheraz@mbp.com>
2017-11-11 23:37:45 -0500

    added mock function

--- javascript/jest/jest01/__tests__/test02_calculator.js
+++ javascript/jest/jest01/__tests__/test02_calculator.js
@@ -1,5 +1,10 @@
 describe("My Application addIt works", () => {
     it("Calculator can add", () => {
-        
+        let addItMock = jest.mock("../src/addIt", () => {
+            return jest.fn(() => 2);
+        });
+        let calculator = require("../src/calculator");
+        let calculatorResult = calculator(1, 1, "+");
+        expect(calculatorResult).toBe(2);
     });
 });
