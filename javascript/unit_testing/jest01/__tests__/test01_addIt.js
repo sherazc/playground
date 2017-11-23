4ba6deb3932ddefdf75e93829ac0c9ef5b3ea086
Sheraz <sheraz@mbp.com>
2017-11-23 09:12:12 -0500

    updated few tests

--- javascript/unit_testing/jest01/__tests__/test01_addIt.js
+++ javascript/unit_testing/jest01/__tests__/test01_addIt.js
@@ -1,23 +1,23 @@
 let add = (a, b) => {
-    console.log(`addIt ${a} + ${b}`);
+    console.log(`add ${a} + ${b}`);
     return a + b;
 }
 
 describe("My Application addIt works", () => {
     describe("addIt can add", () => {
         it("addIt can add 1 + 1", () => {
-            expect(addIt(1, 1)).toBe(2);
+            expect(add(1, 1)).toBe(2);
         });
         test("addIt can add 2 + 2", () => {
-            expect(addIt(2, 2)).toBe(4);
+            expect(add(2, 2)).toBe(4);
         });
     });
 });
 
 test("addIt can add 3 + 3", () => {
-    expect(addIt(3, 3)).toBe(6);
+    expect(add(3, 3)).toBe(6);
 });
 
 it("addIt can add 4 + 4", () => {
-    expect(addIt(4, 4)).toBe(8);
+    expect(add(4, 4)).toBe(8);
 });
--- javascript/unit_testing/jest02_mocking/__tests__/test01_calculatorA_callback_mock.js
+++ javascript/unit_testing/jest02_mocking/__tests__/test01_calculatorA_callback_mock.js
@@ -1,11 +1,12 @@
 describe("My Application calculatorA works", () => {
     it("calculatorA can add", () => {
+
+        let calculatorA = require("../src/calculatorA");
         /*
         Mock function, and its mock implementation to return a value.
         */
         let addItMockFunction = jest.fn(() => 2);
         
-        let calculatorA = require("../src/calculatorA");
         let calculatorResult = calculatorA(1, 1, addItMockFunction);
         expect(calculatorResult).toBe("calculatorA = 2");
     });
