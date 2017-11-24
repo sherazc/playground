94d82caff72192f1dfbeab73d937a955834faf36
Sheraz <sheraz@mbp.com>
2017-11-24 00:56:59 -0500

    Created Mock function

--- javascript/unit_testing/jest02_mocking/__tests__/test01_mock_function.js
+++ javascript/unit_testing/jest02_mocking/__tests__/test01_mock_function.js
@@ -1,23 +1,22 @@
-describe("My Application calculatorA works", () => {
-    it("calculatorA can add", () => {
-
-        let calculatorA = require("../src/calculatorA");
-        /*
-        Mock function, and its mock implementation to return a value.
-        */
-        let addItMockFunction = jest.fn(() => 30);
+describe("My Mock Application", () => {
+    it("mock function", () => {
+        // Create mock function and have it return a value
+        let myMockFunction = jest.fn(() => "Hello Sheraz Chaudhry");
         
-        let calculatorResult = calculatorA(10, 20, addItMockFunction);
-        expect(calculatorResult).toBe("calculatorA = 30");
+        // Running mock function
+        let result = myMockFunction("Sheraz", "Chaudhry");
+
+        // Verifying return value of mock function
+        expect(result).toBe("Hello Sheraz Chaudhry");
 
         // Verify how many times mock function was called
-        // calls is a 2 dimentional array [calls][arguments[]]
-        expect(addItMockFunction.mock.calls.length).toBe(1);
+        // calls is a 2 dimentional array [number of mock function called][arguments[]]
+        expect(myMockFunction.mock.calls.length).toBe(1);
         
-        // Verify first argument to be 10 of first call
-        expect(addItMockFunction.mock.calls[0][0]).toBe(10);
-        
+        // Verify first argument to be "Sheraz" of first call
+        expect(myMockFunction.mock.calls[0][0]).toBe("Sheraz");
+
         // Verify second argument to be 20 of first call
-        expect(addItMockFunction.mock.calls[0][1]).toBe(20);
+        expect(myMockFunction.mock.calls[0][1]).toBe("Chaudhry");
     });
 });
