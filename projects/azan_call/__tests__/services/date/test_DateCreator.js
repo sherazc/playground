520356a5fc1a7a1e99a6bf9d6e51c5b2a430b770
Sheraz <sheraz@mbp.com>
2017-12-13 01:03:28 -0500

    Working on DateCreator

--- projects/azan_call/__tests__/services/date/test_DateCreator.js
+++ projects/azan_call/__tests__/services/date/test_DateCreator.js
@@ -10,17 +10,41 @@ describe("DateCreator", () => {
         expect(DateCreator.fromISO()).toBeFalsy();
     });
 
-    it("fromISO handle string", () => {
-        let date = DateCreator.fromISO("2016-02-29T18:40:50.000Z");
-        // (new Date()).getUTCFullYear
-        //console.log(getUTCFullYear())
+    it("fromISO handle full date time string", () => {
+        let date = DateCreator.fromISO("2016-02-29 18:40:50.123");
+        
         expect(date.getUTCFullYear()).toBe(2016);
         expect(date.getUTCMonth()).toBe(1);
-        expect(date.getUTCDay()).toBe(29);
+        expect(date.getUTCDate()).toBe(29);
+        expect(date.getUTCHours()).toBe(18);
+        expect(date.getUTCMinutes()).toBe(40);
+        expect(date.getUTCSeconds()).toBe(50);
+        expect(date.getUTCMilliseconds()).toBe(123);
+        expect(date.toISOString()).toBe("2016-02-29T18:40:50.123Z");
+    });
+
+    it("fromISO handle date string", () => {
+        let date = DateCreator.fromISO("2016-02-29");
         
         expect(date.getUTCFullYear()).toBe(2016);
-        expect(date.getUTCFullYear()).toBe(2016);
-        expect(date.getUTCFullYear()).toBe(2016);
+        expect(date.getUTCMonth()).toBe(1);
+        expect(date.getUTCDate()).toBe(29);
         
+        expect(date.getUTCHours()).toBe(0);
+        expect(date.getUTCMinutes()).toBe(0);
+        expect(date.getUTCSeconds()).toBe(0);
+        expect(date.getUTCMilliseconds()).toBe(0);
+        expect(date.toISOString()).toBe("2016-02-29T00:00:00.000Z");
+    });
+
+    it("fromISO handle falsy", () => {
+        expect(DateCreator.fromISO("0000-01-01")).toBeFalsy();
+        expect(DateCreator.fromISO("1899-01-01")).toBeFalsy();
+        expect(DateCreator.fromISO("3000-01-01")).toBeFalsy();
+    });
+
+    it("fromISO handle limits", () => {
+        expect(DateCreator.fromISO("1900-01-01").toISOString()).toBe("1900-01-01T00:00:00.000Z");
+        expect(DateCreator.fromISO("2999-12-31").toISOString()).toBe("2999-12-31T00:00:00.000Z");
     });
 });
--- projects/azan_call/src/services/date/DateCreator.js
+++ projects/azan_call/src/services/date/DateCreator.js
@@ -3,10 +3,40 @@ let now = () => {
 }
 
 let fromISO = (isoDateString) => {
-    if(!isoDateString) {
+    if(!isoDateString || isoDateString.length < 4) {
         return;
     }
-    return new Date(Date.parse(isoDateString));
+   
+    let year = subStringToNumber(isoDateString, 0, 4);
+    let month = subStringToNumber(isoDateString, 5, 7);
+    let date = subStringToNumber(isoDateString, 8, 10);
+    let hours = subStringToNumber(isoDateString, 11, 13);
+    let minutes = subStringToNumber(isoDateString, 14, 16);
+    let seconds = subStringToNumber(isoDateString, 17, 19);
+    let milliseconds = subStringToNumber(isoDateString, 20, 23);
+
+    if (
+        year > 1899 && year < 3000 
+        && month > 0 && month < 13
+        && date > 0 && date < 32
+        && hours > -1 && hours < 24
+        && minutes > -1 && minutes < 60
+        && seconds > -1 && seconds < 60
+        ) {
+        return new Date(Date.UTC(year, --month, date, hours, minutes, seconds, milliseconds));
+    } else {
+        return;
+    }    
+}
+
+let subStringToNumber = (stringInput, fromIndex, toIndex) => {
+    let subStringResult = stringInput.substring(fromIndex, toIndex);
+    let numberResult = parseInt(subStringResult);
+    if (numberResult) {
+        return numberResult;
+    } else {
+        return 0;
+    }
 }
 
 module.exports = {
