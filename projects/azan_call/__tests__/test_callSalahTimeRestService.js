1c55d66fe5e1d570d610752b3cb2afa098d32365
Sheraz <sheraz@mbp.com>
2017-12-15 18:52:37 -0500

    tested callSalahTimeService

--- projects/azan_call/__tests__/test_callSalahTimeRestService.js
+++ projects/azan_call/__tests__/test_callSalahTimeRestService.js
@@ -9,6 +9,19 @@ describe("callSalahTimeRestService", () => {
     it("working", (done) => {
         callSalahTimeRestService(Constants.SERVICE_URL, 
             (salahTimeObject) => {
+                expect(salahTimeObject).toBeTruthy();
+                expect(salahTimeObject.prayer_date).toBeTruthy();
+                expect(salahTimeObject.fajr_athan).toBeTruthy();
+                expect(salahTimeObject.fajr_iqama).toBeTruthy();
+                expect(salahTimeObject.thuhr_athan).toBeTruthy();
+                expect(salahTimeObject.thuhr_iqama).toBeTruthy();
+                expect(salahTimeObject.asr_athan).toBeTruthy();
+                expect(salahTimeObject.asr_iqama).toBeTruthy();
+                expect(salahTimeObject.maghrib_athan).toBeTruthy();
+                expect(salahTimeObject.maghrib_iqama).toBeTruthy();
+                expect(salahTimeObject.isha_athan).toBeTruthy();
+                expect(salahTimeObject.isha_iqama).toBeTruthy();
+                expect(salahTimeObject.shurooq).toBeTruthy();
                 done();
             },
             (error) => {
@@ -20,12 +33,7 @@ describe("callSalahTimeRestService", () => {
 
 let mockFetchImpl = () => {
     let fetchPromise = new Promise((resolve, reject) => {
-
-        // 
-        let response = {
-            json: () => JSON.stringify(makeFakeSalatTime())
-        }
-        resolve(response);
+        resolve(makeFakeSalatTime());
     });
     return fetchPromise;
 };
--- projects/azan_call/src/services/callSalahTimeRestService.js
+++ projects/azan_call/src/services/callSalahTimeRestService.js
@@ -3,13 +3,18 @@ let callSalahTimeRestService = (serviceUrl, successCallBack, errorCallBack) => {
         errorCallBack(error);
         console.log(error);
     };
+
+    fetch(serviceUrl)
+        .then((response) => successCallBack(response), errorHandler)
+        .catch(errorHandler);
+    /*
     fetch(serviceUrl)
         .then((response) => response.json(), errorHandler)
         .then((salatTimeObject) => {
-            console.log(salatTimeObject)
             successCallBack(salatTimeObject);
         }, errorHandler)
         .catch(errorHandler);
+        */
 }
 
 module.exports = callSalahTimeRestService;
