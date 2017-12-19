8c717be75a7d05634f1cf1e6df8674f12bc95b83
Sheraz <sheraz@mbp.com>
2017-12-19 08:29:10 -0500

    Detacting orentation

--- projects/azan_call/index.ios.js
+++ projects/azan_call/index.ios.js
@@ -10,47 +10,14 @@ import {
 
 import Main from "./src/Main";
 
+
 export default class azan_call extends Component {
-  onLayout(e) {
-    const {width, height} = Dimensions.get('window')
-    console.log(width, height)
-  }
-
   render() {
-    var style = StyleSheet.create({
-      parent: {
-          width: '100%',
-          height: '100%',
-
-          flexDirection: 'row', 
-          flexWrap: 'wrap'
-      },
-      child: {
-          width: 100,
-          height: '25%', 
-          //aspectRatio: 1,
-          padding: 10,
-          //margin: 10,
-      }
-    });
-    return (
-      <View style={[style.parent]} onLayout={this.onLayout.bind(this)}>
-        <View style={[style.child, {backgroundColor: '#996666'} ]} />
-        <View style={[style.child, {backgroundColor: '#339966'} ]} >
-          <Image source={require("./src/ui/images/azan_called.png")}
-              style={{width: "100%", height: "100%", resizeMode: "contain"}}/>
-        </View>
-        <View style={[style.child, {backgroundColor: '#996633'} ]} />
-        <View style={[style.child, {backgroundColor: '#669933'} ]} />        
-      </View>
-    )
-    /*
     return (
       <View style={styles.container}>
         <Main/>
       </View>
     );
-    */
   }
 }
 
@@ -64,3 +31,40 @@ const styles = StyleSheet.create({
 });
 
 AppRegistry.registerComponent('azan_call', () => azan_call);
+
+/*
+<Main />
+onLayout(e) {
+  const {width, height} = Dimensions.get('window')
+  console.log(width, height)
+}
+
+render() {
+  var style = StyleSheet.create({
+    parent: {
+        width: '100%',
+        height: '100%',
+
+        flexDirection: 'row', 
+        flexWrap: 'wrap'
+    },
+    child: {
+        width: 100,
+        height: '25%', 
+        //aspectRatio: 1,
+        padding: 10,
+        //margin: 10,
+    }
+  });
+  return (
+    <View style={[style.parent]} onLayout={this.onLayout.bind(this)}>
+      <View style={[style.child, {backgroundColor: '#996666'} ]} />
+      <View style={[style.child, {backgroundColor: '#339966'} ]} >
+        <Image source={require("./src/ui/images/azan_called.png")}
+            style={{width: "100%", height: "100%", resizeMode: "contain"}}/>
+      </View>
+      <View style={[style.child, {backgroundColor: '#996633'} ]} />
+      <View style={[style.child, {backgroundColor: '#669933'} ]} />        
+    </View>
+  )
+*/
--- projects/azan_call/src/Main.js
+++ projects/azan_call/src/Main.js
@@ -3,7 +3,8 @@ import {
     StyleSheet,
     Text,
     TouchableHighlight,
-    View
+    View,
+    Dimensions
 } from 'react-native';
 
 import Alert from "./ui/Alert";
@@ -12,6 +13,8 @@ let DateCreator = require("./services/date/DateCreator");
 let startAzanProcess = require("./services/startAzanProcess");
 const Constants = require("./services/Constants");
 
+const ORIENTATION = {POTRATE: "POTRATE", LANDSCAPE: "LANDSCAPE"}
+
 export default class Main extends Component {
     constructor(props) {
         super(props);
@@ -19,20 +22,26 @@ export default class Main extends Component {
             mainMessage: "Loading...",
             subMessage: "",
             azanCalledDateTime: null,
-            alert: Constants.ALERT_BLACK
+            alert: Constants.ALERT_BLACK,
+            orientation: ORIENTATION.POTRATE
         };
     }
 
     componentDidMount() {
         startAzanProcess(this.updateAzanMessage.bind(this), this.getAzanCalledDateTime.bind(this));
+        //this.detactOrientation();
     }
 
     mainTapped() {
-        this.setState({azanCalledDateTime: DateCreator.now()}, () => {
-            //Alert.show("Alert", `Main tapped azanCalledDateTime ${this.state.azanCalledDateTime}`);
-        });
+        this.setState({azanCalledDateTime: DateCreator.now()});
     }
 
+    detactOrientation() {
+        const {width, height} = Dimensions.get('window');
+        let orientation = width > height ? ORIENTATION.LANDSCAPE : ORIENTATION.POTRATE;
+        console.log(orientation);
+        this.setState({orientation: orientation});
+    }
 
     updateAzanMessage(uiMessageResult) {
         this.setState({
@@ -46,13 +55,7 @@ export default class Main extends Component {
         return this.state.azanCalledDateTime;
     }
 
-
-    settings() {
-        Alert.show("Alert", `Settings tapped ${new Date()}`);
-    }
-
     render() {
-
         let backgroundStyle = mainStyles.screenDark;
         switch(this.state.alert) {
             case Constants.ALERT_BLACK:
@@ -68,13 +71,9 @@ export default class Main extends Component {
         }
 
         return (
-            <TouchableHighlight onPress={this.mainTapped.bind(this)} style={backgroundStyle}>
+            <TouchableHighlight onLayout={this.detactOrientation.bind(this)} 
+                onPress={this.mainTapped.bind(this)} style={backgroundStyle}>
                 <View>
-                    <TouchableHighlight onPress={this.settings}>
-                        <Text style={mainStyles.textLight}>
-                            Settings
-                        </Text>
-                    </TouchableHighlight>
                     <Text style={mainStyles.textExtraLargeLight}>
                         {this.state.mainMessage}
                     </Text>
