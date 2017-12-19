0f2e860cc62ab6ae107a4f6f98698e2f05fa725b
Sheraz <sheraz@mbp.com>
2017-12-18 23:40:23 -0500

    Trying out orientation, image, flexbox

--- projects/azan_call/index.ios.js
+++ projects/azan_call/index.ios.js
@@ -2,18 +2,55 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  View
+  View,
+  Text,
+  Image,
+  Dimensions,
 } from 'react-native';
 
 import Main from "./src/Main";
 
 export default class azan_call extends Component {
+  onLayout(e) {
+    const {width, height} = Dimensions.get('window')
+    console.log(width, height)
+  }
+
   render() {
+    var style = StyleSheet.create({
+      parent: {
+          width: '100%',
+          height: '100%',
+
+          flexDirection: 'row', 
+          flexWrap: 'wrap'
+      },
+      child: {
+          width: 100,
+          height: '25%', 
+          //aspectRatio: 1,
+          padding: 10,
+          //margin: 10,
+      }
+    });
+    return (
+      <View style={[style.parent]} onLayout={this.onLayout.bind(this)}>
+        <View style={[style.child, {backgroundColor: '#996666'} ]} />
+        <View style={[style.child, {backgroundColor: '#339966'} ]} >
+          <Image source={require("./src/ui/images/azan_called.png")}
+              style={{width: "100%", height: "100%", resizeMode: "contain"}}/>
+        </View>
+        <View style={[style.child, {backgroundColor: '#996633'} ]} />
+        <View style={[style.child, {backgroundColor: '#669933'} ]} />        
+      </View>
+    )
+    /*
     return (
       <View style={styles.container}>
         <Main/>
       </View>
     );
+    */
   }
 }
 
