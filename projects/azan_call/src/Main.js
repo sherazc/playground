25e50f5c86cb2fe908a472109766fe10bcdc5a6a
Sheraz <sheraz@mbp.com>
2017-12-19 23:46:42 -0500

    Externalized OrientationDetector

--- projects/azan_call/src/Main.js
+++ projects/azan_call/src/Main.js
@@ -8,12 +8,13 @@ import {
 } from 'react-native';
 
 import Alert from "./ui/Alert";
+import OrientationDetector, {ORIENTATION} from "./ui/OrientationDetector"
 let mainStyles = require("./ui/MainStyles").mainStyles;
 let DateCreator = require("./services/date/DateCreator");
 let startAzanProcess = require("./services/startAzanProcess");
 const Constants = require("./services/Constants");
 
-const ORIENTATION = {POTRATE: "POTRATE", LANDSCAPE: "LANDSCAPE"}
+let orientationDetector = new OrientationDetector();
 
 export default class Main extends Component {
     constructor(props) {
@@ -23,24 +24,15 @@ export default class Main extends Component {
             subMessage: "",
             azanCalledDateTime: null,
             alert: Constants.ALERT_BLACK,
-            orientation: ORIENTATION.POTRATE
         };
     }
 
     componentDidMount() {
         startAzanProcess(this.updateAzanMessage.bind(this), this.getAzanCalledDateTime.bind(this));
-        //this.detactOrientation();
     }
 
     mainTapped() {
         this.setState({azanCalledDateTime: DateCreator.now()});
-    }
-
-    detactOrientation() {
-        const {width, height} = Dimensions.get('window');
-        let orientation = width > height ? ORIENTATION.LANDSCAPE : ORIENTATION.POTRATE;
-        console.log(orientation);
-        this.setState({orientation: orientation});
     }
 
     updateAzanMessage(uiMessageResult) {
@@ -70,8 +62,9 @@ export default class Main extends Component {
             default:
         }
 
+        console.log(orientationDetector.orientation);
         return (
-            <TouchableHighlight onLayout={this.detactOrientation.bind(this)} 
+            <TouchableHighlight onLayout={orientationDetector.detectOrientation.bind(orientationDetector)}
                 onPress={this.mainTapped.bind(this)} style={backgroundStyle}>
                 <View>
                     <Text style={mainStyles.textExtraLargeLight}>
---
+++ projects/azan_call/src/ui/OrientationDetector.js
@@ -0,0 +1,19 @@
+import {
+    Dimensions
+} from 'react-native';
+
+const ORIENTATION = {PORTRAIT: "PORTRAIT", LANDSCAPE: "LANDSCAPE"};
+
+class OrientationDetector {
+
+    constructor() {
+        this.orientation = "";
+    }
+
+    detectOrientation(event) {
+        const {width, height} = Dimensions.get('window');
+        this.orientation = width > height ? ORIENTATION.LANDSCAPE : ORIENTATION.PORTRAIT;
+    }
+}
+
+export {OrientationDetector as default, ORIENTATION}
