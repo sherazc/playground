b3d7bbe0ae94b7cfbb22cbcc3ad90830e3835c68
Sheraz <sheraz@mbp.com>
2017-12-31 17:10:45 -0500

    animating ng-repeat

--- javascript/angularjs_01/app06_animation/script.js
+++ javascript/angularjs_01/app06_animation/script.js
@@ -2,5 +2,5 @@ var myApp = angular.module("myApp", ["ngRoute", "ngAnimate"]);
 myApp.config(function($routeProvider) {
     $routeProvider
         .when("/v01_ng-show", {templateUrl: "./views/v01_ng-show.html"})
-        .when("/v02_ng-repeat", {templateUrl: "./views/v02_ng-repeat.html"});
+        .when("/v02_ng-repeat", {templateUrl: "./views/v02_ng-repeat.html", controller: ""});
 });
