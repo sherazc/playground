99ed01cc8855428cdf3f8516b658d80564a7503a
Sheraz <sheraz@mbp.com>
2017-12-31 00:20:16 -0500

    Implemented ngRoute

---
+++ javascript/angularjs_01/app05_routing_ngRoute/script.js
@@ -0,0 +1,26 @@
+var myApp = angular.module("myApp", ["ngRoute"]);
+myApp.config(function($routeProvider) {
+    
+    $routeProvider
+        .when("/home", {
+            templateUrl: "./partial_views/home.html"
+        })
+        .when("/employee", {
+            templateUrl: "./partial_views/employee.html",
+            controller: "employeeController"
+        })
+        .when("/department", {
+            templateUrl: "./partial_views/department.html",
+            controller: "departmentController"
+            
+        });
+        
+});
+
+myApp.controller("employeeController", function($scope) {
+    $scope.employees = ["Sheraz", "Tariq", "Chaudhry"];
+});
+
+myApp.controller("departmentController", function($scope) {
+    $scope.managments = ["IT", "Managment"];
+});
