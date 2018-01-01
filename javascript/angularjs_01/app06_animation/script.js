6d2bb70829810b865cfa7a0ee6f0c6e7f35469f3
Sheraz <sheraz@mbp.com>
2018-01-01 02:31:37 -0500

    list animate

--- javascript/angularjs_01/app06_animation/script.js
+++ javascript/angularjs_01/app06_animation/script.js
@@ -2,5 +2,6 @@ var myApp = angular.module("myApp", ["ngRoute", "ngAnimate"]);
 myApp.config(function($routeProvider) {
     $routeProvider
         .when("/v01_ng-show", {templateUrl: "./views/v01_ng-show.html"})
-        .when("/v02_ng-repeat", {templateUrl: "./views/v02_ng-repeat.html", controller: ""});
+        .when("/v02_ng-repeat", {templateUrl: "./views/v02_ng-repeat.html", controller: "v02_ng-repeat"})
+        .otherwise("/v01_ng-show", {templateUrl: "./views/v01_ng-show.html"});
 });
--- javascript/angularjs_01/app06_animation/views/v02_ng-repeat.js
+++ javascript/angularjs_01/app06_animation/views/v02_ng-repeat.js
@@ -1,3 +1,13 @@
-<div id="view-container">
-    ng-repeat
-</div>
+var myApp = angular.module("myApp");
+myApp.controller("v02_ng-repeat", function($scope) {
+    $scope.items = ["Item 1", "Item 2", "Item 3"];
+    $scope.newItem = "";
+
+    $scope.add = function() {
+        $scope.items.push($scope.newItem);
+    }
+
+    $scope.remove = function(index) {
+        $scope.items.splice(index, 1);
+    }
+});
