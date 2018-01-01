12bb4fd33fcf58db0d3cdd24fe229388b6a19898
Sheraz <sheraz@mbp.com>
2018-01-01 17:10:14 -0500

    Made code changes

---
+++ sonarqube/sonar01/src/main/java/com/sc/sonar/App.java
@@ -0,0 +1,17 @@
+package com.sc.sonar;
+
+public class App {
+    int aVar;
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    private String aMethod(String str) {
+
+        int num = 10;
+        if (num == 0) {
+            return str;
+        }
+        return str;
+    }
+}
