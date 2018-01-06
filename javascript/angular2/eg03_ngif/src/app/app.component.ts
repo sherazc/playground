dfbb308338013bd7426db68016724a0c28ef7942
Sheraz <sheraz@mbp.com>
2018-01-06 01:32:07 -0500

    did some UI changes

--- javascript/angular2/eg03_ngif/src/app/app.component.ts
+++ javascript/angular2/eg03_ngif/src/app/app.component.ts
@@ -5,12 +5,13 @@ import {Component} from '@angular/core';
   template: `
     <h1>Click Button to toggle</h1>
     <button (click)="toggleShowA()">Show {{showA ? "B" : "A"}}</button>
+    <br/><br/>
     <div *ngIf="showA"
-      style="background: orange;">
+      style="background: orange;padding: 20px;">
       Content A
     </div>
     <div *ngIf="!showA"
-      style="background: cornflowerblue;">
+      style="background: cornflowerblue;padding: 20px;">
       Content B
     </div>
   `,
