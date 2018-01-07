a18964dd45676ba28c3761a268cb44895500775e
Sheraz <sheraz@mbp.com>
2018-01-07 13:48:25 -0500

    Creating another example of combining modules

---
+++ lunch_and_learn/lal_109_angular/app04-combining-modules/src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CalculatorComponent]
+})
+export class ComponentsModule { }
