d30c31aee0754ecd46bdaab711ceb2e7813c6590
Sheraz <sheraz@mbp.com>
2018-01-11 23:58:33 -0500

    WIP routers

---
+++ lunch_and_learn/lal_109_angular/app05-router-parameter/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, // if no path then redirect to /home
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    AboutComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
