1310d843b88502e3e0b4261b072f69cae05b085d
Sheraz <sheraz@mbp.com>
2018-03-10 11:44:44 -0500

    angular multiple environments

---
+++ lunch_and_learn/lal_109_angular/app06-multiple-environments/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('app06-multiple-environments App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
---
+++ lunch_and_learn/lal_109_angular/app06-multiple-environments/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  environmentName: 'Production',
+  apiUrl: 'http://api.example.com/prod'
+};
