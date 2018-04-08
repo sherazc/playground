d216d9252dde709834afe23150b0d21f1c20fdfc
Sheraz <sheraz@mbp.com>
2018-04-08 19:54:05 -0400

    TDF done

--- lunch_and_learn/lal_109_angular/app09-router-sub-router-login-guard-multi-module/src/app/components/login/login.component.ts
+++ lunch_and_learn/lal_109_angular/app09-router-sub-router-login-guard-multi-module/src/app/components/login/login.component.ts
@@ -1,27 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication/authentication.service';
 import {NgForm} from '@angular/forms';
-import {SessionUser} from "../../services/modal/session-user";
-import {ActivatedRoute, Route, Router} from "@angular/router";
+import {SessionUser} from '../../services/modal/session-user';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+
+  loginFailed: boolean;
 
   constructor(private authenticationService: AuthenticationService, private route: Router) {}
 
-  ngOnInit() {}
-
   login(loginForm: NgForm) {
+    this.loginFailed = false;
     const sessionUser: SessionUser = this.authenticationService.login(loginForm.value.userId, loginForm.value.password);
     if (sessionUser) {
       this.route.navigate(['/view-items']);
     } else {
-
+      this.loginFailed = true;
     }
-    // loginForm.values;
   }
 }
