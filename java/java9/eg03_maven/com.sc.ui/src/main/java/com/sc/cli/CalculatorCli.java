2cdacc563463a467b4c52f01cf1aac939cf54be7
Sheraz <sheraz@mbp.com>
2018-04-28 03:17:31 -0400

    Working on java9 maven

--- java/java9/eg03_maven/com.sc.ui/src/main/java/com/sc/cli/CalculatorCli.java
+++ java/java9/eg03_maven/com.sc.ui/src/main/java/com/sc/cli/CalculatorCli.java
@@ -2,12 +2,15 @@ package com.sc.cli;
 
 import com.sc.services.calculator.Calculator;
 
+import java.text.MessageFormat;
+
 public class CalculatorCli {
 
     public static void main(String[] args) {
         if (args.length < 3) {
             System.out.println("Invalid command. e.g.");
             System.out.println(CalculatorCli.class.getName() + " 2 + 3");
+            return;
         }
 
         int numA = Integer.parseInt(args[0]);
@@ -16,6 +19,6 @@ public class CalculatorCli {
 
         int result = new Calculator().calculate(numA, operation, numB);
 
-        System.out.println(numA + " " + operation + " " + numB + " = "+ result);
+        System.out.println(MessageFormat.format("{0} {1} {2} = {3}", numA, operation, numB, result));
     }
 }
