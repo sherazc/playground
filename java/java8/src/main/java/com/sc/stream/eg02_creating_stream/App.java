22141f0241d124f5308f0a15421fb1bd876dd396
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-05-01 17:53:25 -0400

    Stream work

---
+++ java/java8/src/main/java/com/sc/stream/eg02_creating_stream/App.java
@@ -0,0 +1,21 @@
+package com.sc.stream.eg02_creating_stream;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+        // of Method
+        Stream<Integer> streamA = Stream.of(1, 2, 3);
+
+        // From List
+        Stream<Integer> streamB = Arrays.asList(1, 2, 3).stream();
+
+        // From Builder
+        Stream.Builder<Integer> streamCBuilder = Stream.builder();
+        streamCBuilder.accept(1);
+        streamCBuilder.accept(2);
+        streamCBuilder.accept(3);
+        Stream<Integer> streamC = streamCBuilder.build();
+    }
+}
