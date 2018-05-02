950186562ee6183f8a26c56e05f605ac74445db5
Sheraz <sheraz@mbp.com>
2018-05-02 08:09:27 -0400

    Java 8

--- java/java8/src/main/java/com/sc/stream/eg04_stream_methods/App.java
+++ java/java8/src/main/java/com/sc/stream/eg04_stream_methods/App.java
@@ -15,7 +15,33 @@ public class App {
 
         // Concat 2 Streams
         Stream<Integer> concatStream = Stream.concat(Stream.of(1, 2), Stream.of(3, 4));
-        System.out.println("========= Concat Stream ========");
+        System.out.println("========= Stream.concat() ========");
         concatStream.forEach(System.out::println);
+
+        // Count
+        System.out.println("========= Stream.count() ========");
+        System.out.println(Stream.of(2, 4, 6).count());
+
+        // Distinct
+        System.out.println("========= Stream.distinct() ========");
+        Stream.of(2, 2, 3, 3).distinct().forEach(System.out::println);
+
+        // Empty. Make a new stream empty
+        System.out.println("========= Stream.empty() ========");
+        System.out.println(Stream.empty().count());
+
+        // filter
+        System.out.println("========= Stream.filter() ========");
+        Stream.of(2, 4, 6).filter(i -> i > 2).forEach(System.out::println);
+
+
+        // findAny(). Observation: Parallel return random number. Sequential returns first element
+        System.out.println("========= Stream.findAny() ========");
+        System.out.println(Stream.of(2, 4, 6).parallel().findAny().get());
+
+        // findFirst().
+        System.out.println("========= Stream.findFirst() ========");
+        System.out.println(Stream.of(2, 4, 6).findFirst().get());
+
     }
 }
