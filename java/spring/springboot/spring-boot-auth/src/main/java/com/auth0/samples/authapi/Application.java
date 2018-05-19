23a091623721242adeca0a402a90fd114b97069b
Sheraz <sheraz@mbp.com>
2018-05-19 14:40:48 -0400

    Working on SB JWT example

--- java/spring/springboot/spring-boot-auth/src/main/java/com/auth0/samples/authapi/Application.java
+++ java/spring/springboot/spring-boot-auth/src/main/java/com/auth0/samples/authapi/Application.java
@@ -2,11 +2,19 @@ package com.auth0.samples.authapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
