e0061edcb8147fa537ca1efd5601896fd1578e6b
Sheraz <sheraz@mbp.com>
2018-05-24 01:08:58 -0400

    Working on ranges

---
+++ kotlin/eg01/src/kt07_loops.kt
@@ -0,0 +1,21 @@
+import java.util.Random
+
+fun main(args: Array<String>) {
+    // For loop
+    for (i in 1..10) {
+        print(i)
+    }
+    println()
+
+
+
+    for (i in 1..10) {
+        if (i < 4) {
+            continue;
+        } else if (i > 8 ) {
+            break;
+        }
+
+        print(i)
+    }
+}
