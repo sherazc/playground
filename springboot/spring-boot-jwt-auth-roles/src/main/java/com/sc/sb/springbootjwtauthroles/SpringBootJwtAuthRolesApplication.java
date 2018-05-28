18e00a2cf470677dadbcd0183a240f88a914faa2
sheraz <sheraz@sheraz.com>
2018-05-28 16:15:56 -0400

    auto commit

---
+++ springboot/spring-boot-jwt-auth-roles/src/main/java/com/sc/sb/springbootjwtauthroles/SpringBootJwtAuthRolesApplication.java
@@ -0,0 +1,12 @@
+package com.sc.sb.springbootjwtauthroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAuthRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAuthRolesApplication.class, args);
+	}
+}
