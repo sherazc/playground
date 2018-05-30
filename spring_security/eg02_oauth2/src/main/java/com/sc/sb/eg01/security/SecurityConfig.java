a929390a140be1a28077a54146e23bc1cd793480
Sheraz <sheraz@mbp.com>
2018-05-30 05:59:39 -0400

    auto commit

---
+++ spring_security/eg02_oauth2/src/main/java/com/sc/sb/eg01/security/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.sc.sb.eg01.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
