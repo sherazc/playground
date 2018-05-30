6af33a11a2c1d4e7a0bf3b8ee0182826bea50271
Sheraz <sheraz@mbp.com>
2018-05-29 20:25:05 -0400

    auto commit

---
+++ spring_security/eg01/src/main/java/com/sc/sb/eg01/Eg01Application.java
@@ -0,0 +1,12 @@
+package com.sc.sb.eg01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eg01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eg01Application.class, args);
+	}
+}
