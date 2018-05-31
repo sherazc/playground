a43ff178915efa98722fc11ee457546d59999f0a
Sheraz <sheraz@mbp.com>
2018-05-31 02:02:46 -0400

    auto commit

--- spring_security/eg02_oauth2/src/main/java/com/sc/sb/security/SecurityConfig.java
+++ spring_security/eg02_oauth2/src/main/java/com/sc/sb/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.sc.sb.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,10 +14,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic().disable()
-                .formLogin().disable()
-                .anonymous().disable()
-                // .authorizeRequests().anyRequest().denyAll() // this will produce 403 Forbidden
+                 .httpBasic().disable()
+                 .formLogin().disable()
+                 .anonymous().disable()
+                 // .authorizeRequests().anyRequest().denyAll() // this will produce 403 Forbidden
         ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("sheraz").password("password").roles("ADMIN")
+                .and()
+                .withUser("user").password("password").roles("USER")
+        ;
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
