f38527c0ebc1c7acfdc8a2e52701e1023450238e
Sheraz <sheraz@mbp.com>
2018-06-03 02:38:10 -0400

    auto commit

--- spring_security/eg03_oauth2/src/main/java/com/sc/sb/controller/EmployeesController.java
+++ spring_security/eg03_oauth2/src/main/java/com/sc/sb/controller/EmployeesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController("/employee")
+// @RestController("/employee")
 public class EmployeesController {
     private List<Employee> employees;
 
