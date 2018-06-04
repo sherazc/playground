1036088729072a6acc069a36a300c87cab26a682
Sheraz <sheraz@mbp.com>
2018-06-04 02:39:25 -0400

    auto commit

---
+++ spring_security/eg04_oauth2_jwt/src/main/java/com/sc/sb/security/authserver/AuthorizationConfig.java
@@ -0,0 +1,96 @@
+package com.sc.sb.security.authserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+    private int accessTokenValiditySeconds = 10000;
+    private int refreshTokenValiditySeconds = 30000;
+
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthorizationConfig(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    /**
+     * Sets client's authentication manager. This authenticate sets client
+     * user details in the token.
+     *
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(this.authenticationManager);
+    }
+
+    /**
+     * Sets roles for Authentication server that could generate token
+     *
+     * @param oauthServer
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer
+                .tokenKeyAccess("hasAuthority('ROLE_TRUSTED_CLIENT')")
+                .checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
+    }
+
+    /**
+     * Sets Authentication Server's user details and type of tokens they can generate.
+     *
+     * In this example user=trusted-app, password=secret
+     *
+     * @param clients
+     * @throws Exception
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("trusted-app")
+                .authorizedGrantTypes("client_credentials", "password", "refresh_token")
+                .authorities("ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write")
+                .resourceIds(resourceId)
+                .accessTokenValiditySeconds(accessTokenValiditySeconds)
+                .refreshTokenValiditySeconds(refreshTokenValiditySeconds)
+                .secret("secret");
+    }
+
+    /**
+     *
+     * In Spring Security 5 we need to explicitly provide the PasswordEncoder that our
+     * passwords are encoded with. Its not the requirement for the Spring Security 4.2.3.
+     *
+     * Use NoOpPasswordEncoder only for Demo purpose. Not in production.
+     *
+     * https://stackoverflow.com/questions/46999940/spring-boot-passwordencoder-error
+     * https://www.harinathk.com/spring/no-passwordencoder-mapped-id-null/
+     * https://docs.spring.io/spring-security/site/docs/5.0.0.BUILD-SNAPSHOT/reference/htmlsingle/#troubleshooting
+     *
+     * @return
+     */
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ spring_security/eg04_oauth2_jwt/src/main/java/com/sc/sb/security/service/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.sc.sb.security.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Configures web base security specific to HTTP
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                 .httpBasic().disable()
+                 .formLogin().disable()
+                 .anonymous().disable()
+                 // .authorizeRequests().anyRequest().denyAll() // this will produce 403 Forbidden
+        ;
+    }
+
+    /**
+     * Configure AuthenticationManagerBuilder
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN")
+        ;
+    }
+
+    /**
+     * Creates AuthenticationManager bean. Because of @Configuration on the class
+     * and @Bean on this method will set AuthenticationManager in spring ApplicationContext.
+     *
+     * AuthenticationManager is a container of Authentication provider
+     * https://stackoverflow.com/questions/9787409/what-is-the-default-authenticationmanager-in-spring-security-how-does-it-authen
+     *
+     * @return
+     * @throws Exception
+     */
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
