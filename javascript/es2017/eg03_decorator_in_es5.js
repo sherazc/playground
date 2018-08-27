7aca002bb313b22443b1a599b08979e64a321943
Sheraz <sheraz@mbp.com>
2018-08-27 01:42:08 -0400

    auto commit

--- javascript/es2017/eg03_decorator_in_es5.js
+++ javascript/es2017/eg03_decorator_in_es5.js
@@ -1,6 +1,6 @@
 /*
 Javascript function decorators are "Higher Order Function"
-"Higher Order Function" are functions that take in function and return a function
+"Higher Order Function" are functions that take in function or return a function
 A type of function that takes in a
 function and returns another function
 */
