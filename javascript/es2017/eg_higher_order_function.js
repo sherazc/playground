2994dddc44e9cdcce1afabb55db17de031ec490d
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-08-28 17:27:12 -0400

    auto commit

---
+++ javascript/es2017/eg_higher_order_function.js
@@ -0,0 +1,15 @@
+var helloFunction = function(name) {
+    return "Hello " + name;
+};
+
+var bannerFunction = function(fn) {
+    return function(name) {
+        var helloFunctionResult = fn.apply(this, [name]);
+        return "====\n" + helloFunctionResult + "\n====";
+    }
+}
+
+var bannerHelloFunction = bannerFunction(helloFunction);
+var bannerHelloFunctionResult = bannerHelloFunction("Sheraz");
+
+console.log(bannerHelloFunctionResult);
