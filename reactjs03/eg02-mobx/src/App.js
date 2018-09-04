4f251671a6498ec83fbc78bce80aafbc5eddd072
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-09-04 11:44:55 -0400

    auto commit

--- reactjs03/eg02-mobx/src/App.js
+++ reactjs03/eg02-mobx/src/App.js
@@ -11,13 +11,21 @@ class App extends Component {
         this.birdInput = event.target.value;
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("Form submitted", this.birdInput);
+    }
+
+
     render() {
         const {BirdStore} = this.props;
         return (
             <div className="App">
                 <h2>{BirdStore.birdCount}</h2>
-                <input value={this.birdInput} onChange={this.handleBirdInputChange.bind(this)}/>
-                <button onClick={() => console.log(this.birdInput)}>Click</button>
+                <form onSubmit={e => this.handleSubmit(e)}>
+                    <input value={this.birdInput} onChange={this.handleBirdInputChange.bind(this)}/>
+                    <button type="button" onClick={() => console.log("Button clicked", this.birdInput)}>Click</button>
+                </form>
             </div>
         );
     }
