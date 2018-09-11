e01e895af0834b3cee2f14dbe46ef7af1e22652f
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-09-11 16:27:43 -0400

    auto commit

--- reactjs03/eg03-mobx-http-async/src/index.js
+++ reactjs03/eg03-mobx-http-async/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from "mobx-react";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = (
+    <Provider>
+       <App />
+   </Provider>
+);
+
+ReactDOM.render(root, document.getElementById('root'));
 registerServiceWorker();
