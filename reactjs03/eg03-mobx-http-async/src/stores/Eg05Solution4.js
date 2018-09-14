b68f0e98ccd0d8c7ca8a7b009137478e3f5924b3
Sheraz <sheraz@mbp.com>
2018-09-14 08:38:39 -0400

    auto commit

--- reactjs03/eg03-mobx-http-async/src/stores/Eg05Solution4.js
+++ reactjs03/eg03-mobx-http-async/src/stores/Eg05Solution4.js
@@ -1,3 +1,4 @@
+import constant from "./Constants"
 import {observable, configure} from "mobx";
 
 configure({enforceActions: "observed"});
@@ -5,4 +6,10 @@ export default class Eg05Solution4 {
 
     @observable
     users = [];
+
+    loadUser = function*() {
+        const response = yield fetch(constant.usersEndpoint);
+        const userData = yield response.json();
+        //this.users =
+    }
 }
