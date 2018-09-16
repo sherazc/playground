77e5360eee8f73ee4458bb6d2ddd7cb185443253
Sheraz <sheraz@mbp.com>
2018-09-16 10:02:57 -0400

    Created cdb project

---
+++ projects/central_masjid_dashboard/cdb-services/src/main/java/com/sc/cdb/cdbservices/CdbServicesApplication.java
@@ -0,0 +1,12 @@
+package com.sc.cdb.cdbservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdbServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdbServicesApplication.class, args);
+	}
+}
