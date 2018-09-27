254ee5806aac4e87d790756a218b2ec202b0aa3e
Sheraz <sheraz@mbp.com>
2018-09-27 09:01:35 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/webservices/src/main/java/com/sc/cdb/Application.java
+++ projects/central_masjid_dashboard/cdb/webservices/src/main/java/com/sc/cdb/Application.java
@@ -1,7 +1,13 @@
 package com.sc.cdb;
 
+import com.sc.cdb.data.model.Address;
+import com.sc.cdb.data.model.Company;
+import com.sc.cdb.data.model.User;
+import com.sc.cdb.data.repository.CompanyRepository;
+import com.sc.cdb.data.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "com.sc")
 @RestController
-public class Application {
+public class Application implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     private final MyService myService;
+    private final CompanyRepository companyRepository;
+    private final UserRepository userRepository;
 
-    public Application(MyService myService) {
+    public Application(MyService myService,
+                       CompanyRepository companyRepository,
+                       UserRepository userRepository) {
         this.myService = myService;
+        this.companyRepository = companyRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
@@ -32,5 +44,35 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        companyRepository.deleteAll();
+        Address address = new Address(
+                "123 St",
+                "City",
+                "ST",
+                "12345",
+                "1.1",
+                "2.2");
+        Company company = new Company(null, "Company Name", address, "icon");
+        companyRepository.save(company);
+
+
+        // https://www.dailycred.com/article/bcrypt-calculator
+        userRepository.deleteAll();
+        User user = new User(
+                null,
+                company.getId(),
+                "email@email.com",
+                "$2a$04$bl.8O/fUKciwtqKeMg8g.Ox/OGlEqleAgQq9Zuo5/HPPHhM8aLocG",
+                "First",
+                "Last",
+                true,
+                new String[]{"USER"}
+        );
+        userRepository.save(user);
+
     }
 }
