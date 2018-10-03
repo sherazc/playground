73b3dc61c0235002ecd38a314a7fd267f4169127
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-10-03 12:08:23 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/webservices/src/main/java/com/sc/cdb/Application.java
+++ projects/central_masjid_dashboard/cdb/webservices/src/main/java/com/sc/cdb/Application.java
@@ -5,6 +5,7 @@ import com.sc.cdb.data.model.Company;
 import com.sc.cdb.data.model.User;
 import com.sc.cdb.data.repository.CompanyRepository;
 import com.sc.cdb.data.repository.UserRepository;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -46,23 +47,48 @@ public class Application implements CommandLineRunner {
                 "12345",
                 "1.1",
                 "2.2");
-        Company company = new Company(null, "Company Name", address, "icon");
-        companyRepository.save(company);
+        Company company1 = new Company(null, "Company Name 1", address, "icon");
+        companyRepository.save(company1);
+
+        Company company2 = new Company(null, "Company Name 2", address, "icon");
+        companyRepository.save(company2);
 
 
         // https://www.dailycred.com/article/bcrypt-calculator
         userRepository.deleteAll();
         User user = new User(
                 null,
-                company.getId(),
-                "email@email.com",
+                company1.getId(),
+                "user@email.com",
                 "$2a$04$bl.8O/fUKciwtqKeMg8g.Ox/OGlEqleAgQq9Zuo5/HPPHhM8aLocG",
-                "First",
-                "Last",
+                "Sheraz",
+                "User",
                 true,
                 Arrays.asList("USER")
         );
         userRepository.save(user);
+        User user2 = new User(
+                null,
+                company1.getId(),
+                "admin.user@email.com",
+                "$2a$04$bl.8O/fUKciwtqKeMg8g.Ox/OGlEqleAgQq9Zuo5/HPPHhM8aLocG",
+                "Sheraz",
+                "Admin",
+                true,
+                Arrays.asList("ADMIN", "USER")
+        );
+        userRepository.save(user2);
 
+        User user3 = new User(
+                null,
+                company1.getId(),
+                "super.admin.user@email.com",
+                "$2a$04$bl.8O/fUKciwtqKeMg8g.Ox/OGlEqleAgQq9Zuo5/HPPHhM8aLocG",
+                "Sheraz",
+                "Super Admin",
+                true,
+                Arrays.asList("SUPER_ADMIN", "USER")
+        );
+        userRepository.save(user3);
     }
 }
