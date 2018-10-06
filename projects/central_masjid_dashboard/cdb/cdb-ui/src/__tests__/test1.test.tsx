9af5f03d747df3872dbd30b8c42d81522a0284c5
Sheraz <sheraz@mbp.com>
2018-10-06 18:40:58 -0400

    auto commit

---
+++ projects/central_masjid_dashboard/cdb/cdb-ui/src/__tests__/test1.test.tsx
@@ -0,0 +1,11 @@
+console.log("################ Testing tests");
+
+/*
+
+it('sums numbers', () => {
+    expect(3).toEqual(3);
+    expect(4).toEqual(4);
+    console.log("################ Testing tests");
+});
+
+*/
