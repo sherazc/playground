e77bb5f1007c22029a0449beca5a63fde9e687ee
Sheraz <sheraz@mbp.com>
2018-10-11 00:41:39 -0400

    auto commit

--- reactjs03/eg04-redux-axios-router-spring/ui/src/App.js
+++ reactjs03/eg04-redux-axios-router-spring/ui/src/App.js
@@ -5,22 +5,25 @@ import Home from "./components/Home";
 import AllUsers from "./components/AllUsers";
 import AddUser from "./components/AddUser";
 import Counter from "./components/Counter";
+import {Provider} from 'react-redux';
+import store from "./store/store";
 
 class App extends Component {
     render() {
         return (
-
-            <BrowserRouter>
-                <div>
-                    <Navigation/>
-                    <Switch>
-                        <Route path='/' component={Home} exact/>
-                        <Route path='/all-users' component={AllUsers}/>
-                        <Route path='/add-user' component={AddUser}/>
-                        <Route path='/counter' component={Counter}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div>
+                        <Navigation/>
+                        <Switch>
+                            <Route path='/' component={Home} exact/>
+                            <Route path='/all-users' component={AllUsers}/>
+                            <Route path='/add-user' component={AddUser}/>
+                            <Route path='/counter' component={Counter}/>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </Provider>
         );
     }
 }
--- reactjs03/eg04-redux-axios-router-spring/ui/src/components/Counter.jsx
+++ reactjs03/eg04-redux-axios-router-spring/ui/src/components/Counter.jsx
@@ -1,13 +1,27 @@
 import React, {Component} from "react";
 
 export default class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.add = this.add.bind(this);
+        this.subtract = this.subtract.bind(this);
+    }
+
+    add() {
+        console.log("add ", new Date());
+    }
+
+    subtract() {
+        console.log("subtract ", new Date());
+    }
+
     render() {
         return (
             <div>
                 <h1>Counter</h1>
                 <h3>8</h3>
-                <button type="button">+</button>
-                <button type="button">-</button>
+                <button type="button" onClick={this.add}>+</button>
+                <button type="button" onClick={this.subtract}>-</button>
             </div>
         );
     }
