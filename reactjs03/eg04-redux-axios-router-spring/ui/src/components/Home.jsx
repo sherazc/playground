f5beaad877eb5ef5567949421f783299ac4e067a
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-10-11 14:53:12 -0400

    Added counter to home page

--- reactjs03/eg04-redux-axios-router-spring/ui/src/components/Home.jsx
+++ reactjs03/eg04-redux-axios-router-spring/ui/src/components/Home.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
 
-export default class Home extends Component{
+class Home extends Component{
     render() {
         return (
             <div>
                 <h1>Home</h1>
+                <h3>Counter</h3>
+                {this.props.count}
             </div>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        count: state.countReducer.count
+    };
+};
+
+export default connect(mapStateToProps)(Home);
