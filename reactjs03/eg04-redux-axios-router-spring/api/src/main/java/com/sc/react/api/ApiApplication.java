b085676a5d97e6cd91807912ad4e51c60815d1ba
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-10-11 16:14:06 -0400

    Trying to configure CORS

--- reactjs03/eg04-redux-axios-router-spring/api/src/main/java/com/sc/react/api/ApiApplication.java
+++ reactjs03/eg04-redux-axios-router-spring/api/src/main/java/com/sc/react/api/ApiApplication.java
@@ -2,11 +2,34 @@ package com.sc.react.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Collections;
+
+@Configuration
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+		corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+		corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.applyPermitDefaultValues();
+
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return urlBasedCorsConfigurationSource;
 	}
 }
