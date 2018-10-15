1ea286d97c1e91d252c94514b14709d43c0043f1
Sheraz <sheraz@mbp.com>
2018-10-14 23:56:38 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/ui/src/components/Register.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/Register.jsx
@@ -32,20 +32,79 @@ class Register extends Component {
 
     render() {
         return (
-            <div>
-                <h3>Register</h3>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1"
-                               aria-describedby="emailHelp" placeholder="Enter email"/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with
-                            anyone else.
-                        </small>
-                    </div>
+            <div className="row">
 
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
+                <div className="col-sm-6">
+                    <h3>Register</h3>
+                    <form>
+
+
+                        <div className="form-group">
+                            <label htmlFor="companyName">Masjid Name</label>
+                            <input type="text" className="form-control" name="companyName" id="companyName"
+                                   placeholder="Masjid Name"/>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="street">Street</label>
+                            <input type="text" className="form-control" name="street" id="street"
+                                   placeholder="Street"/>
+                        </div>
+
+
+                        <div className="form-group">
+                            <label htmlFor="city">City</label>
+                            <input type="text" className="form-control" name="city" id="city"
+                                   placeholder="City"/>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="state">State</label>
+                            <input type="text" className="form-control" name="state" id="state"
+                                   placeholder="State"/>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="zip">Zip</label>
+                            <input type="text" className="form-control" name="zip" id="zip"
+                                   placeholder="Zip"/>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="icon">Icon</label>
+                            <input type="text" className="form-control" name="icon" id="icon"
+                                   placeholder="Icon"/>
+                        </div>
+
+                        <hr/>
+
+                        <div className="form-group">
+                            <label htmlFor="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp" placeholder="Enter email"/>
+                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with
+                                anyone else.
+                            </small>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" name="password" id="password"
+                                   placeholder="Password"/>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="firstName">First Name</label>
+                            <input type="text" className="form-control" name="firstName" id="firstName"
+                                   placeholder="First Name"/>
+                        </div>
+
+
+
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+
             </div>
         );
     }
