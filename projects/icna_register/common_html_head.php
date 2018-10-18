c0877564adbb6c80bdeebc0040c0d74b269cf7ec
Sheraz <sheraz.chaudhry@cioxhealth.com>
2018-10-18 15:16:27 -0400

    auto commit

--- projects/icna_register/common_html_head.php
+++ projects/icna_register/common_html_head.php
@@ -22,8 +22,10 @@
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/js/tempusdominus-bootstrap-4.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/css/tempusdominus-bootstrap-4.min.css" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
 
 <script>
     // Date time picker
--- projects/icna_register/view-events.php
+++ projects/icna_register/view-events.php
@@ -10,10 +10,98 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
-            View Events
+            <h3>View Event</h3>
+<?php
+$stmt = $db->prepare("SELECT * FROM event_tracker");
+// $stmt->bind_param("s", $_POST['name']);
+$stmt->execute();
+$result = $stmt->get_result();
+if($result->num_rows === 0) {
+    echo "No events found";
+} else {
+    ?>
+    <table id="event_grid">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Event Name</th>
+                <th>Chapter Region</th>
+                <th>Event Start</th>
+                <th>Event End</th>
+                <th>Category</th>
+                <th>Category Type</th>
+                <th>Attendance</th>
+                <th>Servings</th>
+                <th>Location Types</th>
+                <th>Street</th>
+                <th>City</th>
+                <th>State</th>
+                <th>Zip</th>
+                <th>Attendees</th>
+                <th>Event In-Charge</th>
+                <th>Speakers</th>
+                <th>Expense</th>
+                <th>Paid By</th>
+                <th>Income</th>
+                <th>Donation</th>
+                <th>Workers</th>
+                <th>Volunteers</th>
+                <th>Rating</th>
+                <th>Issues</th>
+                <th>Comments</th>
+            </tr>
+        </thead>
+        <tbody>
+    <?php
+    while($row = $result->fetch_assoc()) {
+        ?>
+        <tr>
+            <td><?php echo $row['id']?></td>
+            <td><?php echo $row['event_name']?></td>
+            <td><?php echo $row['chapter_region']?></td>
+            <td><?php echo $row['event_start_date']?></td>
+            <td><?php echo $row['event_end_date']?></td>
+            <td><?php echo $row['category']?></td>
+            <td><?php echo $row['category_type']?></td>
+            <td><?php echo $row['attendance']?></td>
+            <td><?php echo $row['servings']?></td>
+            <td><?php echo $row['location_types']?></td>
+            <td><?php echo $row['street']?></td>
+            <td><?php echo $row['city']?></td>
+            <td><?php echo $row['state']?></td>
+            <td><?php echo $row['zip']?></td>
+            <td><?php echo $row['attendees']?></td>
+            <td><?php echo $row['event_in_charge']?></td>
+            <td><?php echo $row['speakers']?></td>
+            <td><?php echo $row['expense']?></td>
+            <td><?php echo $row['paid_by']?></td>
+            <td><?php echo $row['income']?></td>
+            <td><?php echo $row['donation']?></td>
+            <td><?php echo $row['workers']?></td>
+            <td><?php echo $row['volunteers']?></td>
+            <td><?php echo $row['rating']?></td>
+            <td><?php echo $row['issues']?></td>
+            <td><?php echo $row['comments']?></td>
+        </tr>
+        <?php
+    }
+    ?>
+        </tbody>
+    </table>
+    <?php
+}
+?>
         </div>
     </div>
 </div>
+<script>
+    $(document).ready( function () {
+        $('#event_grid').DataTable();
+    });
+</script>
+<?php
+$stmt->close();
+?>
 <?php include_once 'footer.php'?>
 </body>
 </html>
