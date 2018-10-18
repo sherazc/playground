c7360b7b1620910bb4beed06e6affadc3c872237
Sheraz <sheraz@mbp.com>
2018-10-18 01:57:14 -0400

    auto commit

--- projects/icna_register/header.php
+++ projects/icna_register/header.php
@@ -2,21 +2,67 @@
 require_once "db_connect.php";
 require_once "utilities.php";
 ?>
-<div class="container">
-    <div style="background-color: #518bbf; color: #dce0ef; font-size: 40px; padding: 30px; margin-bottom: 20px">
-        <div style="display: inline">
-            EVENT TRACKER
+<style>
+    .mainNav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    .mainNav ul li {
+        float: left;
+    }
+
+    .mainNav ul li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    /* Change the link color to #111 (black) on hover */
+    .mainNav ul li a:hover {
+        background-color: #111;
+    }
+</style>
+<div class="container" style="margin-bottom: 20px;">
+    <div class="row">
+        <div class="col-lg-12">
+            <div style="background-color: #518bbf; color: #dce0ef; font-size: 40px; padding: 30px;">
+                <div style="display: inline; word-spacing: 20px; letter-spacing: 5px; font-weight: 200">
+                    EVENT TRACKER
+                </div>
+                <?php
+                if (isLoggedIn()) {
+                    ?>
+                    <div style="display: inline; float: right; ">
+                        <a href="logout.php" style="border: 0">
+                            <img src="images/baseline-power_settings_new-24px.svg" alt="logout" style="width: 40px; border: 0">
+                        </a>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    <?php
+    if (isLoggedIn()) {
+        ?>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="mainNav">
+                    <ul>
+                        <li><a href="even-tracker.php">Create Event</a></li>
+                        <li><a href="view-events.php">View All Event</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
         <?php
-        if (isLoggedIn()) {
-            ?>
-            <div style="display: inline; float: right; ">
-                <a href="logout.php" style="border: 0">
-                    <img src="images/baseline-power_settings_new-24px.svg" alt="logout" style="width: 40px; border: 0">
-                </a>
-            </div>
-            <?php
-        }
-        ?>
-    </div>
+    }
+    ?>
 </div>
---
+++ projects/icna_register/view-events.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Event Tracker</title>
+    <?php include_once 'common_html_head.php' ?>
+    <?php include_once 'authenticate.php' ?>
+</head>
+<body>
+<?php include_once 'header.php'?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            View Events
+        </div>
+    </div>
+</div>
+<?php include_once 'footer.php'?>
+</body>
+</html>
