73ce7771c5d0733c8df4ff037bba2aee659cb2a1
Sheraz <sheraz@mbp.com>
2018-10-21 10:53:03 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/ui/src/components/Login.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {showAlert, ALERT_SUCCESS, ALERT_ERROR, ALERT_WARN} from "../store/action/alert-actions";
+import {showLoading, hideLoading} from "../store/action/loading-action";
 
 class Login extends Component {
 
@@ -9,6 +10,7 @@ class Login extends Component {
         this.showSuccessLoginAlert = this.showSuccessLoginAlert.bind(this);
         this.showWarnLoginAlert = this.showWarnLoginAlert.bind(this);
         this.showErrorLoginAlert = this.showErrorLoginAlert.bind(this);
+        this.showHideLoading = this.showHideLoading.bind(this);
     }
 
     showSuccessLoginAlert() {
@@ -23,6 +25,13 @@ class Login extends Component {
         this.props.showAlert(ALERT_ERROR, "Some Error message");
     }
 
+    showHideLoading() {
+        this.props.showLoading();
+        const self = this;
+        setTimeout(() => {self.props.hideLoading()}, 2000)
+
+    }
+
     render() {
         return (
             <div>
@@ -33,12 +42,14 @@ class Login extends Component {
                 <button type="button" onClick={this.showWarnLoginAlert}>Show Warn Alert</button>
                 <br/>
                 <button type="button" onClick={this.showErrorLoginAlert}>Show Error Alert</button>
+                <br/>
+                <button type="button" onClick={this.showHideLoading}>Show Loading</button>
             </div>
         );
     }
 }
 
-const actions = {showAlert};
+const actions = {showAlert, showLoading, hideLoading};
 
 
 export default connect(null, actions)(Login);
