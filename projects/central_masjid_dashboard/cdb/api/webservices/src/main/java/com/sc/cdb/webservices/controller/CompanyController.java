d8e47b3617ae6b1b91386820eded60030240138c
Sheraz <sheraz@mbp.com>
2018-12-05 17:57:56 -0500

    auto commit

--- projects/central_masjid_dashboard/cdb/api/webservices/src/main/java/com/sc/cdb/webservices/controller/CompanyController.java
+++ projects/central_masjid_dashboard/cdb/api/webservices/src/main/java/com/sc/cdb/webservices/controller/CompanyController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,7 @@ import java.text.MessageFormat;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/auth/company")
+@RequestMapping("/api/auth/companies")
 public class CompanyController {
     private static final Logger LOG = LoggerFactory.getLogger(CompanyController.class);
 
@@ -100,5 +101,11 @@ public class CompanyController {
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
+    }
+
+    @DeleteMapping("{id}")
+    // @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> delete(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Delete method not implemented yet. ID " + id);
     }
 }
