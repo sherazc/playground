4c09df78a49ccf7b8365536a176910c4eaedb293
Sheraz <sheraz@mbp.com>
2018-12-29 12:01:43 -0500

    Reset and update credentials API done

--- projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/UserCredentialServiceImpl.java
+++ projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/UserCredentialServiceImpl.java
@@ -6,6 +6,7 @@ import com.sc.cdb.services.model.ServiceResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
@@ -16,9 +17,11 @@ public class UserCredentialServiceImpl implements UserCredentialService {
     private static final Logger LOG = LoggerFactory.getLogger(UserCredentialServiceImpl.class);
 
     private UserService userService;
+    private PasswordEncoder passwordEncoder;
 
-    public UserCredentialServiceImpl(UserService userService) {
+    public UserCredentialServiceImpl(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -33,25 +36,48 @@ public class UserCredentialServiceImpl implements UserCredentialService {
             return responseBuilder.target(false).successful(false).message(errorMessage).build();
         }
 
+        return updateUserPassword(credential, responseBuilder, userOptional.get());
+    }
+
+    @Override
+    public ServiceResponse<Boolean> update(Credential credential) {
+        LOG.debug("Updating password of {}", credential.getEmail());
+        ServiceResponse.ServiceResponseBuilder<Boolean> responseBuilder = ServiceResponse.builder();
+        if (StringUtils.isBlank(credential.getExistingCredential())) {
+            String errorMessage = "Failed to update user credentials. Existing password not provided.";
+            LOG.error(errorMessage);
+            return responseBuilder.target(false).successful(false).message(errorMessage).build();
+        }
+
+        Optional<User> userOptional = userService.findUserByEmail(credential.getEmail());
+        if (!userOptional.isPresent()) {
+            String errorMessage = MessageFormat.format("Can not find user by email {0}.", credential.getEmail());
+            LOG.error(errorMessage);
+            return responseBuilder.target(false).successful(false).message(errorMessage).build();
+        }
         User user = userOptional.get();
+
+        if (!passwordEncoder.matches(credential.getExistingCredential(), user.getPassword())) {
+            String errorMessage = "Failed to update user credentials. Existing password do not match.";
+            LOG.error(errorMessage);
+            return responseBuilder.target(false).successful(false).message(errorMessage).build();
+        }
+        return updateUserPassword(credential, responseBuilder, user);
+    }
+
+    private ServiceResponse<Boolean> updateUserPassword(Credential credential, ServiceResponse.ServiceResponseBuilder<Boolean> responseBuilder, User user) {
         user.setPassword(credential.getNewCredential());
         ServiceResponse<User> userServiceResponse = userService.createOrUpdate(user);
         if (userServiceResponse.isSuccessful()) {
-            String message = "Successfully updated user credentials.";
+            String message = "Successfully updated user.";
             LOG.debug(message);
             return responseBuilder.message(message).successful(true).target(true).build();
         } else {
-            String errorMessage = "Failed to updated user credentials. " + userServiceResponse.getMessage();
+            String errorMessage = "Failed to updated user. " + userServiceResponse.getMessage();
             return responseBuilder
                     .fieldErrors(userServiceResponse.getFieldErrors())
                     .message(errorMessage)
                     .build();
         }
-    }
-
-    @Override
-    public ServiceResponse<Boolean> update(Credential credential) {
-        System.out.println(credential);
-        return null;
     }
 }
