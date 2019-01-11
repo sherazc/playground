66e21a6fc2c01780f584d786532f79e4509b6f6f
Sheraz <sheraz@mbp.com>
2019-01-11 01:29:28 -0500

    aws ssl configuration

--- projects/central_masjid_dashboard/cdb/api/webservices/src/main/java/com/sc/cdb/webservices/auth/security/HttpsConfiguration.java
+++ projects/central_masjid_dashboard/cdb/api/webservices/src/main/java/com/sc/cdb/webservices/auth/security/HttpsConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"prod"})
+@Profile({"prod", "prodlocal"})
 public class HttpsConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(HttpsConfiguration.class);
 
