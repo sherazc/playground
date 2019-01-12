9e5e5626da7ba29941b2d535e6e5f0633fe5c57c
Sheraz <sheraz@mbp.com>
2019-01-12 12:42:38 -0500

    Added Elastic IP instead of EC2 domain

--- projects/central_masjid_dashboard/cdb/deploy-cdb.sh
+++ projects/central_masjid_dashboard/cdb/deploy-cdb.sh
@@ -4,4 +4,7 @@
 
 scp -i /Users/sheraz/.ssh/id_rsa -r \
     ./api/webservices/target/cdb.jar \
-    ubuntu@ec2-54-165-184-232.compute-1.amazonaws.com:/home/ubuntu/cdb/app
+    ubuntu@54.165.184.232:/home/ubuntu/cdb/app
+
+echo ====[ Successfully cleaned, built and deployed on EC2. ]====
+echo ====[ Now Restart application on AWS. ]====
