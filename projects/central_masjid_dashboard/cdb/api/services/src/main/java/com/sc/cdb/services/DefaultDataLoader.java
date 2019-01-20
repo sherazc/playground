966f0ec9af2e18adf3c9f009f42ea9b6501c2073
Sheraz <sheraz@mbp.com>
2019-01-19 23:44:28 -0500

    Inserting init data

--- projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/DefaultDataLoader.java
+++ projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/DefaultDataLoader.java
@@ -7,21 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 @Service
 public class DefaultDataLoader {
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultDataLoader.class);
-    private static final String INIT_DATA_dir = "init-data";
 
     private final MongoTemplate mongoTemplate;
 
@@ -30,18 +23,25 @@ public class DefaultDataLoader {
     }
 
     public void load() {
-        List<String> dataResources = listClasspathDirectory(INIT_DATA_dir);
-        dataResources.forEach(this::updateInitData);
+        Map<String, String> collectionResources = getCollectionResources();
+        collectionResources.forEach(this::updateInitData);
+        LOGGER.info("Database initialized!");
     }
 
-    private void updateInitData(String resourceName) {
-        try {
-            LOGGER.debug("Updating data with {}", resourceName);
-            Path resourcePath = Paths.get(ClassLoader.getSystemResource(resourceName).toURI());
-            String collectionName = getCollectionName(resourcePath);
+    private Map<String, String> getCollectionResources() {
+        String initDataDirectory = "/init-data/";
+        Map<String, String> collectionResources = new HashMap<>();
+        collectionResources.put("dashboard", initDataDirectory + "dashboard.json");
+        collectionResources.put("company", initDataDirectory + "company.json");
+        collectionResources.put("user", initDataDirectory + "user.json");
+        return collectionResources;
+    }
 
-            String fileContent = new String(Files.readAllBytes(resourcePath));
+    private void updateInitData(String collectionName, String resourceName) {
+        LOGGER.debug("Initializing collection {} with data in file {}", collectionName, resourceName);
 
+        try (InputStream inputStream = getClass().getResource(resourceName).openStream()) {
+            String fileContent = readStreamContent(inputStream);
             BsonArray bsonArray = BsonArray.parse(fileContent);
             mongoTemplate.dropCollection(collectionName);
 
@@ -50,38 +50,13 @@ public class DefaultDataLoader {
                     mongoTemplate.save(((BsonDocument) bsonValue).toJson(), collectionName);
                 }
             });
-
         } catch (Exception e) {
             LOGGER.error("Error reading database init file: " + resourceName, e);
         }
     }
 
-    private String getCollectionName(Path resourcePath) {
-        File resourceFile = resourcePath.toFile();
-        String fileName = resourceFile.getName();
-        return fileName.substring(0, fileName.lastIndexOf('.'));
-    }
-
-    private List<String> listClasspathDirectory(String classpathDirectory) {
-        List<String> resourceNames = new ArrayList<>();
-        try (InputStream inputStream = getResourceAsStream(classpathDirectory);
-             BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-
-            String resource;
-
-            while ((resource = br.readLine()) != null) {
-                resourceNames.add(classpathDirectory + "/" + resource);
-            }
-        } catch (IOException e) {
-            String errorMessage = "Error occurred loading file names in " + classpathDirectory;
-            LOGGER.error(errorMessage, e);
-            throw new RuntimeException(errorMessage, e);
-        }
-        return resourceNames;
-    }
-
-    private InputStream getResourceAsStream(String resource) {
-        final InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(resource);
-        return inputStream == null ? getClass().getResourceAsStream(resource) : inputStream;
+    private String readStreamContent(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
     }
 }
