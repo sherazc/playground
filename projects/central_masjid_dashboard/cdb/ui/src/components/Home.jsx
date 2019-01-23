c3d4a4c6eac97ef5b029e1d081d8523ad3271e76
Sheraz <sheraz@mbp.com>
2019-01-23 07:59:40 -0500

    Central control URL

--- projects/central_masjid_dashboard/cdb/ui/src/components/Home.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/Home.jsx
@@ -1,12 +1,37 @@
 import React, {Component} from "react";
+import axios from "axios";
+
+const baseUrl = process.env.REACT_APP_API_BASE_PATH;
 
 class Home extends Component {
+
+    state = this.createInitialState();
+
+    createInitialState() {
+        return {
+            companyUrls: [],
+            selectedCompanyUrl: ""
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`${baseUrl}/api/auth/companies/url`).then(
+            response => this.setState({companyUrls: response.data})
+        );
+    }
 
     render() {
         return (
             <div>
                 <h3>Home</h3>
-                Home page
+                Select Dashboard
+                <select className="form-control">
+                    {this.state.companyUrls.map((companyUrl, index) => {
+                        return (<option key={index}>{companyUrl.name}</option>);
+                    })}
+                </select>
+
+
             </div>
         );
     }
