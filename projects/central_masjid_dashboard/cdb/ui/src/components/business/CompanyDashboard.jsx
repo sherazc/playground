7a32f8c5f80b2e408bd48f6ff7ebc953ff7d816d
Sheraz <sheraz@mbp.com>
2019-01-27 00:57:39 -0500

    company dashboard

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard.jsx
@@ -1,12 +1,39 @@
 import React, {Component} from "react";
 import {getPathParamFromProps} from "../../services/utilities";
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import {withStyles} from '@material-ui/core/styles';
-import ContainerGridLayout from "../layout/ContainerGridLayout";
 
 
-const styles = theme => ({});
+const styles = theme => {
+    const main = {height: 300, textAlign: "center", color: "white"};
+    const mainCenter = {...main,
+        backgroundColor: "#c7e7b5",
+        backgroundImage: `url(${process.env.PUBLIC_URL}/images/center_box.svg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%"
+    };
+    const mainSide = {
+        ...main
+    };
+
+    const sideBox = {
+        width: 115, height: 150,
+        backgroundColor: "#ddb167",
+        position: "relative",
+        top: 10,
+        marginLeft: "auto",
+        marginRight: "auto",
+        backgroundImage: `url(${process.env.PUBLIC_URL}/images/side_box.svg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%"
+    };
+
+    return ({
+        mainSide, mainCenter, sideBox,
+        mainLeftSide: {...mainSide, backgroundColor: "#a7d9d3"},
+        mainRightSide: {...mainSide, backgroundColor: "#e5afd3"}
+    })
+};
 
 
 class CompanyDashboard extends Component {
@@ -22,21 +49,27 @@ class CompanyDashboard extends Component {
     }
 
     render() {
+        const {classes} = this.props;
+        const xsBreakPoint = 12;
+        const smBreakPoint = 12;
+        const mdBreakPoint = 4;
+
         return (
-            <ContainerGridLayout>
-                <Grid item>
-                    <h3>Company Dashboard</h3>
-                    <div>
-
-                        This is company dashboard. {this.state.companyDashboardUrl}
-
-                        <Button variant="contained" color="primary">
-                            Hello World
-                        </Button>
-
+            <Grid container justify="center">
+                <Grid item xs={xsBreakPoint} sm={smBreakPoint} md={mdBreakPoint} className={classes.mainLeftSide}>
+                    <div className={classes.sideBox}>
+                    Left side
                     </div>
                 </Grid>
-            </ContainerGridLayout>
+                <Grid item xs={xsBreakPoint} sm={smBreakPoint} md={mdBreakPoint} className={classes.mainCenter}>
+                    Center
+                </Grid>
+                <Grid item xs={xsBreakPoint} sm={smBreakPoint} md={mdBreakPoint} className={classes.mainRightSide}>
+                    <div className={classes.sideBox}>
+                        Right side
+                    </div>
+                </Grid>
+            </Grid>
         );
     }
 }
