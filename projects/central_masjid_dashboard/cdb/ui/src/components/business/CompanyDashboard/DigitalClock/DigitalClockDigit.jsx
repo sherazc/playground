94b8eb710343b481c39c5043ed545218c0c6e0dc
Sheraz <sheraz@mbp.com>
2019-02-09 04:30:54 -0500

    auto commit

---
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/DigitalClock/DigitalClockDigit.jsx
@@ -0,0 +1,96 @@
+import React, {Component} from "react";
+
+
+const styles = {
+    digitContainer : {
+        height: "30px",
+        width: "22px",
+        float: "left",
+        backgroundColor: "green",
+        overflow: "hidden"
+    }
+};
+
+const digitImageBasePath = `${process.env.PUBLIC_URL}/images/clock_digital`;
+
+class DigitalClockDigit extends Component {
+    constructor(props) {
+        super(props);
+        const numberStyle = {
+            width: "22px",
+            height: "30px",
+            position: "relative",
+            top: '0px',
+            backgroundSize: "100% 100%",
+        };
+
+        this.state = {
+            previousDigit: 0,
+            numberPreviousStyle: {...numberStyle},
+            numberCurrentStyle: {...numberStyle},
+        };
+    }
+
+
+    componentDidMount() {
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const previousDigit = prevProps.digit;
+        const currentDigit = this.props.digit;
+
+        if (previousDigit !== currentDigit) {
+            const slideStyle = {
+                transition: "top 1000ms ease-out 0s",
+                top: "-30px",
+            };
+            this.flipNumber(previousDigit, currentDigit, slideStyle);
+            setTimeout(() => {
+                const slideStyle = {
+                    transition: "",
+                    top: "0",
+                };
+                this.flipNumber(currentDigit, previousDigit, slideStyle);
+            }, 1000);
+        }
+    }
+
+    flipNumber(previousDigit, currentDigit, slideStyle) {
+
+        const numberPreviousStyle = {
+            ...this.state.numberPreviousStyle,
+            ...slideStyle,
+            backgroundImage: `url(${digitImageBasePath}/${previousDigit}.svg)`
+        };
+
+        const numberCurrentStyle = {
+            ...this.state.numberCurrentStyle,
+            ...slideStyle,
+            backgroundImage: `url(${digitImageBasePath}/${currentDigit}.svg)`
+        };
+
+        this.setState({numberPreviousStyle, numberCurrentStyle});
+    }
+
+    render() {
+
+        return(
+            <div style={styles.digitContainer}>
+                <div style={this.state.numberPreviousStyle}></div>
+                <div style={this.state.numberCurrentStyle}></div>
+            </div>
+
+            /*
+            <div>
+                previousDigit = {this.state.previousDigit}
+                <br/>
+                currentDigit = {this.props.digit}
+
+            </div>
+            */
+
+        );
+    }
+}
+
+export default DigitalClockDigit;
--- projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/DigitalClock/index.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/DigitalClock/index.jsx
@@ -1,26 +1,54 @@
 import React, {Component} from "react";
 import {withStyles} from '@material-ui/core/styles';
+import DigitalClockDigit from "./DigitalClockDigit";
 
 const addUnit = (num) => {
     return num + "vw";
 };
 
 const styles = theme => {
-    return {};
+
+    const dClockContainer = {
+        width: "174px",
+        height: "66px",
+        background: `url(${process.env.PUBLIC_URL}/images/clock_digital/bg1.svg) no-repeat`,
+        backgroundSize: "100% 100%",
+        backgroundColor: "red",
+        marginTop: "30px",
+        left: "30px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        zIndex: "5",
+        position: "absolute",
+    };
+
+    return {
+        dClockContainer,
+    };
 };
 
 class DigitalClock extends Component {
+    state = {
+        secondDigit: 0
+    };
+
     componentDidMount() {
     }
 
     componentWillUnmount() {
     }
 
+    addSecond() {
+        this.setState({secondDigit: ++this.state.secondDigit});
+    }
+
     render() {
         const {classes} = this.props;
         return (
-            <div>
-                Digital clock
+            <div className={classes.dClockContainer}>
+                <DigitalClockDigit digit={Math.floor(this.state.secondDigit % 10)}/>
+                <br/>
+                <button onClick={this.addSecond.bind(this)}>Add Digit</button>
             </div>
         );
     }
