86d3f9afde973cc7c6bf66c8cd7ff70e86b6177d
Sheraz <sheraz@mbp.com>
2019-03-14 18:49:18 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/api/data/src/main/java/com/sc/cdb/data/model/auth/UserCompany.java
+++ projects/central_masjid_dashboard/cdb/api/data/src/main/java/com/sc/cdb/data/model/auth/UserCompany.java
@@ -1,8 +1,10 @@
 package com.sc.cdb.data.model.auth;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class UserCompany extends User {
     private Company company;
 }
