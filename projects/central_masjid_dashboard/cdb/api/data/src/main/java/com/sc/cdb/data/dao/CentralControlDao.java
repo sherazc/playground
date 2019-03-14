7f1f93e17c10024c51668753972c742820d64933
Sheraz <sheraz@mbp.com>
2019-03-14 17:51:26 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/api/data/src/main/java/com/sc/cdb/data/dao/CentralControlDao.java
+++ projects/central_masjid_dashboard/cdb/api/data/src/main/java/com/sc/cdb/data/dao/CentralControlDao.java
@@ -1,46 +1,8 @@
 package com.sc.cdb.data.dao;
 
-import com.mongodb.client.result.UpdateResult;
-import com.sc.cdb.data.model.cc.CentralControl;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.stereotype.Repository;
+import com.sc.cdb.data.model.cc.PrayerConfig;
 
-import java.util.List;
-
-@Repository
-public class CentralControlDao {
-
-    private MongoTemplate mongoTemplate;
-
-    public CentralControlDao(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
-    }
-
-    public void save(CentralControl centralControl) {
-        mongoTemplate.save(centralControl);
-    }
-
-    public void dropCollection() {
-        mongoTemplate.dropCollection(CentralControl.class);
-    }
-
-    // TODO remove below method this is just for testing updating complex object
-    // https://docs.mongodb.com/manual/reference/method/db.collection.findAndModify/#db.collection.findAndModify
-    // https://stackoverflow.com/questions/47699646/updating-replacing-a-deeply-nested-object-in-mongodb-with-spring-data-mongodb
-    public void updateComplexObject() {
-        // Query reference
-        // https://www.baeldung.com/queries-in-spring-data-mongodb
-        Query query = new Query(Criteria.where("companyId").is("company1"));
-        Update update = new Update().set("jummahs.0.khateeb", "Kateeb 1 Changed again");
-        UpdateResult updateResult = mongoTemplate.updateMulti(query, update, CentralControl.class);
-        System.out.println(updateResult);
-        List<CentralControl> centralControls = mongoTemplate.find(new Query(Criteria.where("companyId").is("company1")), CentralControl.class);
-        System.out.println(centralControls);
-
-        List<CentralControl> centralControls1 = mongoTemplate.findAll(CentralControl.class);
-        centralControls1.forEach(System.out::println);
-    }
+public interface CentralControlDao {
+    boolean isCentralControlExists(String companyId);
+    boolean updatePrayerConfig(String companyId, PrayerConfig prayerConfig);
 }
---
+++ projects/central_masjid_dashboard/cdb/api/data/src/main/java/com/sc/cdb/data/dao/CentralControlDaoImpl.java
@@ -0,0 +1,63 @@
+package com.sc.cdb.data.dao;
+
+import com.mongodb.client.result.UpdateResult;
+import com.sc.cdb.data.model.cc.CentralControl;
+import com.sc.cdb.data.model.cc.PrayerConfig;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CentralControlDaoImpl implements CentralControlDao {
+
+    private MongoTemplate mongoTemplate;
+
+    public CentralControlDaoImpl(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public boolean isCentralControlExists(String companyId) {
+        return this.mongoTemplate.exists(
+                new Query(Criteria.where("companyId").is(companyId)),
+                CentralControl.class);
+    }
+
+    @Override
+    public boolean updatePrayerConfig(String companyId, PrayerConfig prayerConfig) {
+        Query query = new Query(Criteria.where("companyId").is(companyId));
+        Update update = new Update().set("prayerConfig", prayerConfig);
+        UpdateResult updateResult = mongoTemplate.updateMulti(query, update, CentralControl.class);
+        return updateResult != null && updateResult.isModifiedCountAvailable();
+    }
+
+
+    public void save(CentralControl centralControl) {
+        mongoTemplate.save(centralControl);
+    }
+
+    public void dropCollection() {
+        mongoTemplate.dropCollection(CentralControl.class);
+    }
+
+    // TODO remove below method this is just for testing updating complex object
+    // https://docs.mongodb.com/manual/reference/method/db.collection.findAndModify/#db.collection.findAndModify
+    // https://stackoverflow.com/questions/47699646/updating-replacing-a-deeply-nested-object-in-mongodb-with-spring-data-mongodb
+    public void updateComplexObject() {
+        // Query reference
+        // https://www.baeldung.com/queries-in-spring-data-mongodb
+        Query query = new Query(Criteria.where("companyId").is("company1"));
+        Update update = new Update().set("jummahs.0.khateeb", "Kateeb 1 Changed again");
+        UpdateResult updateResult = mongoTemplate.updateMulti(query, update, CentralControl.class);
+        System.out.println(updateResult);
+        List<CentralControl> centralControls = mongoTemplate.find(new Query(Criteria.where("companyId").is("company1")), CentralControl.class);
+        System.out.println(centralControls);
+
+        List<CentralControl> centralControls1 = mongoTemplate.findAll(CentralControl.class);
+        centralControls1.forEach(System.out::println);
+    }
+}
