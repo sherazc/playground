c86814fb030c8020cf4fb5f8643051860fc63f2e
Sheraz <sheraz@mbp.com>
2019-05-11 08:27:19 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Updates.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Updates.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Rod from "./Rod";
+import "./Update.scss";
 
 class Updates extends Component {
 
@@ -12,9 +13,15 @@ class Updates extends Component {
     render() {
         return (
             <div>
+                <div className="heading1 sectionHeadingSpacing">Reminders</div>
+                <div className="heading2">Quran</div>
 
-                <div className="sectionHeading">Reminders</div>
-                <div>Quran</div>
+
+                <div className="heading1">Heading1</div>
+                <div className="heading2">Heading2</div>
+                <div className="heading3">Heading3</div>
+                <div className="heading4">Heading4</div>
+                <div className="heading5">Heading5</div>
                 <div><Rod/></div>
             </div>
         );
