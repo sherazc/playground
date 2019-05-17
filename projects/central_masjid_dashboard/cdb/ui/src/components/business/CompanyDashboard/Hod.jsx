a0f43e143b7704851cd44b6c0c94b119449c0ebb
Sheraz <sheraz@mbp.com>
2019-05-17 06:36:45 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Hod.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Hod.jsx
@@ -4,7 +4,6 @@ import "./Hod.scss"
 
 const baseUrl = process.env.REACT_APP_API_BASE_PATH;
 
-
 class Hod extends Component {
     state = this.createInitialState();
 
@@ -13,10 +12,25 @@ class Hod extends Component {
             hadeesDetail: {}
         };
     }
+
+    componentDidMount() {
+        axios.get(`${baseUrl}/api/hod`).then(
+            response => this.setState({
+                hadeesDetail: response.data
+            })
+        );
+    }
+
     render() {
+        const {text, reference} = this.state.hadeesDetail;
         return (
             <div>
-                Hadees
+                <div className="textRegular">
+                    {text}
+                </div>
+                <div className="textSmall">
+                    {reference}
+                </div>
             </div>
         );
     }
