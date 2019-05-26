b763d07f250b130d9f0385ca55edb176ce06e962
Sheraz <sheraz@mbp.com>
2019-05-26 02:03:09 -0400

    Central Control API

--- projects/central_masjid_dashboard/cdb/api/webservices/src/main/java/com/sc/cdb/webservices/prayer/CentralControlController.java
+++ projects/central_masjid_dashboard/cdb/api/webservices/src/main/java/com/sc/cdb/webservices/prayer/CentralControlController.java
@@ -1,6 +1,9 @@
 package com.sc.cdb.webservices.prayer;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/companies", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CentralControlController {
 
+
+  @GetMapping("url/{url}/central-control")
+  public ResponseEntity<?> findByCompanyUrl(@PathVariable String url) {
+
+
+
+
+    return ResponseEntity.ok("Working");
+  }
 }
