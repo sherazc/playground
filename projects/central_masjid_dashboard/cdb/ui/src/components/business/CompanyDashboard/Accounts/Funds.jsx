9f859bdc97f90d94ea9931938ec4be18d5cb4a92
Sheraz <sheraz@mbp.com>
2019-06-09 22:44:41 -0400

    Central Control UI

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Accounts/Funds.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Accounts/Funds.jsx
@@ -5,26 +5,55 @@ class Funds extends Component {
         super(props);
         console.log("Funds created", this.props)
     }
-    render() {
+
+    makeFund(fund) {
         return (
             <div>
-                Funds
-                <br/>
-                Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>Funds
-                <br/>
+                <div>{fund.name}</div>
+                <table border="1">
+                    <tbody>
+                    <tr>
+                        <th>Target</th>
+                        <td>{fund.goal}</td>
+                    </tr>
+                    <tr>
+                        <th>End Date</th>
+                        <td>{fund.endDate}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Remaining</th>
+                        <td>{fund.goal - fund.current}</td>
+                    </tr>
+                    {
+                        fund.pledge && fund.pledge > 0 && (
+                            <tr>
+                                <th>Pledges</th>
+                                <td>{fund.pledge}</td>
+                            </tr>
+                        )
+                    }
+                    <tr>
+                        <th>Collected</th>
+                        <td>{fund.current}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    render() {
+        const {funds} = this.props;
+        if (!funds || funds.length < 1) {
+            return <div>No Funds Found</div>
+        }
+
+        return (
+            <div>
+                {this.makeFund(funds[0])}
+                <hr/>
+                {this.makeFund(funds[1])}
             </div>
         );
     }
