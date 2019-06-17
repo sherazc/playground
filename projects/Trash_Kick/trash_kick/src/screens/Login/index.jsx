2fa709b154f8fb31f816a0717776ff211d554a86
Sheraz <sheraz@mbp.com>
2019-06-17 19:13:23 -0400

    Initializing screens

---
+++ projects/Trash_Kick/trash_kick/src/screens/Login/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class Login extends Component {
+
+    render() {
+        return(
+            <View>
+                <Text>Splash Screen</Text>
+            </View>
+        );
+    }
+}
+
+export default Login;
