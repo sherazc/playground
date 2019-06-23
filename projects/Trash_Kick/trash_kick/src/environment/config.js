e01d687f63f59bfa94a64f6c9809dda55dfb13b0
Sheraz <sheraz@mbp.com>
2019-06-23 02:50:57 -0400

    added react-native-config. Linked it. And used it in JSX

--- projects/Trash_Kick/trash_kick/src/environment/config.js
+++ projects/Trash_Kick/trash_kick/src/environment/config.js
@@ -1,5 +1,7 @@
 
+import Config from 'react-native-config'
+
 export default {
-    API_BASE_URL: "NA - API_BASE_URL",
-    GOOGLE_MAPS_API_KEY: "NA GOOGLE_MAPS_API_KEY"
+    API_BASE_URL: Config.API_BASE_URL,
+    GOOGLE_MAPS_API_KEY: Config.GOOGLE_MAPS_API_KEY
 };
