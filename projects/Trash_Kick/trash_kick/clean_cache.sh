2f97f98336b250fda044cf5724ae668c9966f8ec
Sheraz <sheraz@mbp.com>
2019-06-24 07:52:26 -0400

    installed google map cocopods

--- projects/Trash_Kick/trash_kick/clean_cache.sh
+++ projects/Trash_Kick/trash_kick/clean_cache.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 watchman watch-del-all
-rm -rf node_modules
-npm install
 rm -rf /tmp/metro-bundler-cache-*
 rm -rf /tmp/haste-map-react-native-packager-*
+rm -rf ./node_modules
+rm -rf ./android/app/build
+rm -rf ./android/build
+rm -rf ./ios/build
+rm -rf ./ios/Pods
+
+npm install
+
+cd ios
+pod install
