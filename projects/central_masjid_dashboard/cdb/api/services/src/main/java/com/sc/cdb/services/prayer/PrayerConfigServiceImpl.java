065fc6ab349f6f867a5ed6bee51ebace895b509e
Sheraz <sheraz@mbp.com>
2019-09-17 23:33:12 -0400

    Next prayers

--- projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/prayer/PrayerConfigServiceImpl.java
+++ projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/prayer/PrayerConfigServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sc.cdb.services.prayer;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 import java.util.TreeMap;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 import com.sc.cdb.data.dao.PrayerConfigDao;
 import com.sc.cdb.data.model.prayer.Prayer;
 import com.sc.cdb.data.model.prayer.PrayerConfig;
 import com.sc.cdb.data.repository.PrayerConfigRepository;
 import com.sc.cdb.services.model.ServiceResponse;
+import org.apache.commons.lang3.SerializationUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -55,25 +59,77 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
         return serviceResponseBuilder.build();
     }
 
-    private Prayer findDatePrayerAndNextChange(List<Prayer> yearPrayers, int month, int day) {
+    private Prayer findDatePrayerAndNextChange(List<Prayer> yearPrayers, int month, int date) {
+
+        Optional<Prayer> foundPrayerOptional = yearPrayers.stream()
+                .filter(prayer -> comparePrayerMonthDate(prayer, month, date))
+                .findFirst();
+
+        if (foundPrayerOptional.isEmpty()) {
+            return null;
+        }
+
+        List<Prayer> yearPrayersMutable = new ArrayList<>(yearPrayers);
+        yearPrayersMutable.sort(this::comparePrayers);
+
+        // noinspection CollectionAddedToSelf
+        yearPrayersMutable.addAll(yearPrayersMutable);
+
+
+
+
+        /*
+        List<Prayer> yearPrayerClone = yearPrayersMutable.stream()
+                .map(SerializationUtils::clone)
+                .collect(Collectors.toList());
+
 
         TreeMap<String, Prayer> twoYearPrayers = yearPrayers.stream()
                 .collect(
                         TreeMap::new, // supplier
                         this::duplicatePrayers, // accumulator
-                        // this::combinePrayerTrees
                         TreeMap::putAll // combiner - combine second into first
                 );
 
-        twoYearPrayers.
-                forEach();
+
+        yearPrayers.stream().sorted(this::comparePrayers)
+                .collect(ArrayList::new,
+                        null,
+                        List::addAll);
+
 
         System.out.println(twoYearPrayers);
-
+        */
 
         return null;
     }
 
+    private boolean comparePrayerMonthDate(Prayer prayer, int month, int date) {
+        if (prayer.getDate() == null) {
+            return false;
+        }
+        Calendar prayerCalendar = Calendar.getInstance();
+        prayerCalendar.setTime(prayer.getDate());
+
+        return prayerCalendar.get(Calendar.MONTH) + 1 == month
+                && prayerCalendar.get(Calendar.DATE) == date;
+    }
+
+
+    private int comparePrayers(Prayer p1, Prayer p2) {
+        if (p1.getDate() == null && p2.getDate() == null) {
+            return 0;
+        }
+
+        if (p1.getDate() == null && p2.getDate() != null) {
+            return -1;
+        }
+
+        if (p1.getDate() != null && p2.getDate() == null) {
+            return 1;
+        }
+        return SDF_MM_DD.format(p1.getDate()).compareTo(SDF_MM_DD.format(p2.getDate()));
+    }
 
 
     private void duplicatePrayers(TreeMap<String, Prayer> prayerMap, Prayer prayer) {
