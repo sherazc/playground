0dfab7315731c2b1efb0b39a848c41f805dec601
Sheraz <sheraz@mbp.com>
2019-09-18 20:43:06 -0400

    Next prayers

--- projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/prayer/PrayerConfigServiceImpl.java
+++ projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/prayer/PrayerConfigServiceImpl.java
@@ -3,6 +3,7 @@ package com.sc.cdb.services.prayer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.TreeMap;
@@ -49,9 +50,9 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
                 serviceResponseBuilder.successful(false).message("Prayer not found.");
             } else {
 
-                Prayer prayer = findDatePrayerAndNextChange(prayers, month, day);
+                Prayer prayer = findDatePrayerAndNextChange2(prayers, month, day);
                 serviceResponseBuilder
-                        .target(prayers.get(0))
+                        .target(prayer)
                         .successful(true)
                         .message("Prayer found.");
             }
@@ -71,7 +72,7 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
         int foundIndex = -1;
 
         for (int i = 0; i < yearPrayersMutable.size(); i++) {
-            if (comparePrayerMonthDate(yearPrayersMutable.get(i), month, date) < 1)
+            if (comparePrayerMonthDate(yearPrayersMutable.get(i), month, date) < 0)
                 continue;
             Prayer loopPrayer = yearPrayersMutable.get(i);
             if(foundPrayer == null && comparePrayerMonthDate(loopPrayer, month, date) == 0) {
@@ -87,10 +88,52 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
     }
 
     private void setPrayerNextChange(Prayer foundPrayer, Prayer loopPrayer) {
-        // if next change is not set then set it
+        if (StringUtils.isNotBlank(foundPrayer.getFajrIqama())
+                && StringUtils.isBlank(foundPrayer.getFajrChange())
+                && StringUtils.isNotBlank(loopPrayer.getFajrIqama())
+                && !StringUtils.equals(foundPrayer.getFajrIqama(), loopPrayer.getFajrIqama())
+                && loopPrayer.getDate() != null) {
+            foundPrayer.setFajrChangeDate(dateToCurrentYearData(loopPrayer.getDate()));
+            foundPrayer.setFajrChange(loopPrayer.getFajrIqama());
+        }
+
+        if (StringUtils.isNotBlank(foundPrayer.getDhuhrIqama())
+                && StringUtils.isBlank(foundPrayer.getDhuhrChange())
+                && StringUtils.isNotBlank(loopPrayer.getDhuhrIqama())
+                && !StringUtils.equals(foundPrayer.getDhuhrIqama(), loopPrayer.getDhuhrIqama())
+                && loopPrayer.getDate() != null) {
+            foundPrayer.setDhuhrChangeDate(dateToCurrentYearData(loopPrayer.getDate()));
+            foundPrayer.setDhuhrChange(loopPrayer.getDhuhrIqama());
+        }
+
+        if (StringUtils.isNotBlank(foundPrayer.getAsrIqama())
+                && StringUtils.isBlank(foundPrayer.getAsrChange())
+                && StringUtils.isNotBlank(loopPrayer.getAsrIqama())
+                && !StringUtils.equals(foundPrayer.getAsrIqama(), loopPrayer.getAsrIqama())
+                && loopPrayer.getDate() != null) {
+            foundPrayer.setAsrChangeDate(dateToCurrentYearData(loopPrayer.getDate()));
+            foundPrayer.setAsrChange(loopPrayer.getAsrIqama());
+        }
+
+        if (StringUtils.isNotBlank(foundPrayer.getIshaIqama())
+                && StringUtils.isBlank(foundPrayer.getIshaChange())
+                && StringUtils.isNotBlank(loopPrayer.getIshaIqama())
+                && !StringUtils.equals(foundPrayer.getIshaIqama(), loopPrayer.getIshaIqama())
+                && loopPrayer.getDate() != null) {
+            foundPrayer.setIshaChangeDate(dateToCurrentYearData(loopPrayer.getDate()));
+            foundPrayer.setIshaChange(loopPrayer.getIshaIqama());
+        }
     }
 
+    private Date dateToCurrentYearData(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        int currentYear = calendar.get(Calendar.YEAR);
+        calendar.setTime(date);
+        calendar.set(Calendar.YEAR, currentYear);
+        return calendar.getTime();
+    }
 
+/*
     private Prayer findDatePrayerAndNextChange(List<Prayer> yearPrayers, int month, int date) {
 
         List<Prayer> yearPrayersMutable = new ArrayList<>(yearPrayers);
@@ -118,7 +161,7 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
 
 
 
-        /*
+
         List<Prayer> yearPrayerClone = yearPrayersMutable.stream()
                 .map(SerializationUtils::clone)
                 .collect(Collectors.toList());
@@ -139,10 +182,12 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
 
 
         System.out.println(twoYearPrayers);
-        */
+
 
         return null;
     }
+    */
+
 
     private int comparePrayerMonthDate(Prayer prayer, int month, int date) {
         if (prayer.getDate() == null) {
@@ -166,13 +211,13 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
         return result;
     }
 
-    private int comparePrayer(Prayer prayer1, Prayer prayer2) {
+    private int comparePrayers(Prayer prayer1, Prayer prayer2) {
         if (prayer1.getDate() == null && prayer2.getDate() == null) {
             return 0;
         } else if (prayer1.getDate() == null) {
-            return -1;
-        } else if (prayer2.getDate() == null) {
             return 1;
+        } else if (prayer2.getDate() == null) {
+            return -1;
         }
 
         Calendar prayerCalendar2 = Calendar.getInstance();
@@ -184,22 +229,18 @@ public class PrayerConfigServiceImpl implements PrayerConfigService {
         return this.comparePrayerMonthDate(prayer1, prayerMonth, prayerDate);
     }
 
-
+/*
     private int comparePrayers(Prayer p1, Prayer p2) {
         if (p1.getDate() == null && p2.getDate() == null) {
             return 0;
-        }
-
-        if (p1.getDate() == null && p2.getDate() != null) {
+        } else if (p1.getDate() == null && p2.getDate() != null) {
             return -1;
-        }
-
-        if (p1.getDate() != null && p2.getDate() == null) {
+        } else if (p1.getDate() != null && p2.getDate() == null) {
             return 1;
         }
         return SDF_MM_DD.format(p1.getDate()).compareTo(SDF_MM_DD.format(p2.getDate()));
     }
-
+*/
 
     private void duplicatePrayers(TreeMap<String, Prayer> prayerMap, Prayer prayer) {
         if (prayer.getDate() == null) {
