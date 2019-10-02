701150da44d8c0fdc3588a6cbbb09d858768ef9b
Sheraz <sheraz@mbp.com>
2019-10-02 08:13:29 -0400

    auto commit

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/admin/TabPrayer/Dst/Dst.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/admin/TabPrayer/Dst/Dst.jsx
@@ -5,6 +5,7 @@ import {
     FormControlLabel, Checkbox, FormGroup, Switch
 } from "@material-ui/core";
 import styles from "../ResetPrayerConfig/ResetPrayerConfig.module.scss";
+import CloseablePanel from "../../../../common/CloseablePanel/CloseablePanel";
 
 
 class Dst extends Component {
@@ -12,40 +13,44 @@ class Dst extends Component {
     render() {
         return (
             <div>
-                <h3>DST</h3>
-                <FormControl>
-                    <FormGroup aria-label="position" row>
-                        <FormControlLabel
-                            control={<Switch color="primary"/>}
-                            label="Enable"
-                            labelPlacement="start"
-                        />
-                        <FormControlLabel
-                            control={<Switch color="primary"/>}
-                            label="Automatic Calculate"
-                            labelPlacement="start"
-                        />
-                    </FormGroup>
+                <CloseablePanel
+                    title="DST Settings"
+                    // editMode={this.state.editMode}
+                    // defaultExpanded={this.props.defaultExpanded}
+                    // onSave={this.props.onSave}
+                    //onCancel={this.onCancel.bind(this)}
+                >
+
+                    <FormControl>
+                        <FormGroup aria-label="position" row>
+                            <FormControlLabel
+                                control={<Switch color="primary"/>}
+                                label="Enable"
+                                labelPlacement="start"
+                            />
+                            <FormControlLabel
+                                control={<Switch color="primary"/>}
+                                label="Automatic Calculate"
+                                labelPlacement="start"
+                            />
+                        </FormGroup>
 
 
-                        <TextField
-                            margin="dense" name="start"
-                            label="Start" type="text"
-                            value=""
-                            onChange={() => {}}/>
+                            <TextField
+                                margin="dense" name="start"
+                                label="Start" type="text"
+                                value=""
+                                onChange={() => {}}/>
 
-                        <TextField
-                            margin="dense" name="start"
-                            label="End" type="text"
-                            value=""
-                            onChange={() => {}}/>
+                            <TextField
+                                margin="dense" name="start"
+                                label="End" type="text"
+                                value=""
+                                onChange={() => {}}/>
 
 
-                </FormControl>
-
-                <Button variant="outlined" color="primary">
-                    Apply
-                </Button>
+                    </FormControl>
+                </CloseablePanel>
             </div>
         );
     }
