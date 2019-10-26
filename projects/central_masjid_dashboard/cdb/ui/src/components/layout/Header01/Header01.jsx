2853f5e7970eb715a8b8b6c39090552f8301b933
Sheraz <sheraz@mbp.com>
2019-10-26 00:29:31 -0400

    UI Design

--- projects/central_masjid_dashboard/cdb/ui/src/components/layout/Header01/Header01.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/layout/Header01/Header01.jsx
@@ -6,14 +6,16 @@ class Header01 extends Component {
     render() {
         return (
             <div className={styles.container}>
-                <div className={styles.panelRight}>
+                <div className={styles.panelLeft}>
                     <Logo className={styles.logo} classPath={styles.logoPath}/>
-                    <div className={styles.appName}>Masjid Dashboard</div>
+                    <div className={`${styles.appName} ${styles.heading4}`}>Masjid Dashboard</div>
                 </div>
-                <div className={styles.panelRight}>
+                <div className={styles.panelCenter}>
                     <div className={styles.navContainer}>
                         Page1 Page2
                     </div>
+                </div>
+                <div className={styles.panelRight}>
                     <div className={styles.profile}>
                         Login Profile
                     </div>
