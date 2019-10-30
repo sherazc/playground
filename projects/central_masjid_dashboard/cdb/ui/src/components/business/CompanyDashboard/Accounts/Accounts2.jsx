216e7490227667aa5184fa45fc6235834c25b393
Sheraz <sheraz@mbp.com>
2019-10-30 08:38:26 -0400

    Account slides

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Accounts/Accounts2.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/CompanyDashboard/Accounts/Accounts2.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import Funds from "./Funds/Funds";
 import Expenses from "./Expenses/Expenses";
 import styles from "./Accounts.module.scss"
+import {equalObjects} from "../../../../services/utilities";
 
 const TOTAL_SLIDES = 2;
 
@@ -21,9 +22,79 @@ class Accounts2 extends Component {
         this.animationStaySeconds = 5;
         this.currentSlide = 0;
 
+        this.state = {
+            expenses: {},
+            funds: {},
+        }
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
+
+        if (this.shouldUpdateStateFromProps(this.props, prevProps, this.state)) {
+            this.cleanup();
+
+        }
+
+
+
+        /*
+        Expense
+        check if props.expense != state.expense
+        state.expense = props.expenses
+
+
+        get all enabled
+        create component only if found
+        if component created then push it to slides []
+
+        Funds
+            same as Expenses
+
+        Create this.slideDiv[]
+
+        if slideDiv[] is empty show error message
+
+        Set state
+        this.state = {
+            slidesClasses: this.createDefaultSlideClasses(slideDiv.lenght)
+            currentSlide: 0
+        }
+
+            createDefaultSlideClasses()
+                will set Open style class in slidesClasses[0]
+                and will set Close style class in slidesClasses[0]
+
+
+        If slideDiv.lenght is > 1
+            Start animation by toggling open close style classes in this.state.slidesClasses
+
+        */
+
+
+
+    }
+
+
+    shouldUpdateStateFromProps(currentProps, prevProps, state) {
+        const currentExpense = currentProps.centralControl.expenses;
+        const previousExpense = prevProps.centralControl.expenses;
+        const stateExpense = state.expenses;
+
+        const currentFunds = currentProps.centralControl.funds;
+        const previousFunds = prevProps.centralControl.funds;
+        const stateFunds = state.funds;
+
+        const expenseShouldUpdate = !equalObjects(currentExpense, previousExpense)
+            && !equalObjects(currentExpense, stateExpense);
+
+
+        const fundsShouldUpdate = !equalObjects(currentFunds, previousFunds)
+            && !equalObjects(currentFunds, stateFunds);
+
+        return expenseShouldUpdate || fundsShouldUpdate;
+    }
+
+    cleanup() {
 
     }
 
