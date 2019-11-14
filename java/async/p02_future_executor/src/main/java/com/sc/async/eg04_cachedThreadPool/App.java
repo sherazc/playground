2ea183a4a44efb19b7879c708f0585ca3d8c6af5
Sheraz <sheraz@mbp.com>
2019-11-13 21:05:09 -0500

    Thread Pools

--- java/async/p02_future_executor/src/main/java/com/sc/async/eg04_cachedThreadPool/App.java
+++ java/async/p02_future_executor/src/main/java/com/sc/async/eg04_cachedThreadPool/App.java
@@ -1,19 +1,22 @@
 package com.sc.async.eg04_cachedThreadPool;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-@SuppressWarnings("Duplicates")
 public class App {
 
     public static void main(String[] args) {
+        Runnable task = () -> System.out.println("Thread: " + Thread.currentThread().getName());
 
-        ExecutorService executorService = Executors.newCachedThreadPool();
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(3);
 
-        for (int i = 0; i < 10; i++) {
-            executorService.execute(
-                    () -> System.out.println("Thread: " + Thread.currentThread().getName()));
-        }
+        // Run after 10 second
+        executorService.schedule(task, 10, TimeUnit.SECONDS);
+
+        // Start running after 15 seconds and then run every 10 second
+        executorService.scheduleAtFixedRate(task, 15, 10, TimeUnit.SECONDS);
+
 
         System.out.println("Done. Main Thread: " + Thread.currentThread().getName());
     }
