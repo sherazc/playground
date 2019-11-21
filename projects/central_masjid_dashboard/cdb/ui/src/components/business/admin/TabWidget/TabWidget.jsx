492ca657d27eacee573828ca938fceffe0507b2c
Sheraz <sheraz@mbp.com>
2019-11-21 06:59:26 -0500

    auto commit

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/admin/TabWidget/TabWidget.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/admin/TabWidget/TabWidget.jsx
@@ -1,28 +1,39 @@
 import React, {Component} from "react";
 import styles from "./TabWidget.module.scss"
+import {widgets} from "./widgets";
+
 class TabWidget extends Component {
+
+    makeWidget(widget) {
+        return (
+            <div className={styles.widgetContainer}>
+                <div className={styles.widgetHeader}>
+                    {widget.name}
+                </div>
+                <div className={styles.widgetScriptDescriptionContainer}>
+                    <div className={styles.widgetScript}>
+                        {widget.script}
+                    </div>
+                    <div className={styles.widgetDescription}>
+                        Details
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 
     render() {
         return(
             <div>
-                <div>
+                <div className={styles.mainDescription}>
                     These are all the widgets available to you.
                     Incorporate them in your website by copying pasting the code.
                 </div>
-                <div className={styles.widgetContainer}>
-                    <div className={styles.widgetHeader}>
-                        Prayer Time
-                    </div>
-                    <div className={styles.widgetScriptDetailContainer}>
-                        <div className={styles.widgetScript}>
-                            code
-                        </div>
-                        <div className={styles.widgetDetail}>
-                            Details
-                        </div>
-                    </div>
 
-                </div>
+                {widgets.map(widget => this.makeWidget(widget))}
+
+
 
             </div>
         );
---
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/admin/TabWidget/widgets.js
@@ -0,0 +1,14 @@
+export const widgets = [
+    {
+        name: "Widget Name1",
+        description: "Widget Description1",
+        image: "/images/clock_analog/dial.svg",
+        script: `<div>some script1</div>`
+    },
+    {
+        name: "Widget Name2",
+        description: "Widget Description2",
+        image: "/images/clock_analog/dial.svg",
+        script: `<div>some script2</div>`
+    }
+];
