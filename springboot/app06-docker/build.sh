df12b2b9302cfec658e44b0985835a4627e0ed96
Sheraz <sheraz@mbp.com>
2020-01-23 21:22:46 -0500

    spring boot docker

--- springboot/app06-docker/build.sh
+++ springboot/app06-docker/build.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+# Removing resources
 docker rm -f sb_app06_8082
 docker rm -f sb_app06_8084
 docker rm -f sb_app06_db
 docker rmi sb_app06_image
-
+docker network rm sb_app06_network
 docker volume rm sb_app06_db_volume
+
+# Creating and building
+docker network create --driver=bridge sb_app06_network
 docker volume create sb_app06_db_volume
 
+# -p 3306:3306 is required if
 docker run \
-    --detach \
-    --name=sb_app06_db \
-    -p 3306:3306 \
-    --env="MYSQL_ROOT_PASSWORD=password" \
-    --env="MYSQL_DATABASE=sb_db" \
-    --env="MYSQL_USER=dbuser" \
-    --env="MYSQL_PASSWORD=password" \
-    --mount source=sb_app06_db_volume,target=/var/lib/mysql \
-    mysql:latest
+  --detach \
+  --name=sb_app06_db \
+  -p 3306:3306 \
+  --env="MYSQL_ROOT_PASSWORD=password" \
+  --env="MYSQL_DATABASE=sb_db" \
+  --env="MYSQL_USER=dbuser" \
+  --env="MYSQL_PASSWORD=password" \
+  --mount source=sb_app06_db_volume,target=/var/lib/mysql \
+  --network sb_app06_network \
+  mysql:latest
 
 mvn clean install
 
@@ -25,12 +31,12 @@ docker build . -t sb_app06_image
 
 docker run -it \
   --name sb_app06_8082 \
-  --link sb_app06_db \
+  --network sb_app06_network \
   -p 8082:8080 \
   -d sb_app06_image
 
 docker run -it \
   --name sb_app06_8084 \
-  --link sb_app06_db \
+  --network sb_app06_network \
   -p 8084:8080 \
   -d sb_app06_image
