d48d882109354305ea39c584f7abc2123883a70e
Sheraz <sheraz@mbp.com>
2020-02-03 19:33:56 -0500

    Refactoring authentication and app bar

--- projects/central_masjid_dashboard/cdb/ui/src/components/layout/Header01/Nav/Drawer02.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/layout/Header01/Nav/Drawer02.jsx
@@ -6,7 +6,17 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
+
 import MailIcon from '@material-ui/icons/Mail';
+import {mapStateLoginToProps} from "../../../../store/lib/utils";
+import {loginResetAction, viewMyProfileAction} from "../../../../store/login/loginActions";
+
+import {connect} from "react-redux";
+import {withRouter} from 'react-router-dom';
+import Icon from '@material-ui/core/Icon';
+import {isSuperAdminLogin} from "../../../../services/auth/AuthNZ";
+
+const baseLinkUrl = process.env.PUBLIC_URL;
 
 class Drawer02 extends Component {
     constructor(props) {
@@ -15,6 +25,7 @@ class Drawer02 extends Component {
     }
 
     createDrawerContent(props) {
+        const superAdminLogin = isSuperAdminLogin(this.props.login);
         return (
             <div
                 style={{width: 250}}
@@ -23,22 +34,51 @@ class Drawer02 extends Component {
                 onKeyDown={props.onCloseDrawer}
             >
                 <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text} onClick={() => console.log("Selected")}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
-                            <ListItemText primary={text}/>
-                        </ListItem>
-                    ))}
+                    <ListItem button
+                              onClick={() => this.props.history.push(`${baseLinkUrl}/auth/company/user/list/current`)}>
+                        <ListItemIcon><Icon>group</Icon></ListItemIcon>
+                        <ListItemText primary="Users"/>
+                    </ListItem>
+
+
+                    <ListItem button
+                              onClick={() => this.props.history.push(`${baseLinkUrl}/auth/company/view`)}>
+                        <ListItemIcon><Icon>business</Icon></ListItemIcon>
+                        <ListItemText primary="Masjid"/>
+                    </ListItem>
+
+                    <ListItem button
+                              onClick={() => this.props.history.push(`${baseLinkUrl}/auth/admin`)}>
+                        <ListItemIcon><Icon>settings</Icon></ListItemIcon>
+                        <ListItemText primary="Settings"/>
+                    </ListItem>
                 </List>
-                <Divider/>
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
-                            <ListItemText primary={text}/>
-                        </ListItem>
-                    ))}
-                </List>
+
+                {superAdminLogin && (
+                    <>
+                        <Divider/>
+                        <List>
+                            <ListItem button
+                                      onClick={() => this.props.history.push(`${baseLinkUrl}/auth/company/list`)}>
+                                <ListItemIcon><Icon>account_balance</Icon></ListItemIcon>
+                                <ListItemText primary="All Companies"/>
+                            </ListItem>
+
+
+                            <ListItem button
+                                      onClick={() => this.props.history.push(`${baseLinkUrl}/auth/company/user/list/all`)}>
+                                <ListItemIcon><Icon>supervised_user_circle</Icon></ListItemIcon>
+                                <ListItemText primary="All Users"/>
+                            </ListItem>
+
+                            <ListItem button
+                                      onClick={() => this.props.history.push(`${baseLinkUrl}/examples`)}>
+                                <ListItemIcon><Icon>bug_report</Icon></ListItemIcon>
+                                <ListItemText primary="Debug"/>
+                            </ListItem>
+                        </List>
+                    </>
+                )}
             </div>
         );
     }
@@ -54,4 +94,7 @@ class Drawer02 extends Component {
     }
 }
 
-export default Drawer02;
+export default connect(mapStateLoginToProps, {
+    loginResetAction,
+    viewMyProfileAction
+})(withRouter(Drawer02));
