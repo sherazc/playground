b914a255e9dfe6617ea17f2e152a07fe301e8b39
Sheraz <sheraz@mbp.com>
2020-03-05 08:04:44 -0500

    LAL JPA

--- lunch_and_learn/lal_springboot/sc10_sb_jpa_relations/src/main/java/com/lal/sb/Application.java
+++ lunch_and_learn/lal_springboot/sc10_sb_jpa_relations/src/main/java/com/lal/sb/Application.java
@@ -5,7 +5,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Query Joins
 // https://www.roytuts.com/spring-boot-data-jpa-left-right-inner-and-cross-join-examples/
+
+// Long query tutorial
+// https://www.petrikainulainen.net/programming/spring-framework/spring-data-jpa-tutorial-three-custom-queries-with-query-methods/
+
+// Query Methods
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+
+// Criteria Specification
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#specifications
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
