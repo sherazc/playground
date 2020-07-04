c857178960dfb8b79f60aaf0afb38b4b27d9b896
Sheraz <sheraz@mbp.com>
2020-07-04 11:19:43 -0400

    Restaurant payment

---
+++ projects/restaurant_payment/rp-server-apps/customer-data/src/test/java/com/sc/rp/data/customer/AppTest.java
@@ -0,0 +1,20 @@
+package com.sc.rp.data.customer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
