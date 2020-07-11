2429322518b6767eda43a95820bfc1e30e465734
Sheraz <sheraz@mbp.com>
2020-07-11 14:52:56 -0400

    Restaurant payment

--- projects/restaurant_payment/run.sh
+++ projects/restaurant_payment/run.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-java -jar api/webservices/target/cdb.jar
+
+docker run -itd \
+  --name rp-server-container \
+  -p 8082:8082 \
+  -p 8084:8084 \
+  -p 8086:8086 \
+  rp-server-image
+
+
+docker ps -a
+
+date
