5133f1239a1b2a22fce2dcc81f3259402f32c9a7
Sheraz <sheraz@mbp.com>
2020-07-25 16:56:17 -0400

    Restaurant payment

--- projects/restaurant_payment/rp-server/system-app/src/main/java/com/sc/rp/app/system/config/SystemContextConfiguration.java
+++ projects/restaurant_payment/rp-server/system-app/src/main/java/com/sc/rp/app/system/config/SystemContextConfiguration.java
@@ -4,10 +4,22 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("com.sc.rp.app.system")
 @PropertySource("classpath:system-context.properties")
 @EnableAutoConfiguration
-public class SystemContextConfiguration {
+public class SystemContextConfiguration implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/static-system/"
+    };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
 }
