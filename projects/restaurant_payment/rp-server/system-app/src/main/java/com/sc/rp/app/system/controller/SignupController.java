62fc98f9bf0ef3c5cff6728122b78a76a4dc322f
Sheraz <sheraz@mbp.com>
2020-07-31 00:23:46 -0400

    Restaurant payment

--- projects/restaurant_payment/rp-server/system-app/src/main/java/com/sc/rp/app/system/controller/SignupController.java
+++ projects/restaurant_payment/rp-server/system-app/src/main/java/com/sc/rp/app/system/controller/SignupController.java
@@ -1,17 +1,25 @@
 package com.sc.rp.app.system.controller;
 
 import com.sc.rp.app.system.model.SignupRequest;
+import com.sc.rp.data.system.entity.Company;
+import com.sc.rp.data.system.entity.CompanyUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SignupController {
 
     @GetMapping("/signup")
-    public String signUp() {
-        return "signup";
+    public ModelAndView signUp() {
+        ModelAndView modelAndView = new ModelAndView("signup");
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setCompany(new Company());
+        signupRequest.setCompanyUser(new CompanyUser());
+        modelAndView.addObject("signupRequest", signupRequest);
+        return modelAndView;
     }
 
     @PostMapping("/signup")
