2175b174458a3fd00523f888f1ea520dfa9b8b07
Sheraz <sheraz@mbp.com>
2020-08-01 23:51:15 -0400

    Restaurant payment

--- projects/restaurant_payment/rp-server/system-app/src/main/java/com/sc/rp/app/system/model/SignupRequest.java
+++ projects/restaurant_payment/rp-server/system-app/src/main/java/com/sc/rp/app/system/model/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.sc.rp.app.system.model;
 
+import com.sc.rp.data.system.entity.Bank;
 import com.sc.rp.data.system.entity.Company;
 import com.sc.rp.data.system.entity.CompanyUser;
 import lombok.Data;
@@ -9,4 +10,5 @@ public class SignupRequest {
     private Company company;
     private CompanyUser companyUser;
     private Long pricingPlanId;
+    private Bank bank;
 }
