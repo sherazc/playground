f6b6f5bac400d20c6f67f89471b389bf21a36e78
Sheraz <sheraz@mbp.com>
2020-09-02 01:32:17 -0400

    CompletableFuture

---
+++ java/async/p03_completable_future/src/main/java/com/sc/async/eg08_orTimeout/App.java
@@ -0,0 +1,28 @@
+package com.sc.async.eg08_orTimeout;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import com.sc.async.common.MyThreadUtils;
+
+public class App {
+    public static void main(String[] args) {
+        CompletableFuture
+                .supplyAsync(() -> longRunningStage())
+                .orTimeout( 1000, TimeUnit.MILLISECONDS) // Throws TimeoutException on timeout
+                .exceptionally(throwable -> handleException(throwable))
+                .thenAccept(i -> System.out.println("Done " + i));
+
+        MyThreadUtils.sleep(2000);
+    }
+
+    private static Integer handleException(Throwable throwable) {
+        System.out.println("Handling Exception. " + throwable.getClass().getName());
+        return 200;
+    }
+
+    private static Integer longRunningStage() {
+        MyThreadUtils.sleep(5000);
+        return 100;
+    }
+}
