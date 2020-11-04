dc8601ab59882b5c6c9f59d884e0375b8d23003a
Sheraz <sheraz@sheraz-desktop.com>
2020-11-04 13:38:06 -0500

    CMD Mobile

--- projects/central_masjid_dashboard/MasjidDashboardMobile/src/components/CompanySelect/CompanyList.tsx
+++ projects/central_masjid_dashboard/MasjidDashboardMobile/src/components/CompanySelect/CompanyList.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { CompanyListData } from "../../types/types";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { CompanyListData, Company } from '../../types/types';
 
 interface Props {
     companyListData: CompanyListData;
 }
 
-export const CompanyList: React.FC<Props> = ({companyListData}) => {
+export const CompanyList: React.FC<Props> = ({ companyListData }) => {
     return (
         <View style={styles.container}>
-            <Text>Company List component</Text>
+            <Text>Select Masjid</Text>
+            {buildCompanyFlatList(companyListData)}
+
+        </View>
+    );
+}
+
+
+const buildCompanyFlatList = (cld?: CompanyListData) => {
+
+    if (!cld || !cld.companies || cld.companies.length < 1) {
+        return (<View><Text>Loading...</Text></View>);
+    }
+
+    return (
+        <View>
+            <FlatList
+                data={cld.companies}
+                // @ts-ignore
+                renderItem={buildCompanyListItem}
+                keyExtractor={item => item.id}
+            />
+        </View>
+    );
+}
+
+const buildCompanyListItem = ({ item, onPress, style }: { item: Company, onPress: Function, style: object }) => {
+    return (
+        <View>
+            <Text>{item.name}</Text>
         </View>
     );
 }
