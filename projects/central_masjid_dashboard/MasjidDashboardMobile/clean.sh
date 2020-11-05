4bdcbf30e4cc2d359a17278ed9eed7d670196c8b
Sheraz <sheraz@mbp.com>
2020-11-05 01:50:10 -0500

    CMD Mobile

---
+++ projects/central_masjid_dashboard/MasjidDashboardMobile/clean.sh
@@ -0,0 +1,23 @@
+# clean android
+cd android
+rm -rf .idea
+./gradlew clean
+rm -rf build
+cd ..
+
+# clean ios
+cd ios
+rm Podfile.lock
+rm -rf Pods
+cd ..
+
+# clean node
+rm yarn.lock
+rm -rf node_modules
+
+
+# download dependencies
+yarn install
+cd ios
+pod install
+cd ..
