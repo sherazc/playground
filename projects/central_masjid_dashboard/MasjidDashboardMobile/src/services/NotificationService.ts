1d7422930bd2a8c13006bbaa09a095fdce614d71
Sheraz <sheraz@sheraz-desktop.com>
2020-12-17 08:00:17 -0500

    MDB Mobile

--- projects/central_masjid_dashboard/MasjidDashboardMobile/src/services/NotificationService.ts
+++ projects/central_masjid_dashboard/MasjidDashboardMobile/src/services/NotificationService.ts
@@ -4,23 +4,39 @@ import store from '../store/rootReducer';
 import { Constants } from './Constants';
 
 // TODO: find proper way to call it. Once if not expired.
-// TODO: accecpt company data instead of company id
 export default function setupNotifications(companyId: string) {
     if (!isNotificationAlreadySet(companyId)) {
         console.log(`Not setting up notification. Notification already set for ${companyId}.`);
         return
     }
 
-    // TODO: do this in an interval
-    // Run the interval in a promise
-    if (!isValidCompanyDataAvailable(companyId, companyData)) {
-        console.log("Not setting up ")
-        return
-    }
+    startNotificaitonsSetInterval(companyId);
+}
 
-    // Set delay to setup notifications. Maybe create a new setupNotificationWithDelay(); Or maybe accecpt
-    // loadCompanyPrayerYear(companyId, now.getFullYear())
-    //     .then(loadPrayersYearSuccessful, loadPrayersYearFailed);
+const startNotificaitonsSetInterval = (companyId: string) => {
+    const resetNotificationInterval = setInterval(() => {
+        const companyData = store.getState().companyData;
+        
+        if (!isValidCompanyDataAvailable(companyId, companyData)) {
+            console.log("Not setting up ")
+            return
+        }
+
+        resetNotifications(companyData);
+
+        clearInterval(resetNotificationInterval);
+    }, 2000);
+}
+
+const resetNotifications = (companyData: CompanyData) => {
+    /*
+
+    Call notifcation api to cancle previous notifications
+
+    use prayersYear to build scheduled notifications for next 10 days
+
+    update notificaitions expiration time
+    */
 }
 
 const isNotificationAlreadySet = (companyId: string): boolean => {
@@ -34,8 +50,6 @@ const isNotificationAlreadySet = (companyId: string): boolean => {
         && currentTimeMillis < notificationExpirationMillis;
 }
 
-
-
 // Checks same company and valid prayerYear
 const isValidCompanyDataAvailable = (companyId: string, companyData: CompanyData): boolean => {
     return isSameCompany(companyId, companyData)
@@ -48,4 +62,3 @@ const isSameCompany = (companyId: string, companyData: CompanyData): boolean => 
     return companyData != undefined && companyData.company != undefined
         && companyData.company.id != undefined && companyData.company.id === companyId;
 }
-
