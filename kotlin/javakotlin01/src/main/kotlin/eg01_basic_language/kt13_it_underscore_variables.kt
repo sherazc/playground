b8d003590595f91d01712e33cbd5bdc48895ca6d
Sheraz <sheraz@mbp.com>
2020-12-27 12:53:29 -0500

    Kotlin

--- kotlin/javakotlin01/src/main/kotlin/eg01_basic_language/kt13_it_underscore_variables.kt
+++ kotlin/javakotlin01/src/main/kotlin/eg01_basic_language/kt13_it_underscore_variables.kt
@@ -1,7 +1,14 @@
 package eg01_basic_language
+
 fun main(args: Array<String>) {
-    fun calculate1(a: Int, b: (Int) -> Int) =  println("calculate1 = ${b(a)}")
-    fun calculate2(a: Int, b: Int, c: (Int, Int) -> Int) =  println("calculate2 = ${c(a, b)}")
+    // it. For single argument lambda, "it" can be used and not specify variable name
+    fun calculate1(a: Int, b: (Int) -> Int) = println("calculate1 = ${b(a)}")
+    calculate1(2) { i -> i * 2 }
+    calculate1(2) { it * 2 }
 
-
+    // _. Underscore could be used for argument that are not used in lambda
+    fun calculate2(a: Int, b: Int, c: Int, total: (Int, Int, Int) -> Int) = println("calculate2 = ${total(a, b, c)}")
+    val doubleMiddle = { _: Int, i: Int, _: Int -> i * 2 } // Underscore for first and last
+    calculate2(1, 2, 3, doubleMiddle)
+    calculate2(1, 2, 3) { _, i, _ -> i * 2 }
 }
