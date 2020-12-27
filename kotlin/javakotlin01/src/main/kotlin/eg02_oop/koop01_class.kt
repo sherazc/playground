02de5c72c3a1a44c654942e315cd636d651155bc
Sheraz <sheraz@mbp.com>
2020-12-27 16:18:20 -0500

    Kotlin

---
+++ kotlin/javakotlin01/src/main/kotlin/eg02_oop/koop01_class.kt
@@ -0,0 +1,58 @@
+package eg02_oop
+
+class EmptyClass
+
+class ClassWithBody {}
+
+class ClassWithProperties {
+    // By default each property is private
+    // Each property gets public getter and setter
+    val a = 10
+}
+
+class PrimaryConstructor constructor()
+
+class PrimaryConstructorNoKeyword()
+
+class ClassPropertyValuePassedInPrimaryConstructor(a: Int)
+
+class ClassPropertyDefaultValue(a: Int = 10)
+
+class InitializeProperties(a:Int = 10) {
+    val b:Int = a * 2
+}
+
+class InitializePrimaryConstructor(a: Int = 10) {
+    val b: Int
+    init {
+        b = a * 2;
+        println("InitializePrimaryConstructor initialized")
+    }
+}
+
+class ClassWithFunction {
+    fun f1() {}
+}
+
+class SecondaryConstructor {
+    constructor(a: Int)
+}
+
+// Secondary constructor have to call primary
+class SecondaryConstructorCallPrimary(a: Int) {
+    var b:Int = 0
+
+    constructor(a: Int, b:Int) : this(a) {
+        this.b = b
+    }
+}
+
+fun main(args: Array<String>) {
+    // Create object from class
+    val obj = SecondaryConstructorCallPrimary(10, 20)
+    println("SecondaryConstructorCallPrimary.b = ${obj.b}")
+}
+
+
+
+
