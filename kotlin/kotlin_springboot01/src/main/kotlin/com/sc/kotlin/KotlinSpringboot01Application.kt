c7eae37755ac66c2a50511c6f4222e19c903f37e
Sheraz <sheraz@mbp.com>
2020-12-28 08:51:52 -0500

    auto commit

---
+++ kotlin/kotlin_springboot01/src/main/kotlin/com/sc/kotlin/KotlinSpringboot01Application.kt
@@ -0,0 +1,11 @@
+package com.sc.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringboot01Application
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringboot01Application>(*args)
+}
