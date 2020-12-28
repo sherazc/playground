3cfe22c0250c9f7640ad95c2d13d64d8e2c99a42
Sheraz <sheraz@mbp.com>
2020-12-28 08:51:39 -0500

    Kotlin

---
+++ kotlin/javakotlin01/src/main/kotlin/eg02_oop/koop03_interface.kt
@@ -0,0 +1,36 @@
+package eg02_oop
+
+interface A {
+    fun a1()
+    fun a2() {
+        println("Interface A function f2")
+    }
+}
+
+interface B {
+    var b1: Int
+    fun b2()
+}
+
+// C inherits A and B
+interface C : A, B
+
+// D implements A, B, and C
+class D: A, B, C {
+    override fun a1() {
+        println("a1")
+    }
+
+    override var b1: Int = 20;
+
+    override fun b2() {
+        println("b2")
+    }
+}
+
+fun main(args: Array<String>) {
+    val x = D();
+    x.a1()
+    x.a2()
+    println(x.b1)
+}
