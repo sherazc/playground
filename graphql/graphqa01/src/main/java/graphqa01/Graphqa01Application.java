087dcf235308772d5d0da31f364892dcaf782d96
Sheraz <sheraz@mbp.com>
2021-01-23 10:45:24 -0500

    GraphQL

---
+++ graphql/graphqa01/src/main/java/graphqa01/Graphqa01Application.java
@@ -0,0 +1,13 @@
+package graphqa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Graphqa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Graphqa01Application.class, args);
+	}
+
+}
---
+++ graphql/graphqa01/src/test/java/graphqa01/Graphqa01ApplicationTests.java
@@ -0,0 +1,13 @@
+package graphqa01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Graphqa01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
