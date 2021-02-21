aacdb5602cf3a1cb44f609c7f8b93d63959ac6b3
Sheraz <sheraz@mbp.com>
2021-02-21 00:32:00 -0500

    CMD move misc items

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/Calendar/Calendar.jsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/Calendar/Calendar.jsx
@@ -4,6 +4,10 @@ import {
 } from '@material-ui/core';
 
 import styles from "./Calendar.module.scss";
+import Header02 from "../../layout/Header02/Header02";
+import Content01 from "../../layout/Content01/Content01";
+import Layout02 from "../../layout/Layout02/Layout02";
+import Footer02 from "../../layout/Footer02/Footer02";
 
 
 class Calendar extends Component {
@@ -26,9 +30,14 @@ class Calendar extends Component {
     getMonths(calendarType) {
         let months;
         if (calendarType && calendarType === "Gregorian") {
-            months = ["January", "Feburary"];
+            months = ["All Months", "1 - January", "2 - February", "3 - March", "4 - April", "5 - May", "6 - June",
+                "7 - July", "8 - August", "9 - September", "10 - October", "11 - November", "12 - December"];
         } else {
-            months = ["a", "b"];
+            months = ["All Months", "1 - Al-Muḥarram - ٱلْمُحَرَّم", "2 - Ṣafar - صَفَر", "3 - Rabīʿ al-ʾAwwal - رَبِيع ٱلْأَوَّل",
+                "4 - Rabīʿ ath-Thānī - رَبِيع ٱلثَّانِي", "5 - Jumadā al-ʾŪlā - جُمَادَىٰ ٱلْأُولَىٰ",
+                "6 - Jumādā ath-Thāniyah - جُمَادَىٰ ٱلثَّانِيَة", "7 - Rajab - رَجَب", "8 - Shaʿbān - شَعْبَان",
+                "9 - Ramaḍān - رَمَضَان", "10 - Shawwāl - شَوَّال", "11 - Ḏū al-Qaʿdah - ذُو ٱلْقَعْدَة",
+                "12 - Ḏū al-Ḥijjah - ذُو ٱلْحِجَّة"];
         }
 
         return months;
@@ -52,25 +61,34 @@ class Calendar extends Component {
         return items.map((item, index) => <MenuItem key={index} value={item}>{item}</MenuItem>);
     }
 
-    render() {return (<div>
-        <div>
-            <Select
-                name="calendarTypeSelected"
-                value={this.state.calendarTypeSelected}
-                label="Calendar Type"
-                onChange={this.onChangeSelect}>
-                {this.createMenuItems(this.state.calendarTypes)}
-            </Select>
+    render() {
+        return (
+            <Layout02>
+                <Header02 title={"Calendar"}/>
+                <Content01>
+                    <div>
+                        <Select
+                            name="calendarTypeSelected"
+                            value={this.state.calendarTypeSelected}
+                            label="Calendar Type"
+                            onChange={this.onChangeSelect}>
+                            {this.createMenuItems(this.state.calendarTypes)}
+                        </Select>
 
-            <Select
-                name="monthSelected"
-                value={this.state.monthSelected}
-                label="Month"
-                onChange={this.onChangeSelect}>
-                {this.createMenuItems(this.state.months)}
-            </Select>
-        </div>
-    </div>)};
+                        <Select
+                            name="monthSelected"
+                            value={this.state.monthSelected}
+                            label="Month"
+                            onChange={this.onChangeSelect}>
+                            {this.createMenuItems(this.state.months)}
+                        </Select>
+                    </div>
+                </Content01>
+                <Footer02/>
+            </Layout02>
+
+        )
+    }
 }
 
 export default Calendar;
