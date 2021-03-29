a2f160a837d9f69b6a4cbd169a5ef2be81a2d146
Sheraz <sheraz@mbp.com>
2021-03-29 00:17:23 -0400

    CDB deploy

---
+++ projects/central_masjid_dashboard/cdb/container/md-scp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp target/masjid-dashboard.tar.gz sheraz@sheraz-lubuntu1
---
+++ projects/central_masjid_dashboard/cdb/container/md-zip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf target
+mkdir target
+tar -czvf ./target/masjid-dashbaord.tar.gz masjid-dashboard
