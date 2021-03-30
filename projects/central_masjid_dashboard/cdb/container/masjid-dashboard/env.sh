9f1074c589b404a944c3dbee28ef5e412dfe0dc5
Sheraz <sheraz@mbp.com>
2021-03-30 08:51:24 -0400

    MD Docker

--- projects/central_masjid_dashboard/cdb/container/masjid-dashboard/env.sh
+++ projects/central_masjid_dashboard/cdb/container/masjid-dashboard/env.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Containers
-export cdb_container_network=cdb_network
-export cdb_container_app=cdb_app_container
-export cdb_container_db=cdb_db_container
+export md_container_network=md_network
+export md_container_app=md_app_container
+export md_container_db=md_db_container
 
 # App paths
-export cdb_path_root=~/cdb
-export cdb_path_db=$cdb_path_root/db
-export cdb_path_nginx=$cdb_path_root/nginx
+export md_path_root=~/md
+export md_path_db=$md_path_root/db
+export md_path_nginx=$md_path_root/nginx
 
 # Logs
-export cdb_path_logs_db=$cdb_path_root/logs/db
-export cdb_path_logs_nginx=$cdb_path_root/logs/nginx
-export cdb_path_logs_app=$cdb_path_root/logs/app
+export md_path_logs_db=$md_path_root/logs/db
+export md_path_logs_nginx=$md_path_root/logs/nginx
+export md_path_logs_app=$md_path_root/logs/app
--- projects/central_masjid_dashboard/cdb/container/md-zip.sh
+++ projects/central_masjid_dashboard/cdb/container/md-zip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -rf target
 mkdir target
+rm -rf ./masjid-dashboard/app/cdb.jar
+cp ../api/webservices/target/cdb.jar ./masjid-dashboard/app
 tar -czvf ./target/masjid-dashbaord.tar.gz masjid-dashboard
