68a7ebc588361edc2a39083e719c58ee19299c50
Sheraz <sheraz@mbp.com>
2021-04-10 10:04:19 -0400

    md install

--- projects/central_masjid_dashboard/cdb/container/md-install/env.sh
+++ projects/central_masjid_dashboard/cdb/container/md-install/env.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
+# Package variables
+md_app_jar=cdb.jar
+md_install=md-install
+md_app_jar_source=api/webservices/target/$md_app_jar
+
 # Containers
-export md_container_network=md_network
-export md_container_app=md_app_container
-export md_container_db=md_db_container
+md_container_network=md_network
+md_container_app=md_app_container
+md_container_db=md_db_container
 
 # App paths
-export md_app_jar=cdb.jar
-export md_install=md-install
-export md_path_root=~/masjid-dashboard
-export md_path_install_root=$md_path_root/$md-install
-export md_path_db=$md_path_root/db
-export md_path_nginx=$md_path_root/nginx
+md_path_root=~/masjid-dashboard
+md_path_install_root=$md_path_root/$md-install
+md_path_db=$md_path_root/db
+md_path_nginx=$md_path_root/nginx
 
 # Logs
-export md_path_logs_db=$md_path_root/logs/db
-export md_path_logs_nginx=$md_path_root/logs/nginx
-export md_path_logs_app=$md_path_root/logs/app
+md_path_logs_db=$md_path_root/logs/db
+md_path_logs_nginx=$md_path_root/logs/nginx
+md_path_logs_app=$md_path_root/logs/app
--- projects/central_masjid_dashboard/cdb/container/md-zip.sh
+++ projects/central_masjid_dashboard/cdb/container/md-zip.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-./md-install/env.sh
+. ./md-install/env.sh
 
+export n2=chaudhry
 export md_path_target=./target
 
 rm -rf $md_path_target
 mkdir $md_path_target
 
-# rm -rf $md_path_install/app/cdb.jar
+
+rm -rf $md_install/app/$md_app_jar
 # cp ../api/webservices/target/cdb.jar $md_path_install/app
 # tar -czvf ./target/md-install.tar.gz $md_path_install
 
