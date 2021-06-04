d25e84d57033565bb395c11a547e363a5652f6ca
Sheraz <sheraz@mbp.com>
2021-06-04 12:22:28 -0400

    auto commit

---
+++ graphql/graphql04_core_graphql-java_only/src/main/java/com/sc/graphql/config/GraphQlConfiguration.java
@@ -0,0 +1,10 @@
+package com.sc.graphql.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class GraphQlConfiguration {
+
+}
