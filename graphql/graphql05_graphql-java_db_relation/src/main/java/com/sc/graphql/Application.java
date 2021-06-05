333ad3b3c45ed2085488e19e9c160715ee1c588b
Sheraz <sheraz@mbp.com>
2021-06-05 14:56:50 -0400

    GraphQL

--- graphql/graphql05_graphql-java_db_relation/src/main/java/com/sc/graphql/Application.java
+++ graphql/graphql05_graphql-java_db_relation/src/main/java/com/sc/graphql/Application.java
@@ -1,5 +1,6 @@
 package com.sc.graphql;
 
+import com.sc.graphql.service.DataLoader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,11 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @RequiredArgsConstructor
 public class Application implements CommandLineRunner {
 
+	private final DataLoader dataLoader;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		this.dataLoader.load();
 	}
 }
---
+++ graphql/graphql05_graphql-java_db_relation/src/main/java/com/sc/graphql/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.sc.graphql.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Employee")
+@Table(name = "EMPLOYEE")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double salary;
+    private Long departmentId;
+}
