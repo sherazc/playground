75eb64e8422012fd727687330a47d0bc30ff3209
Sheraz <sheraz@mbp.com>
2021-07-08 00:37:17 -0400

    Spring shedlock

--- springboot/app08_schedule_shedLock/src/main/java/com/sc/spring/schedule/app08_schedule_shedLock/Application.java
+++ springboot/app08_schedule_shedLock/src/main/java/com/sc/spring/schedule/app08_schedule_shedLock/Application.java
@@ -1,13 +1,56 @@
 package com.sc.spring.schedule.app08_schedule_shedLock;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Scheduled(fixedRate = 2000)
+    @SchedulerLock(name = "myJobName")
+    void someJob() {
+        jdbcTemplate.queryForList("select * from shedlock").forEach(r ->
+                r.forEach((k, v) -> System.out.println(k + "=" + v)));
+
+        System.out.println(Thread.currentThread().getName() + " - Date: " + new Date());
+        System.out.println("================================");
+    }
+}
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+class ScheduleConfiguration {
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime()
+                        .build()
+        );
+    }
 }
