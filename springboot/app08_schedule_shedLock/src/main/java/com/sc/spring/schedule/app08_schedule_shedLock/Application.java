f6edcc730a1a6e9acc766af1ac978507577eaa07
Sheraz <sheraz@mbp.com>
2021-07-07 21:44:57 -0400

    Spring schedule shedlock

---
+++ springboot/app08_schedule_shedLock/src/main/java/com/sc/spring/schedule/app08_schedule_shedLock/Application.java
@@ -0,0 +1,13 @@
+package com.sc.spring.schedule.app08_schedule_shedLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ springboot/app08_schedule_shedLock/src/test/java/com/sc/spring/schedule/app08_schedule_shedLock/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.spring.schedule.app08_schedule_shedLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
