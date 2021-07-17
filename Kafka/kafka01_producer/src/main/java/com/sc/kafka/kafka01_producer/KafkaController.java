43abe9aacdef68c9de30576549e886fe9629b612
Sheraz <sheraz@mbp.com>
2021-07-17 11:24:12 -0400

    Kafka

---
+++ Kafka/kafka01_producer/src/main/java/com/sc/kafka/kafka01_producer/KafkaController.java
@@ -0,0 +1,23 @@
+package com.sc.kafka.kafka01_producer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+@RequiredArgsConstructor
+public class KafkaController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC_NAME = "sbTopic01";
+
+    @GetMapping("/publish/text")
+    public String publish(@RequestParam String message) {
+        kafkaTemplate.send(TOPIC_NAME, message);
+        return "Message Sent";
+    }
+}
---
+++ Kafka/kafka01_producer/src/test/java/com/sc/kafka/kafka01_producer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.kafka.kafka01_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
