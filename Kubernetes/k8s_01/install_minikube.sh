02b59893f59e0117f1fd628b555c597f1e8f5f56
Sheraz <sheraz@home>
2021-10-31 08:11:21 -0400

    kubernetes

---
+++ Kubernetes/k8s_01/install_minikube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://minikube.sigs.k8s.io/docs/start/
+# minikube installs kubectl (kubernetes-cli) as its dependency
+
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/
+# To install kubectl without minikube use this command
+# brew install kubernetes-cli
+
+brew install minikube
---
+++ Kubernetes/k8s_01/start_minikube.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://minikube.sigs.k8s.io/docs/start/
+
+
+# https://minikube.sigs.k8s.io/docs/drivers/
+# minikube can be started in many different kind of VMs. But docker is prefered driver
+
+minikube start --driver=docker
