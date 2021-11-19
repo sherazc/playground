3d7730620e8ca04430e836f60ca7df8dc42dd105
Sheraz <sheraz@home>
2021-11-19 09:06:10 -0500

    Junit 5

--- java-unit-test/p01_junit5/src/test/java/com/sc/junit/App01Test.java
+++ java-unit-test/p01_junit5/src/test/java/com/sc/junit/App01Test.java
@@ -1,6 +1,5 @@
 package com.sc.junit;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
---
+++ java-unit-test/p01_junit5/src/test/java/com/sc/junit/App04_ParameterizedTest.java
@@ -0,0 +1,16 @@
+package com.sc.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.time.Month;
+
+class App04_ParameterizedTest {
+
+    @ParameterizedTest
+    @EnumSource(Month.class)
+    void shouldMonthsBelow12(Month month) {
+        Assertions.assertTrue(month.getValue() < 13);
+    }
+}
