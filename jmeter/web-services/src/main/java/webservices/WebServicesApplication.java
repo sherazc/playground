be23cecc73281aeee4ba1f846c97b2b90caed49b
Sheraz <sheraz@home>
2021-11-22 07:48:24 -0500

    auto commit

---
+++ jmeter/web-services/src/main/java/webservices/WebServicesApplication.java
@@ -0,0 +1,13 @@
+package webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesApplication.class, args);
+	}
+
+}
