89b571ba591f9685e2fc32174cc44daea1117159
Sheraz <sheraz@home>
2021-12-03 08:54:53 -0500

    JUnit5 Mockito

--- java-unit-test/p02_junit5_mockito/src/test/java/com/sc/junit/App01_Extension_Test.java
+++ java-unit-test/p02_junit5_mockito/src/test/java/com/sc/junit/App01_Extension_Test.java
@@ -3,17 +3,24 @@ package com.sc.junit;
 import com.sc.junit.service.Calculation;
 import com.sc.junit.service.Calculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 class App01_Extension_Test {
 
-    private Calculator calculator;
+    private final Calculator calculator;
+    private Calculation calculation;
+
+    @Mock
+    private Set<String> names;
+
     /**
      * With JUnit5 and MockitoExtension, Mocks can be passed in constructor
      */
@@ -21,11 +28,17 @@ class App01_Extension_Test {
         this.calculator = calculator;
     }
 
+    @BeforeEach
+    void beforeEach(@Mock Calculation calculation) {
+        this.calculation = calculation;
+    }
+
     @Test
     void shouldNotBeNull() {
         Assertions.assertNotNull(calculator);
+        Assertions.assertNotNull(calculation);
+        Assertions.assertNotNull(names);
     }
-
 
     /**
      * With JUnit5 and MockitoExtension mocks can be passed in test methods
