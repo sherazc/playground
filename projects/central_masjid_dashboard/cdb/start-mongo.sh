cb750e601092bcd4c8ff528177a9c62b0378a332
Sheraz <sheraz@home>
2021-12-25 19:50:58 -0500

    Created mongodb

---
+++ projects/central_masjid_dashboard/cdb/start-mongo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Vars
+baseDir=$PWD
+dataDir=$baseDir/mongo-data
+
+# Clean up
+rm -rf $dataDir
+mkdir $dataDir
+docker rm -f cdbdb
+
+# Create container
+docker run -it \
+    -p 27017:27017 \
+    --name cdbdb \
+    -v $dataDir:/data/db \
+    -d mongo
