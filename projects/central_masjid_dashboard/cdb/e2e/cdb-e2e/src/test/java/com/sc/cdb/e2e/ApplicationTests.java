604a5fb191888928374bb682a0fe285432f10650
Sheraz <sheraz@home>
2022-01-01 13:22:38 -0500

    CDB E2E

---
+++ projects/central_masjid_dashboard/cdb/e2e/cdb-e2e/src/test/java/com/sc/cdb/e2e/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.cdb.e2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
