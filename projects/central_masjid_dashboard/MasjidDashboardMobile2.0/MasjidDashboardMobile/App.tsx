b5dbf4ef8f6179e971e936dca19442f919dedf3f
Sheraz <sheraz@home>
2022-01-10 21:00:31 -0500

    added react-native-push-notification"

--- projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/App.tsx
+++ projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/App.tsx
@@ -16,7 +16,8 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
-  View,
+  View, TouchableOpacity, Alert
+
 } from 'react-native';
 
 import {
@@ -35,13 +36,26 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const onSendNotification = () => {
+    Alert.alert(
+      "Alert Title",
+      "My Alert Msg");
+  }
+
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        
+        <TouchableOpacity onPress={onSendNotification}>
+          <Text style={{height: 60, width: "100%", backgroundColor: "green", color: "white", alignItems: 'center', justifyContent: "center", textAlign: 'center', padding: 10, fontSize: 30}} >
+            Send notification
+          </Text>
+        </TouchableOpacity>
+
+
       </ScrollView>
     </SafeAreaView>
   );
