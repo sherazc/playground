c8211fd68009a5d040a6cce796e4bc70e8e8ffc2
Sheraz <sheraz@home>
2022-01-12 22:29:06 -0500

    Added required dependencies

--- projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/android/app/src/main/java/com/masjiddashboardmobile/MainActivity.java
+++ projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/android/app/src/main/java/com/masjiddashboardmobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.masjiddashboardmobile;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +12,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "MasjidDashboardMobile";
+  }
+
+  // Added as specified in https://reactnavigation.org/docs/getting-started/
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
