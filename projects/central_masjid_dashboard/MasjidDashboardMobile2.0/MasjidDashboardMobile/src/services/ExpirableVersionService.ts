133d58891c2284060fedf6a4aad813fd2c1f842b
Sheraz <sheraz@sheraz.com>
2022-03-06 08:58:14 -0500

    expiration time

--- projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/src/services/ExpirableVersionService.ts
+++ projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/src/services/ExpirableVersionService.ts
@@ -3,16 +3,17 @@ import { ExpirableVersion } from '../types/types';
 import { createExpirationDate, nowUtcDate } from './DateService';
 
 export const isExpired = (expirableVersion?: ExpirableVersion) => {
+    /*
     console.log("expirableVersion", expirableVersion);
     console.log("expirableVersion.expirationDate.getTime()", expirableVersion.expirationDate);
     console.log("nowUtcDate().getTime()", nowUtcDate());
     console.log("expirableVersion.expirationDate.getTime()", expirableVersion.expirationDate.getTime());
     console.log("nowUtcDate().getTime()", nowUtcDate().getTime());
     console.log("expirableVersion.expirationDate.getTime() < nowUtcDate().getTime()", expirableVersion.expirationDate.getTime() < nowUtcDate().getTime());
-
+*/
     return !expirableVersion
         || !expirableVersion.expirationDate
-        || expirableVersion.expirationDate.getTime() < nowUtcDate().getTime()
+        || expirableVersion.expirationDate.getTime() < nowUtcDate().getTime();
 }
 
 export const createOrRefreshExpirableVersion = (e?: ExpirableVersion) => {
