471c37a9fd75c278e214e752b961f60874c4e54e
Sheraz <sheraz@sheraz.com>
2022-03-18 08:56:22 -0400

    Rx Java

---
+++ rxjava/rxjava01/src/main/java/com/sc/rxjava/RxJava01_createObservable.java
@@ -0,0 +1,41 @@
+package com.sc.rxjava;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.List;
+
+public class RxJava01_createObservable {
+
+    public static void main(String[] args) {
+        createObserverWithJust();
+        createObserverFromIterable();
+        createObserverUsingCreate();
+    }
+
+    private static void createObserverWithJust() {
+        Observable<Integer> observable = Observable
+                .just(1, 2, 3);
+
+        observable.subscribe(System.out::println);
+    }
+
+
+    private static void createObserverFromIterable() {
+        List<Integer> integers = List.of(1, 2, 3);
+
+        Observable<Integer> observable = Observable.fromIterable(integers);
+
+        observable.subscribe(System.out::println);
+    }
+
+
+    private static void createObserverUsingCreate() {
+        Observable<Integer> observable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+        });
+
+        observable.subscribe(System.out::println);
+    }
+}
