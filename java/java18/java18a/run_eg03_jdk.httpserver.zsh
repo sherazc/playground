f1df8fc703ceee4a1b82d21f06b5befdb946c9e1
Sheraz <sheraz@sheraz.com>
2022-06-12 15:48:27 -0400

    JDK17

--- java/java18/java18a/run_eg03_jdk.httpserver.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-# https://unix.stackexchange.com/questions/76505/unix-portable-way-to-get-scripts-absolute-path-in-zsh
-currentAbsoluteDirectory=${0:a:h}
-java -m jdk.httpserver -h
-java -m jdk.httpserver --port 4000 --directory $currentAbsoluteDirectory
--- java/java18/java18a/run_eg04_SimpleFileServer.zsh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-java src/main/java/com/sc/java18a/eg04SimpleFileServer/MySimpleFileServer.java
