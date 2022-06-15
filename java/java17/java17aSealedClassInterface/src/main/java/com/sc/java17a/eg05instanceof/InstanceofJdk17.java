614694f48aad9c9098cf4a890cc79270bd1a6498
Sheraz <sheraz@sheraz.com>
2022-06-14 23:38:16 -0400

    JDK17

--- java/java17/java17aSealedClassInterface/src/main/java/com/sc/java17a/eg05instanceof/InstanceofJdk17.java
+++ java/java17/java17aSealedClassInterface/src/main/java/com/sc/java17a/eg05instanceof/InstanceofJdk17.java
@@ -11,13 +11,14 @@ public class InstanceofJdk17 {
     public boolean isA17aB17(A17 a17) {
 
         // In Java17 this is a compilation error. Because of sealed class compiler
-        // recognizes that a17 can never be instanceof B17
+        // recognizes that a17 can never be instanceof B17 class
 
         // return a17 instanceof B17;
 
         return true;
     }
 }
+
 sealed interface A17 permits C17 {}
 class B17 {}
 final class C17 implements A17 {}
---
+++ java/java17/java17aSealedClassInterface/src/main/java/com/sc/java17a/eg06_sealed_permit_keywords/SealedPermitKeywords.java
@@ -0,0 +1,11 @@
+package com.sc.java17a.eg06_sealed_permit_keywords;
+
+public class SealedPermitKeywords {
+
+    /**
+     * To keep backwards compatibility "sealed" and "permit" keywords can be used as class, method and variable names
+     */
+    public void sealed() {
+        int permits = 5;
+    }
+}
