64497c11b0f938535fd0533108e9b5096467ac67
Sheraz <sheraz@sheraz.com>
2022-08-19 09:33:41 -0400

    MDB

--- projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/__tests__/services/CompanyDataService-test.ts
+++ projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/__tests__/services/CompanyDataService-test.ts
@@ -1,10 +1,11 @@
-import { getCompanyId, getCompanyName, isValidCompany } from "../../src/services/CompanyDataService";
-import { Company } from "../../src/types/types";
+import * as DateService from "../../src/services/common/DateService";
+import { getCompanyId, getCompanyName, isValidCompany, updateCompanyData } from "../../src/services/CompanyDataService";
+import { Company, CompanyData } from "../../src/types/types";
 
 // TODO: upgrade react-native-push-notification and check if this is still needed
 jest.mock("react-native-push-notification", () => ({}));
 
-describe("CompanyDataService", () => {
+describe("CompanyDataService - Utility functions", () => {
 
     it("isValidCompany()", () => {
         expect(isValidCompany()).toBeFalsy();
@@ -12,34 +13,64 @@ describe("CompanyDataService", () => {
         // @ts-ignore
         expect(isValidCompany({})).toBeFalsy();
         // @ts-ignore
-        expect(isValidCompany({id: "abc"})).toBeTruthy();
+        expect(isValidCompany({ id: "abc" })).toBeTruthy();
     });
 
 
     it("getCompanyName()", () => {
         // @ts-ignore
-        const company:Company = {}
+        const company: Company = {}
 
         expect(getCompanyName(company)).toBe("");
 
         company.name = ""
         expect(getCompanyName(company)).toBe("");
-        
+
         company.name = "a"
         expect(getCompanyName(company)).toBe("a");
     });
 
     it("getCompanyId()", () => {
         // @ts-ignore
-        const company:Company = {}
+        const company: Company = {}
 
         expect(getCompanyId(company)).toBeUndefined();
 
         company.id = ""
         expect(getCompanyId(company)).toBeUndefined();
-        
+
         company.id = "a"
         expect(getCompanyId(company)).toBe("a");
-        
+
     });
 });
+
+
+describe("CompanyDataService - API Service functions", () => {
+
+    it("updateCompanyData()", () => {
+
+        // @ts-ignore
+        const companyData: CompanyData = {
+            // @ts-ignore
+            company: { id: "a" }
+        };
+
+        jest.spyOn(DateService, "getTodaysMonth").mockImplementation(() => 2);
+        const a = DateService.getTodaysMonth();
+        console.log(a)
+
+        // const updateCompanyData = require("../../src/services/common/DateService");
+
+        // jest.spyOn(updateCompanyData, "getTodaysMonth").mockImplementation(() => 2);
+
+        updateCompanyData(companyData);
+
+    });
+
+    afterEach(() => {
+        // restore the spy created with spyOn
+        jest.restoreAllMocks();
+    });
+});
+
