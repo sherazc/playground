ff9f7b65e4f11e243480fd7214919f986fb155c6
Sheraz <sheraz@sheraz.com>
2022-08-31 08:18:59 -0400

    MDB

--- projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/__tests__/services/CompanyListDataService-test-2.ts
+++ projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/__tests__/services/CompanyListDataService-test-2.ts
@@ -2,19 +2,21 @@ import { updateCompanyListData } from "../../src/services/CompanyListDataService
 import { mockCreateCompanyListData } from "../../__mocks__/MockTypes";
 import * as ApiMdb from "../../src/services/ApiMdb";
 import * as ReduxStoreService from "../../src/store/ReduxStoreService";
+import * as ExpirableVersionService from "../../src/services/ExpirableVersionService";
+import { ExpirableVersion } from "../../src/types/types";
 
-
+/*
 jest.mock("../../src/services/ExpirableVersionService", () => ({
     isExpired: () => true,
     createOrRefreshExpirableVersion: () => ({
         version: 300, // mockCreateExpirableVersion() creates version=300
     })
 }));
-
+*/
 
 describe("CompanyListDataService", () => {
 
-    test("updateCompanyListData() - Expired, Same Version", () => {
+    test("updateCompanyListData() - Expired, Same Version", async () => {
         // Setup
         const mockCompanyListData = mockCreateCompanyListData();
         let mockInitTime = mockCompanyListData.tracker?.expirableVersion?.expirationDate?.jsDate.getTime();
@@ -24,16 +26,20 @@ describe("CompanyListDataService", () => {
             version: 300 // mockCreateExpirableVersion() creates version=300
         }));
 
-        
-        const spyStoreDispatchCompanyListData = jest.spyOn(ReduxStoreService, "storeDispatchCompanyListData").mockImplementation();
+
+        const spyIsExpired = jest.spyOn(ExpirableVersionService, "isExpired").mockImplementation(() => true);
+        const spyCreateOrRefreshExpirableVersion = jest.spyOn(ExpirableVersionService, "createOrRefreshExpirableVersion").mockImplementation((e?: ExpirableVersion) => ({}));
+        const spyStoreDispatchCompanyListData = jest.spyOn(ReduxStoreService, "storeDispatchCompanyListData").mockImplementation(() => {});
 
 
         // Call
-        updateCompanyListData(mockCompanyListData);
+        await updateCompanyListData(mockCompanyListData);
 
         // Assert
         expect(spyApiCompanyListVersion).toBeCalled();
         expect(spyStoreDispatchCompanyListData).toBeCalled();
+        expect(spyIsExpired).toBeCalled();
+        expect(spyCreateOrRefreshExpirableVersion).toBeCalled();
     });
 
     afterEach(() => {
