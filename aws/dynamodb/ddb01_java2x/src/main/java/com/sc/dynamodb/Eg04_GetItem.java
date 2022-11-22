b01b0b22cf1193c6f6c4a37a15a721f39c3aaa94
Sheraz <sheraz@sheraz.com>
2022-11-22 08:47:48 -0500

    Dynamo DB

--- aws/dynamodb/ddb01_java2x/src/main/java/com/sc/dynamodb/Eg04_GetItem.java
+++ aws/dynamodb/ddb01_java2x/src/main/java/com/sc/dynamodb/Eg04_GetItem.java
@@ -3,6 +3,7 @@ package com.sc.dynamodb;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
 
@@ -19,34 +20,35 @@ public class Eg04_GetItem {
         // Step 1 - Create client
         DynamoDbClient dynamoDbClient = LocalDbConnection.getClient();
 
-        // Step 2 - Put Item request
+        // Step 2 - Get Item request
         GetItemRequest getItemRequest = getGetItemRequest(tableName, primaryKeyName, sortKeyName);
 
-        // Step 3 - Put Item
+        // Step 3 - Get Item
         getItem(dynamoDbClient, getItemRequest);
-
-
     }
 
     public static void getItem(DynamoDbClient dynamoDbClient, GetItemRequest getItemRequest) {
         System.out.println("Getting items...");
-        // TODO: Do this in next example.
-        // https://nickolasfisher.com/blog/Querying-DynamoDB-in-Java-with-the-AWS-SDK-20
-        System.out.println("Done! " + putItemResponse.toString());
+        GetItemResponse getItemResponse = dynamoDbClient.getItem(getItemRequest);
+        System.out.printf("records found = %b\n", getItemResponse.hasItem());
+        Map<String, AttributeValue> attributeValueMap = getItemResponse.item();
+
+        attributeValueMap.forEach((name, value) -> {
+            System.out.printf("%s=%s\n", name, value.s());
+        });
     }
 
-    private static GetItemRequest getGetItemRequest(String tableName, String primaryKeyName, String primaryKeyValue) {
+    private static GetItemRequest getGetItemRequest(String tableName, String primaryKeyName, String sortKeyName) {
         System.out.println("Creating GetItemRequest...");
 
         Map<String, AttributeValue> keyToGet = Map.of(
-                primaryKeyName, AttributeValue.builder().s("pk_").build());
+                primaryKeyName, AttributeValue.builder().s("pk_1668693849032").build(),
+                sortKeyName, AttributeValue.builder().s("sk_1668693849032").build()
+        );
 
         return GetItemRequest.builder()
                 .key(keyToGet)
-
                 .tableName(tableName)
                 .build();
     }
-
-
 }
