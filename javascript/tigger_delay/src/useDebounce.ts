415f257833c5b60c4f8e7fd7ff5ee9121a63fcf0
Sheraz <sheraz@sheraz.com>
2022-11-29 08:09:37 -0500

    debounce

--- javascript/tigger_delay/src/useDebounce.ts
+++ javascript/tigger_delay/src/useDebounce.ts
@@ -1,6 +1,5 @@
 import { debounceFunction } from "./Debounce";
 
-console.log("TRes")
 // Business logic function
 const myBusinessLogicFunction = (a: number, b: number) => {
     console.log("myBusinessLogicFunction Called", a + b, new Date());
@@ -9,13 +8,10 @@ const myBusinessLogicFunction = (a: number, b: number) => {
 // Convert business logic function into debounce function
 const myBusinessLogicFunctionDebounce = debounceFunction(myBusinessLogicFunction, 3000);
 
-
 // Add onclick Event function
 export const useDebounce = () => {
-    const buttonActivate = document.getElementById("buttonActivate") as HTMLButtonElement;
+    const buttonActivate = document.getElementById("useDebounceButton") as HTMLButtonElement;
     buttonActivate.onclick = function() {
         myBusinessLogicFunctionDebounce(1, 2);
     }    
 }
-
-// export {useDebounce};
