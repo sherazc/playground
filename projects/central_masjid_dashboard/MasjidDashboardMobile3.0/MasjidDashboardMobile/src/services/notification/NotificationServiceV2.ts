7926b31517a02a1a8d1461c205bd9b6f984bde1d
Sheraz <sheraz@sheraz.com>
2022-12-14 23:50:39 -0500

    MDB

--- projects/central_masjid_dashboard/MasjidDashboardMobile3.0/MasjidDashboardMobile/src/services/notification/NotificationServiceV2.ts
+++ projects/central_masjid_dashboard/MasjidDashboardMobile3.0/MasjidDashboardMobile/src/services/notification/NotificationServiceV2.ts
@@ -2,6 +2,7 @@ import { storeGetCompanyData, storeGetSetting } from "../../store/ReduxStoreServ
 import { CompanyData, CompanyNotification, SettingData } from "../../types/types";
 import { debounce } from "../Debounce";
 import { getCurrentSystemDate, dayOfTheYear, TIME_24_REGX, addMinutesTo24hTime } from '../common/DateService';
+import { expoRemoveAllExistingNotificationsAsync } from "./ExpoNotification";
 
 
 
@@ -9,7 +10,7 @@ import { getCurrentSystemDate, dayOfTheYear, TIME_24_REGX, addMinutesTo24hTime }
 const setupNotificationV2 = (settingState: SettingData, companyId: string) => {
 
     const notificationPromise = new Promise<boolean>((resolve, reject) => {
-        const currentTimeMillis = getCurrentSystemDate().getTime();
+        const now = getCurrentSystemDate();
         
         const settingStore = storeGetSetting();
         const companyData = storeGetCompanyData();
@@ -17,8 +18,32 @@ const setupNotificationV2 = (settingState: SettingData, companyId: string) => {
         const sameSettingAlert = isSameSettingAlert(settingState, settingStore);
         const anyAlertOn = isAnyAlertOn(settingState);
         const validCompanyDataAvailable = isValidCompanyDataAvailable(companyId, companyData);
+        const notificationExpired = isNotificationExpired(now.getTime(), companyData.companyNotification);
 
+
+        // Invalid company Data
+        if (!validCompanyDataAvailable) {
+            removeAllExistingNotificationsAsyncV2();
+            reject();
+            return;
+        }
+
+        // No alerts setting
+        if (!anyAlertOn) {
+            removeAllExistingNotificationsAsyncV2();
+            resolve(false);
+            return;
+        }
+
+        // Alerts not expired and same as previous alert settings
+        if (!notificationExpired && sameSettingAlert) {
+            resolve(false);
+            return;
+        }
         
+
+
+
 
     });
 
@@ -31,6 +56,9 @@ const setupNotificationV2 = (settingState: SettingData, companyId: string) => {
 }
 export const setupNotificationV2Debounce = debounce(setupNotificationV2, 3000);
 
+export const removeAllExistingNotificationsAsyncV2 = ():Promise<any> => {
+    return expoRemoveAllExistingNotificationsAsync();
+}
 
 const isSameSettingAlert = (setting1: SettingData, setting2: SettingData) => {
     return setting1 && setting2
@@ -43,7 +71,7 @@ const isAnyAlertOn = (setting: SettingData): boolean => {
     return setting.azanAlert || setting.beforeIqamaAlert || setting.iqamaAlert;
 }
 
-// Checks same company and valid prayerYear
+// Checks companyId matches and valid prayerYear
 const isValidCompanyDataAvailable = (companyId: string, companyData: CompanyData): boolean => {
     return isSameCompany(companyId, companyData)
         && companyData.prayersYear != undefined && companyData.prayersYear.year != undefined
@@ -56,9 +84,8 @@ const isSameCompany = (companyId: string, companyData: CompanyData): boolean => 
         && companyData.company.id != undefined && companyData.company.id === companyId;
 }
 
-const isNotificationExpired = (companyNotification?: CompanyNotification): boolean => {
-    return companyData != undefined && companyData.company != undefined
-        && companyData.company.id != undefined && companyData.company.id === companyId;
+const isNotificationExpired = (nowMilliseconds: number, companyNotification?: CompanyNotification): boolean => {
+    return companyNotification === undefined
+        || companyNotification.expirationMillis === undefined
+        || companyNotification.expirationMillis < nowMilliseconds;
 }
-
-
