b6d331eeb433c28d452eba5098965bc88ba42b3e
Sheraz <sheraz@sheraz.com>
2023-02-16 23:07:57 -0500

    CDB 2.0

--- projects/central_masjid_dashboard/cdb/backup-db.sh
+++ projects/central_masjid_dashboard/cdb/backup-db.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 backupDir="db-backup-$(date +"%Y-%m-%d-%H-%M")"
-rm -rf "$backupDir"
+# rm -rf "$backupDir"
 mkdir "$backupDir"
-cd "$backupDir"
-touch file.txt
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=company  --out=./cdb/company.json
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=centralControl  --out=./cdb/centralControl.json
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=companyDataVersion  --out=./cdb/companyDataVersion.json
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=companyListVersion  --out=./cdb/companyListVersion.json
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=prayerConfig  --out=./cdb/prayerConfig.json
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=user  --out=./cdb/user.json
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=hadith  --out=./cdb/hadith.json
-# mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=picklist  --out=./cdb/picklist.json
 
+cd "$backupDir"
+
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=company  --out=company.json
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=centralControl  --out=centralControl.json
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=companyDataVersion  --out=companyDataVersion.json
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=companyListVersion  --out=companyListVersion.json
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=prayerConfig  --out=prayerConfig.json
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=user  --out=user.json
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=hadith  --out=hadith.json
+mongoexport --uri="mongodb://localhost:27017/cdb"  --collection=picklist  --out=picklist.json
+
+# shellcheck disable=SC2103
+cd ..
+tar -cvzf "$backupDir.tar.gz" "$backupDir"
+
+rm -rf "$backupDir"
