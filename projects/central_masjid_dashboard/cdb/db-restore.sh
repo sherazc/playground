17339216baac557594c12673172c4a118ed557ee
Sheraz <sheraz@sheraz.com>
2023-02-16 23:42:20 -0500

    CDB 2.0

---
+++ projects/central_masjid_dashboard/cdb/db-restore.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# scp -i /Users/sheraz/.ssh/id_rsa -r \
+#     ubuntu@3.215.75.115:/home/ubuntu/cdb/data_export/db-backup-2023-02-17-03-49.tar.gz \
+#     misc/data_export/db-backup-2023-02-17-03-49.tar.gz
+
+backupDir="db-backup-2023-02-17-03-49"
+backupFile="$backupDir.tar.gz"
+tar -zxvf "$backupFile"
+cd "$backupDir"
+
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=company  --file=company.json
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=centralControl  --file=centralControl.json
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=companyDataVersion  --file=companyDataVersion.json
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=companyListVersion  --file=companyListVersion.json
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=prayerConfig  --file=prayerConfig.json
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=user  --file=user.json
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=hadith  --file=hadith.json
+mongoimport --uri="mongodb://localhost:27017/cdb"  --collection=picklist  --file=picklist.json
+
+
+cd ..
+
+rm -rf "$backupDir"
