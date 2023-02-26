e722bf4481d8ff163ef49c457b338947467b6a5d
Sheraz <sheraz@sheraz.com>
2023-02-25 19:46:14 -0500

    NodeJS NPM Monorepo Workspace

---
+++ nodejs/monorepo_multi_module/clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -rf masjid_lib/package-lock.json
+rm -rf masjid_lib/dist
+rm -rf masjid_lib/node_modules
+
+rm -rf masjid_ui/package-lock.json
+rm -rf masjid_ui/dist
+rm -rf masjid_ui/node_modules
+
+rm -rf masjid_reactjs/package-lock.json
+rm -rf masjid_reactjs/build
+rm -rf masjid_reactjs/node_modules
---
+++ nodejs/npm_file_dependency_masjid_app/clean.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm -rf masjid_lib/package-lock.json
+rm -rf masjid_lib/dist
+rm -rf masjid_lib/node_modules
+
+rm -rf masjid_ui/package-lock.json
+rm -rf masjid_ui/dist
+rm -rf masjid_ui/node_modules
+
+rm -rf masjid_reactjs/package-lock.json
+rm -rf masjid_reactjs/build
+rm -rf masjid_reactjs/node_modules
+
+
+rm -rf masjid_react_native/package-lock.json
+rm -rf masjid_react_native/.expo
+rm -rf masjid_react_native/node_modules
