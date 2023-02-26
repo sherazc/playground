abade7fbbbc7a25ef0600c95f8bfd69705386965
Sheraz <sheraz@sheraz.com>
2023-02-26 07:57:46 -0500

    MDB Archive

--- projects/central_masjid_dashboard/MasjidDashboardMobile2.0/MasjidDashboardMobile/run-android.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start android emulator on mac without starting android studio
-# $ANDROID_HOME/tools/emulator @Pixel_2_API_27
-
-# Manually start metro on linux
-# gnome-terminal -e "bash -c \"react-native start ; exec bash\""
-
-npx react-native run-android --verbose
