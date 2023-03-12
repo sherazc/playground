e31af690259549c10cbaecd51860e1bd8eed445f
Sheraz <sheraz@sheraz.com>
2023-03-12 12:18:17 -0400

    CDB 2.0

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/DashboardSelect/DashboardSelect.tsx
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/DashboardSelect/DashboardSelect.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {CompanyDashboardV2} from "../CompanyDashboardV2/CompanyDashboardV2";
+import CompanyDashboard from "../CompanyDashboard/CompanyDashboard";
 
 interface Props {
+
 }
 
-export const DashboardSelect: React.FC<Props> = () => {
+export const DashboardSelect: React.FC<Props> = (props) => {
 
+    useEffect(() => {
+        console.log(props)
+    }, [])
 
 
     return (
         <div>
 
             Dashboard Select
+            <CompanyDashboard {...props}/>
             <CompanyDashboardV2/>
         </div>
     );
