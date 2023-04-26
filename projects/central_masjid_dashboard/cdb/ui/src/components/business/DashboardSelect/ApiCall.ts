bfe0b17442f8fdeb01332695a01d24fef9c1102c
Sheraz <sheraz@sheraz.com>
2023-04-26 01:31:33 -0400

    CDB 2.0

--- projects/central_masjid_dashboard/cdb/ui/src/components/business/DashboardSelect/ApiCall.ts
+++ projects/central_masjid_dashboard/cdb/ui/src/components/business/DashboardSelect/ApiCall.ts
@@ -46,30 +46,26 @@ const apiCentralConfiguration = (companyUrl: string): Promise<CustomConfiguratio
  *
  * TODO: Change parameters to ApiRequest
  *
- * @param endpoint
- * @param payload
- * @param apiMethod
- * @param headers
  */
-const callApi = (endpoint: string, payload?: any, apiMethod?: ApiMethod, headers?: ApiHeaders): Promise<any> => {
+const callApi = (request: ApiRequest): Promise<any> => {
     const requestInit: RequestInit = {
-        method: apiMethod ? apiMethod : "GET"
+        method: request.method ? request.method : "GET"
     }
 
-    if (headers) {
-        requestInit.headers = headers;
+    if (request.headers) {
+        requestInit.headers = request.headers;
     }
 
-    if (payload) {
-        if (typeof payload === "string") {
-            requestInit.body = payload
-        } else if(payload instanceof String) {
-            requestInit.body = payload.toString();
+    if (request.payload) {
+        if (typeof request.payload === "string") {
+            requestInit.body = request.payload
+        } else if(request.payload instanceof String) {
+            requestInit.body = request.payload.toString();
         } else {
-            requestInit.body = JSON.stringify(payload);
+            requestInit.body = JSON.stringify(request.payload);
         }
     }
-    return fetch(endpoint, requestInit).then(response => response.json());
+    return fetch(request.endpoint, requestInit).then(response => response.json());
 }
 
 
@@ -78,57 +74,56 @@ const callApi = (endpoint: string, payload?: any, apiMethod?: ApiMethod, headers
  *
  *
  */
-const cdbApiUnAuth = (baseUrl: string, authToken?: string, globalCbs?: InterceptorCallBacks) => {
+const cdbApiUnAuth = (baseUrl: string, commonHeaders?: ApiHeaders, globalCbs?: InterceptorCallBacks) => {
 
     const endpoints = makeEndpoints(baseUrl);
 
-    const authTokenHeader:[string, string][] = [];
-    if (authToken) {
-        authTokenHeader.push(["Authorization", `Bearer ${authToken}`])
-    }
+    // const common:ApiHeaders;
+    // if (commonHeaders) {
+    //     authTokenHeader.push(["Authorization", `Bearer ${authToken}`])
+    // }
 
 
     const api = {
 
         apiCentralConfiguration: (companyId: string): Promise<CustomConfiguration> => {
-
             // Step 1 Create Endpoint
             const endpoint = endpoints.createConfigurationEndpoint(companyId);
 
             // Step 2: Create Request
-            console.log("Calling API ", endpoint);
-
-
+            const request: ApiRequest = {
+                endpoint
+            }
 
             // Step 3: Create Promise to call API and decorate it with callbacks
-            // TODO: refactor it in a function
-            if (globalCbs && globalCbs.before) {
-                globalCbs.before();
-            }
-            return new Promise((resolve, reject) => {
-                let responsePromise = callApi(endpoint);
-                responsePromise.then(response => {
-                    resolve(response);
-                    if (globalCbs && globalCbs.afterSuccess) {
-                        globalCbs.afterSuccess(response);
-                    }
-                }, error => {
-                    reject(error);
-                    if (globalCbs && globalCbs.afterError) {
-                        globalCbs.afterError(error);
-                    }
-                })
-            });
-
-
-
-
+            return createRequestPromise(request, globalCbs);
         }
     }
 
 
     return api;
 }
+
+const createRequestPromise = (request: ApiRequest, globalCbs?: InterceptorCallBacks): Promise<any> => {
+    if (globalCbs && globalCbs.before) {
+        globalCbs.before();
+    }
+    return new Promise((resolve, reject) => {
+        let responsePromise = callApi(request);
+        responsePromise.then(response => {
+            resolve(response);
+            if (globalCbs && globalCbs.afterSuccess) {
+                globalCbs.afterSuccess(response);
+            }
+        }, error => {
+            reject(error);
+            if (globalCbs && globalCbs.afterError) {
+                globalCbs.afterError(error);
+            }
+        })
+    });
+}
+
 
 const globalCbs: InterceptorCallBacks = {
     before: () => console.log("Before"),
@@ -137,7 +132,7 @@ const globalCbs: InterceptorCallBacks = {
 }
 
 
-let api = cdbApiUnAuth("http://localhost:8085", "TestTokenABC", globalCbs);
+let api = cdbApiUnAuth("http://localhost:8085", [], globalCbs);
 
 api.apiCentralConfiguration("5da2632ef2a2337a5fd916d3").then(r => console.log(r));
 // api.apiCentralConfiguration("hic").then(r => console.log(r));
