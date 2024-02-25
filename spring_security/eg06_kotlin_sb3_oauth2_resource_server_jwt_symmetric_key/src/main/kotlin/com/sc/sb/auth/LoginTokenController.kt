3331e4544b3501689ec622821396073e48002046
sheraz <sheraz@mbp16.com>
2024-02-24 19:41:07 -0500

    Kotlin sb3 security

--- spring_security/eg06_kotlin_sb3_oauth2_resource_server_jwt_symmetric_key/src/main/kotlin/com/sc/sb/auth/LoginTokenController.kt
+++ spring_security/eg06_kotlin_sb3_oauth2_resource_server_jwt_symmetric_key/src/main/kotlin/com/sc/sb/auth/LoginTokenController.kt
@@ -1,9 +1,18 @@
 package com.sc.sb.auth
 
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/login")
-class LoginTokenController {
+class LoginTokenController(private val scTokenGeneratorService: ScTokenGeneratorService) {
+
+    @GetMapping("/token")
+    @PreAuthorize("hasRole('USER')")
+    fun token(authentication: Authentication, @RequestParam requestedScopes: Array<String>) =
+        scTokenGeneratorService.generateToken(authentication, requestedScopes)
 }
