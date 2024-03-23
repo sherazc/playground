6f1750b959e036bbacf439d7d29cdde0ca183b94
sheraz <sheraz@mbp16.com>
2024-03-23 10:08:58 -0400

    Embedded google slide

--- reactjs/reactjs05_embed_google_slide/src/components/Home/Home.tsx
+++ reactjs/reactjs05_embed_google_slide/src/components/Home/Home.tsx
@@ -3,38 +3,55 @@ import styles from "./Home.module.scss"
 import {Screen1} from "./Screen1/Screen1";
 import {Screen2} from "./Screen2/Screen2";
 import {Screen3} from "./Screen3/Screen3";
-interface Props {}
+
+interface Props {
+}
 
 type ScreenName = "Screen1" | "Screen2" | "Screen3";
 
-const screens: Array<{name: ScreenName, screen: JSX.Element}> = [
+const screens: Array<{ name: ScreenName, screen: JSX.Element }> = [
     {name: "Screen1", screen: <Screen1/>},
     {name: "Screen2", screen: <Screen2/>},
     {name: "Screen3", screen: <Screen3/>},
 ]
 
-export const Home:React.FC<Props> = () => {
+export const Home: React.FC<Props> = () => {
 
     const [currentScreen, setCurrentScreen] =
         useState<ScreenName>("Screen1");
 
-    const getScreen = (screenName: String) =>
+    const getScreen = (screenName: String): JSX.Element =>
         screens.filter(s => s.name === screenName)[0].screen;
 
     return (
-    <>
-        <header>
-            <nav className={styles.main_nav}>
-                <ul>
-                    <li onClick={() => {setCurrentScreen("Screen1")}}>Screen1</li>
-                    <li onClick={() => {setCurrentScreen("Screen2")}}>Screen2</li>
-                    <li onClick={() => {setCurrentScreen("Screen3")}}>Screen3</li>
-                </ul>
-            </nav>
-        </header>
-        <div>
-        {getScreen(currentScreen)}
-        </div>
-    </>
+        <>
+            <header>
+                <nav className={styles.main_nav}>
+                    <ul>
+                        <li>
+                            <button className={currentScreen === "Screen1" ? styles.selected : ""} onClick={() => {
+                                setCurrentScreen("Screen1")
+                            }}>Screen1
+                            </button>
+                        </li>
+                        <li>
+                            <button className={currentScreen === "Screen2" ? styles.selected : ""} onClick={() => {
+                                setCurrentScreen("Screen2")
+                            }}>Screen2
+                            </button>
+                        </li>
+                        <li>
+                            <button className={currentScreen === "Screen3" ? styles.selected : ""}
+                                    onClick={() => {setCurrentScreen("Screen3")
+                            }}>Screen3
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <div>
+                {getScreen(currentScreen)}
+            </div>
+        </>
     );
 }
