110906674cec3b934d92c31baab07ae8ff4d80af
sheraz <sheraz@mbp16.com>
2024-04-20 22:56:32 -0400

    CDB - S3 support

---
+++ aws/s301/src/main/java/com/sc/aws/Step2_ListBuckets.java
@@ -0,0 +1,20 @@
+package com.sc.aws;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.util.List;
+
+public class Step2_ListBuckets {
+    public static void main(String[] args) {
+        AmazonS3 s3 = AmazonS3ClientBuilder
+                .standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
+
+        List<Bucket> buckets = s3.listBuckets();
+        buckets.forEach(System.out::println);
+    }
+}
