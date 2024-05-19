b55b186e4cbea5e2c447f9f94ce873a1c914891e
sheraz <sheraz@mbp16.com>
2024-05-19 08:34:46 -0400

    MDB - support s3

--- projects/central_masjid_dashboard/cdb/api/services/src/main/java/com/sc/cdb/services/storage/FilesManager.java
+++
@@ -1,9 +0,0 @@
-package com.sc.cdb.services.storage;
-
-public interface FilesManager {
-    boolean exists(String storage, String directory, String fileName);
-    long size(String storage, String directory, String fileName);
-    boolean delete(String storage, String directory, String fileName);
-    byte[] read(String storage, String directory, String fileName);
-    boolean write(String storage, String directory, String fileName, byte[] data);
-}
