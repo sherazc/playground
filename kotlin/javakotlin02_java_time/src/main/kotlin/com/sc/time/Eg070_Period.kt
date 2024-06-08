a76435a7178e26cc6aa28240cb52a9f32f84524f
sheraz <sheraz@mbp16.com>
2024-06-08 15:04:03 -0400

    Java Time API

--- kotlin/javakotlin02_java_time/src/main/kotlin/com/sc/time/Eg070_Period.kt
+++ kotlin/javakotlin02_java_time/src/main/kotlin/com/sc/time/Eg070_Period.kt
@@ -4,11 +4,28 @@ import java.time.LocalDate
 import java.time.Period
 
 fun main() {
+    /*
+    Period is used to calculate time between 2 dates
+    And add it to LocalDate
+     */
+
     val now = LocalDate.now()
     val nextDate = now.plusMonths(5).plusYears(2).plusDays(40)
-    val period: Period = Period.between(now, nextDate)
-    println("period: $period")
-    println("period days: ${period.days}")
-    println("period months: ${period.months}")
-    println("period years: ${period.years}")
+    val period1: Period = Period.between(now, nextDate)
+    println("period: $period1")
+    println("period days: ${period1.days}")
+    println("period months: ${period1.months}")
+    println("period years: ${period1.years}")
+
+    val period2 = Period.ofDays(5)
+        .plusMonths(2)
+        .plusYears(4)
+    println(period2.isNegative)
+
+    val period3 = Period.of(3, 9, 20)
+    println(period3.isZero)
+
+    val period4 = Period.parse("P2Y3M5D")
+    val dateTime1:LocalDate = period4.addTo(LocalDate.now()) as LocalDate
+    println(dateTime1)
 }
