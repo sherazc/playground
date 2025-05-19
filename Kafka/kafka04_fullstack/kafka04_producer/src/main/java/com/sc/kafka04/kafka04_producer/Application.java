ae706fcfb285c4b1502f74edee7b8f2a2c612739
sheraz@mbp16 <sheraz@bitsegment.com>
2025-05-19 10:13:44 -0400

    Kafka Full Stack

--- Kafka/kafka04_fullstack/kafka04_producer/src/main/java/com/sc/kafka04/kafka04_producer/Application.java
+++ Kafka/kafka04_fullstack/kafka04_producer/src/main/java/com/sc/kafka04/kafka04_producer/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Kafka04ProducerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Kafka04ProducerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
---
+++ Kafka/kafka04_fullstack/kafka04_producer/src/main/java/com/sc/kafka04/kafka04_producer/config/KafkaConfig.java
@@ -0,0 +1,33 @@
+package com.sc.kafka04.kafka04_producer.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+  @Bean
+  public ProducerFactory<String, String> producerFactory() {
+    Map<String, Object> configProps = new HashMap<>();
+    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+    // add other producer configs if needed
+
+    return new DefaultKafkaProducerFactory<>(configProps);
+  }
+
+  @Bean
+  public KafkaTemplate<String, String> kafkaTemplate() {
+    return new KafkaTemplate<>(producerFactory());
+  }
+}
