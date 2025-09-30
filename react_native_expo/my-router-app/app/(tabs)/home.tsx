6357fd74443c30b2e37bb92c77681afb753659ac
sheraz@mbp16 <sheraz@bitsegment.com>
2025-09-30 15:46:59 -0400

    expo router

--- react_native_expo/my-router-app/app/(tabs)/home.tsx
+++ react_native_expo/my-router-app/app/(tabs)/home.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Home() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home Page</Text>
+      <Button
+        title="Go to Index (Clear History)"
+        onPress={() => {
+          // replace clears the stack history
+          router.dismissAll();
+          router.replace("/");
+        }}
+      />
     </View> 
   );
 }
--- react_native_expo/my-router-app/app/_layout.tsx
+++ react_native_expo/my-router-app/app/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack >
-      <Stack.Screen name="index" options={{ title: "Index Page" }} />
+      <Stack.Screen name="index" options={{ title: "Index Page", 
+        // headerBackVisible: false, 
+        animation: "none" 
+      }} />
       <Stack.Screen name="about" options={{ title: "About Us" }} />
       {/*  "headerShown: false" hides the Stack Nav header */}
       <Stack.Screen name="(tabs)" options={{ title: "Dashboard", headerShown: false }} />
