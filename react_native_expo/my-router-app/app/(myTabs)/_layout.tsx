2593a40d380dc512ec4cc7a083b962d91da1924c
sheraz@mbp16 <sheraz@bitsegment.com>
2025-10-01 20:11:02 -0400

    expo router

--- react_native_expo/my-router-app/app/(myTabs)/_layout.tsx
+++ react_native_expo/my-router-app/app/(myTabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from "expo-router";
 
-export default function TabLayout() {
+export default function MyTabLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="home" options={{ title: "Home", headerShown: false}} />
