19f29af2fce6c539d818507bec8e3d6b26ebfb06
sheraz@mbp16 <sheraz@bitsegment.com>
2025-10-05 07:54:44 -0400

    Router hook

--- react_native_expo/my-router-app/src/app/(myTabs)/home.tsx
+++ react_native_expo/my-router-app/src/app/(myTabs)/home.tsx
@@ -1,7 +1,8 @@
-import { router } from "expo-router";
+import { useRouter } from "expo-router";
 import { Button, Text, View } from "react-native";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home Page</Text>
